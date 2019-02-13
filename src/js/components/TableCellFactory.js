d9f9a46a8f79c797861c5c0ed12ce1f781b3e9a0
anh-trinh <anh-trinh@cybozu.vn>
2019-02-13 10:20:34 +0700

    Refactor TableCellFactory

--- src/js/components/TableCellFactory.js
+++ src/js/components/TableCellFactory.js
@@ -12,33 +12,49 @@ const validFieldTypes = ['text', 'dropdown', 'checkbox', 'multichoice', 'radio',
 
 const createTableCell = (type, fieldName) => {
   let field;
+  let FieldComponent;
   if (!validFieldTypes.some(fieldType => fieldType === type)) {
     throw new Error(Message.control.INVALID_TABLE_FIELDS + '"' + validFieldTypes.join('","') + '"');
   }
   const init = ({updateRowData, rowIndex, table}) => {
-    // switch case types
+    switch (type) {
+      case 'text':
+        FieldComponent = Text;
+        break;
+      case 'dropdown':
+        FieldComponent = Dropdown;
+        break;
+      case 'checkbox':
+        FieldComponent = CheckBox;
+        break;
+      case 'multichoice':
+        FieldComponent = MultipleChoice;
+        break;
+      case 'radio':
+        table.data[rowIndex][fieldName].name += '_' + rowIndex;
+        FieldComponent = RadioButton;
+        break;
+      case 'label':
+        FieldComponent = Label;
+        break;
+      case 'icon':
+        FieldComponent = IconButton;
+        break;
+      case 'alert':
+        FieldComponent = Alert;
+        break;
+      default:
+        break;
+    }
+    field = new FieldComponent({...table.data[rowIndex][fieldName]});
+
+    // assign listeners
     switch (type) {
       case 'text':
       case 'dropdown':
       case 'checkbox':
       case 'multichoice':
       case 'radio':
-        if (type === 'text') {
-          field = new Text({...table.data[rowIndex][fieldName]});
-        }
-        if (type === 'dropdown') {
-          field = new Dropdown({...table.data[rowIndex][fieldName]});
-        }
-        if (type === 'checkbox') {
-          field = new CheckBox({...table.data[rowIndex][fieldName]});
-        }
-        if (type === 'multichoice') {
-          field = new MultipleChoice({...table.data[rowIndex][fieldName]});
-        }
-        if (type === 'radio') {
-          table.data[rowIndex][fieldName].name += '_' + rowIndex;
-          field = new RadioButton({...table.data[rowIndex][fieldName]});
-        }
         field.on('change', (value) => {
           const rowData = JSON.parse(JSON.stringify(table.data[rowIndex]));
           rowData[fieldName].value = value;
@@ -49,20 +65,12 @@ const createTableCell = (type, fieldName) => {
       case 'label':
       case 'icon':
       case 'alert':
-        if (type === 'label') {
-          field = new Label({...table.data[rowIndex][fieldName]});
-        }
-        if (type === 'icon') {
-          field = new IconButton({...table.data[rowIndex][fieldName]});
-        }
-        if (type === 'alert') {
-          field = new Alert({...table.data[rowIndex][fieldName]});
-        }
-        // if has custom onclick call it
+        // if has custom onclick add to props
         break;
       default:
         break;
     }
+
     // return DOM
     const dom = field.render();
     return dom;
@@ -75,5 +83,4 @@ const createTableCell = (type, fieldName) => {
   };
   return new TableCell({init, update});
 };
-
 export default createTableCell;
