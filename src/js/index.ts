5d3ae31e956f36efc6a3809eb902b9fb2c1196b4
trung-doan <trung-doan@cybozu.vn>
2019-05-27 10:44:12 +0700

    v0.3.0 IconButton js component

--- src/js/index.ts
+++ src/js/index.ts
@@ -3,7 +3,7 @@ import '../legacyJS/style';
 import Alert from '../legacyJS/js/components/Alert';
 import Label from '../legacyJS/js/components/Label';
 import Button from '../legacyJS/js/components/Button';
-import IconButton from '../legacyJS/js/components/IconButton';
+import IconButton from '../js/IconButton';
 import Dropdown from '../legacyJS/js/components/Dropdown';
 import NotifyPopup from '../legacyJS/js/components/NotifyPopup';
 import Text from '../legacyJS/js/components/Text';
