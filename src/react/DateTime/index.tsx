e4c51c62f36137a8c1a687d501f29826823f832d
Nhat Nguyen <phongnhat19@gmail.com>
2019-05-27 13:09:49 +0700

    temp commit

--- src/react/DateTime/index.tsx
+++ src/react/DateTime/index.tsx
@@ -1,4 +1,4 @@
-import "../../css/DateTime.css";
+import "../css/DateTime.css";
 import React, { useState, useEffect, createRef } from "react";
 import {en} from './components/Locale'
 import {format} from './components/Locale'
@@ -8,12 +8,12 @@ import Calendar from './components/Calendar'
 import TimePicker from "./components/TimePicker";
 
 type DateTimeConstructorParameters = {
-  date: Date 
-  onChange: Function
-  locale: Locale
-  dateFormat: string
-  mode: string
-  timeFormat: string
+	date: Date 
+	onChange?: Function
+	locale?: Locale
+	dateFormat?: string
+	mode?: string
+	timeFormat?: string
 }
 
 const DateTime = ({date, onChange=(date: Date)=> {} ,locale = en, dateFormat="MM/dd/YYYY", mode="date", timeFormat="HH:mm"}:DateTimeConstructorParameters) => {
@@ -21,6 +21,8 @@ const DateTime = ({date, onChange=(date: Date)=> {} ,locale = en, dateFormat="MM
 	const [dateError, setDateError] = useState("")
 	const [timePickerDisplay, setTimePickerDisplay] = useState("none")
 	const [timeError, setTimeError] = useState("")
+	const [inputValue, setInputValue] = useState("")
+	const [timeValue, setTimeValue] = useState("")
 	const wrapperRef: React.RefObject<HTMLDivElement> = createRef<HTMLDivElement>();
 
 	useEffect(()=>{
@@ -43,7 +45,7 @@ const DateTime = ({date, onChange=(date: Date)=> {} ,locale = en, dateFormat="MM
 	return (
 		<div className="date-time-container" ref={wrapperRef}>
 			<div className="date-container">
-				<div className="text-input-container" key={format(date, dateFormat)}>
+				<div className="text-input-container" key={`${format(date, dateFormat)}-${dateError}`}>
 					<input
 						type="text"
 						className="text-input"
@@ -51,7 +53,7 @@ const DateTime = ({date, onChange=(date: Date)=> {} ,locale = en, dateFormat="MM
 							setPickerDisplay("block")
 							setTimePickerDisplay("none")
 						}}
-						defaultValue={date ? format(date, dateFormat) : ""}
+						defaultValue={date && !dateError ? format(date, dateFormat) : inputValue}
 						onBlur={
 							(e)=>{
 								if (e.relatedTarget == null 
@@ -64,6 +66,7 @@ const DateTime = ({date, onChange=(date: Date)=> {} ,locale = en, dateFormat="MM
 									setDateError("")
 									let tempDate = parseStringToDate(e.target.value)
 									if(!e.target.value) {
+										onChange(null)
 										setPickerDisplay("none")
 									} 
 									else if (tempDate instanceof Date && !isNaN(tempDate as any)) {
@@ -73,6 +76,7 @@ const DateTime = ({date, onChange=(date: Date)=> {} ,locale = en, dateFormat="MM
 										setPickerDisplay("none")
 									}
 									else if (e.target.value) {
+										setInputValue(e.target.value)
 										setDateError("Invalid date")
 										setPickerDisplay("none")
 									}
@@ -111,6 +115,7 @@ const DateTime = ({date, onChange=(date: Date)=> {} ,locale = en, dateFormat="MM
 							}
 							else {
 								onChange(null)
+								setInputValue("")
 							}
 							setPickerDisplay("none")
 						}
@@ -118,17 +123,17 @@ const DateTime = ({date, onChange=(date: Date)=> {} ,locale = en, dateFormat="MM
 				/>
 			</div>
 			{
-				mode==='datetime' && 
+				(mode==='datetime' || mode==='time') && 
 				<div className="time-container">
 					<input
 						type="text"
-						key={format(date, dateFormat)}
+						key={`${format(date, timeFormat)}-${timeError}`}
 						className="text-input"
 						onFocus={() => {
 							setTimePickerDisplay("flex")
 							setPickerDisplay("none")
 						}}
-						defaultValue={date ? format(date, timeFormat) : ""}
+						defaultValue={date && !timeError ? format(date, timeFormat) : timeValue}
 						onBlur={
 							(e)=>{
 								if (e.relatedTarget === null || !e.relatedTarget['classList'].contains('kuc-time-list-item')) {
@@ -141,6 +146,7 @@ const DateTime = ({date, onChange=(date: Date)=> {} ,locale = en, dateFormat="MM
 										onChange(returnDate)
 									}
 									else if (e.target.value) {
+										setTimeValue(e.target.value)
 										setTimeError("Invalid time")
 										setTimePickerDisplay("none")
 									}
@@ -151,6 +157,7 @@ const DateTime = ({date, onChange=(date: Date)=> {} ,locale = en, dateFormat="MM
 							(e) => {
 								if (e.key === 'Tab') {
 									setTimePickerDisplay("none")
+									return
 								}
 							}
 						}
@@ -170,6 +177,7 @@ const DateTime = ({date, onChange=(date: Date)=> {} ,locale = en, dateFormat="MM
 								let tempDate = new Date()
 								if (date) tempDate = new Date(date)
 								tempDate.setHours(timePickerDate.getHours(), timePickerDate.getMinutes())
+								setTimeValue(format(date, timeFormat))
 								onChange(tempDate)
 								setTimePickerDisplay("none")
 							}
