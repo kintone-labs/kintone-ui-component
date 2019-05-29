33c4329c8a1ec6bef16dbd53a6c8fdf8f6b37220
Nhat Nguyen <phongnhat19@gmail.com>
2019-05-29 10:59:05 +0700

    update builder for Window

--- scripts/builder.js
+++ scripts/builder.js
@@ -1,17 +1,18 @@
 const childProcess = require('child_process')
 const os = require('os');
+const path = require('path')
 
 console.log('Transpilling typescript...')
 
-childProcess.spawnSync('./node_modules/typescript/bin/tsc',{stdio: 'inherit'})
-
 if (os.type() === 'Linux' || os.type() === 'Darwin') {
+    childProcess.spawnSync('./node_modules/typescript/bin/tsc',{stdio: 'inherit'})
     childProcess.spawnSync('mkdir',['-p','dist/esm/css'],{stdio: 'inherit'})
     childProcess.spawnSync('cp',['-a','src/css/.','dist/esm/css/'], {stdio: 'inherit'})
 }
 else if (os.type() === 'Windows_NT') {
-    childProcess.spawnSync('mkdir',['"dist/esm/css"'],{stdio: 'inherit'})
-    childProcess.spawnSync('xcopy',['"src/css"','"dist/esm/css/"', '/O', '/X', '/E', '/H', '/K'],{stdio: 'inherit'})
+    childProcess.spawnSync('npm.cmd',['run','transpile'],{stdio: 'inherit'})
+    childProcess.spawnSync('mkdir',['-p','dist/esm/css'],{stdio: 'inherit'})
+    childProcess.spawnSync('xcopy',[path.resolve('./src/css/'), path.resolve('./dist/esm/css/'), '/O', '/X', '/E', '/H', '/K', '/Y'],{stdio: 'inherit'})
 }
 else
     throw new Error("Unsupported OS found: " + os.type());
