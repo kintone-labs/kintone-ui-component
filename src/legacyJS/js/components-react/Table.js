97e533acd43fa9da196237e20cafe878e46ff754
trung-doan <trung-doan@cybozu.vn>
2019-05-29 15:04:35 +0700

    Using new IconButton for table component

--- src/legacyJS/js/components-react/Table.js
+++ src/legacyJS/js/components-react/Table.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import IconButton from './IconButton';
+import IconButton from '../../../react/IconButton';
 
 import '../../../css/Table.css';
 
--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ const jsUMDConfig = {
                 }
             },
             {
-                test: /\.ts?$/,
+                test: /\.ts(x*)?$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader",
