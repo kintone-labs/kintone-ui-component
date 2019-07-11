41ae83ebe6e558e7c83e60da258e057b48d19481
yutaro-suzuki <yutaro-suzuki@cybozu.co.jp>
2019-07-11 14:20:32 +0900

    update fieldgroup layout

--- src/js/FieldGroup/index.ts
+++ src/js/FieldGroup/index.ts
@@ -30,6 +30,8 @@ class FieldGroup extends Control {
     }
     this.element = document.createElement('div');
     this.element.className = 'kuc-fieldgroup';
+    const container = document.createElement('div');
+    container.className = 'kuc-fieldgroup-container';
     
     this.fgTab = document.createElement('span');
     this.fgTab.className = this._getClassName();
@@ -50,9 +52,9 @@ class FieldGroup extends Control {
     if (this._props.content) {
       this.fgContents.appendChild(this._props.content);
     }
-
-    this.element.appendChild(this.fgTab);
-    this.element.appendChild(this.fgContents);
+    container.appendChild(this.fgTab);
+    container.appendChild(this.fgContents);
+    this.element.appendChild(container);
   }
 
   private _getClassName() {
