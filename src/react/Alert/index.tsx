5b7c996ff5e78e10cb87bd55cc1c6373f155e2f8
minhhy2801 <minhhy2897@gmail.com>
2019-09-04 09:48:10 +0700

    alert set style, className

--- src/react/Alert/index.tsx
+++ src/react/Alert/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {CSSProperties} from 'react';
 import '../../css/font.css'
 import '../../css/Alert.css';
 
 type AlertProps = {
+  style?: CSSProperties;
+  className?: string; 
   text?: string;
   type?: string;
   isDisabled?: boolean;
@@ -10,7 +12,7 @@ type AlertProps = {
   onClick?: (e: React.SyntheticEvent<EventTarget>) => void;
 }
 
-const Alert = ({text, type, isDisabled, isVisible, onClick}: AlertProps) => {
+const Alert = ({style, className, text, type, isDisabled, isVisible, onClick}: AlertProps) => {
   const _getClassName = () => {
     const className = [
       'kuc-alert',
@@ -35,7 +37,8 @@ const Alert = ({text, type, isDisabled, isVisible, onClick}: AlertProps) => {
 
   return (
     <div
-      className={_getClassName()}
+      style={style}
+      className={`${_getClassName()} ${className}`}
       onClick={_onClick}
     >
       {text}
