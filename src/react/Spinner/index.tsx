c68ed74ec9c7b5fa06c5977da35243d6fc81232c
minhhy2801 <minhhy2897@gmail.com>
2019-09-04 15:44:59 +0700

    spinner change function name

--- src/react/Spinner/index.tsx
+++ src/react/Spinner/index.tsx
@@ -2,17 +2,17 @@ import React, {CSSProperties} from 'react';
 import '../../css/Spinner.css';
 
 type SpinnerProps = {
-  style?: CSSProperties;
-  className?: string;
+  setStyles?: CSSProperties;
+  setClassName?: string;
   isVisible?: boolean;
 }
 
-const Spinner = ({isVisible, style, className}: SpinnerProps) => {
+const Spinner = ({isVisible, setStyles, setClassName}: SpinnerProps) => {
   if (isVisible === false) {
     return null;
   }
   return (
-    <div style={style} className={`kuc-spinner-outer ${className}`}>
+    <div style={setStyles} className={`kuc-spinner-outer ${setClassName}`}>
       <div className="kuc-spinner">
         <div className="kuc-loader" />
       </div>
