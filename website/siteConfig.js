e5f324e84df36e5ba7f511630f0a9ce95f625818
Nhat Nguyen <phongnhat19@gmail.com>
2019-09-18 11:30:33 +0700

    remove temp version

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -49,7 +49,7 @@ const siteConfig = {
   users,
 
   /* path to images for header/footer */
-  headerIcon: 'img/favicon.ico',
+  headerIcon: 'img/header.png',
   footerIcon: 'img/favicon.ico',
   favicon: 'img/favicon.ico',
 
