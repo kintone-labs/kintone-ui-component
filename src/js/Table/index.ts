4cf49fc8867512f4efcf66eaa52be25c9feb0c84
Pham-Gia-Huong <phamgiahuong2912@gmail.com>
2019-09-27 16:34:57 +0700

    check action exist not add to component

--- src/js/Table/index.ts
+++ src/js/Table/index.ts
@@ -322,8 +322,14 @@ export default class Table extends Control {
 
   showActionButtons() {
     this._props.actionButtonsShown = true
-    if(this._props.columns) {
-      this._props.columns.push({actions: this._props.actionButtonsShown});
+    if(this._props.columns) { 
+      let existAction = this._props.columns.filter(column => {
+        const {actions} = (column as ActionFlag)
+          return actions;
+      })[0];
+      if(!existAction){
+          this._props.columns.push({actions:this._props.actionButtonsShown});
+      }
     }
     this._renderTableRows()
   }
