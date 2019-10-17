6a68dda19906daf68f50d6ad0ad29e6114407b59
Pham-Gia-Huong <phamgiahuong2912@gmail.com>
2019-10-17 17:35:18 +0700

    check version release

--- scripts/builder.js
+++ scripts/builder.js
@@ -5,12 +5,18 @@ const path = require('path')
 console.log('Transpilling typescript...')
 
 if (os.type() === 'Linux' || os.type() === 'Darwin') {
-    childProcess.spawnSync('npm',['run','transpile'],{stdio: 'inherit'})
+    const transpile = childProcess.spawnSync('npm',['run','transpile'],{stdio: 'inherit'})
+    if(transpile.status !== 0){
+        throw "Transpile fail";
+    }  
     childProcess.spawnSync('mkdir',['-p','esm/css'],{stdio: 'inherit'})
     childProcess.spawnSync('cp',['-a','src/css/.','esm/css/'], {stdio: 'inherit'})
 }
 else if (os.type() === 'Windows_NT') {
-    childProcess.spawnSync('npm.cmd',['run','transpile'],{stdio: 'inherit'})
+    const transpile = childProcess.spawnSync('npm.cmd',['run','transpile'],{stdio: 'inherit'}); 
+    if(transpile.status !== 0){
+        throw "Transpile fail";
+    }
     childProcess.spawnSync('mkdir',['-p','esm/css'],{stdio: 'inherit'})
     childProcess.spawnSync('xcopy',[path.resolve('./src/css/'), path.resolve('./esm/css/'), '/O', '/X', '/E', '/H', '/K', '/Y'],{stdio: 'inherit'})
 }
