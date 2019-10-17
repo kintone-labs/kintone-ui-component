793a808ceb52309856097ea754e5b8809bba0070
Pham-Gia-Huong <phamgiahuong2912@gmail.com>
2019-10-17 12:49:57 +0700

    ok

---
+++ .github/build/index.js
@@ -0,0 +1,30 @@
+const package = require("../package.json");
+const packageLog = require("../package-lock.json");
+let https = require("https");
+let username = "/Pham-Gia-Huong";
+let repo = "/kintone-ui-component";
+let options = {
+  host: "api.github.com",
+  path: "/repos" + username + repo + "/releases",
+  method: "GET",
+  headers: { "user-agent": "node.js" }
+};
+
+let request = https.request(options, function(response) {
+  let body = "";
+  response.on("data", function(chunk) {
+    body += chunk.toString("utf8");
+  });
+
+  response.on("end", function() {
+    let release = JSON.parse(body);    
+    let tagVersion = release[0].tag_name;
+    let packageVersion = "v" + package.version;
+    let packageLockVersion = "v" + packageLog.version;
+    if ((tagVersion !== packageVersion) || (tagVersion !== packageLockVersion)) {
+      throw "Version is invalid";
+    }
+  });
+});
+
+request.end();
