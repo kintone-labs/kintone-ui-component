b77800f4f5122b5cf3d7ae5e17ba88751cf56694
Pham-Gia-Huong <phamgiahuong2912@gmail.com>
2019-10-17 09:57:09 +0700

    test deploy

--- src/js/Alert/index.ts
+++ src/js/Alert/index.ts
@@ -19,6 +19,8 @@ class Alert extends Control<AlertProps> {
     };
     if (params) {
       this._props = {...this._props, ...params};
+      console.log("haha");
+      
       this._props.text = (params.text && typeof params.text === "string") ? params.text : "";
     }
 
