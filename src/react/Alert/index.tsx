1a7ecbe7636e9327de07b21ce2f6039d7e1202f3
Pham-Gia-Huong <phamgiahuong2912@gmail.com>
2019-10-18 09:42:52 +0700

    test release 2

--- src/react/Alert/index.tsx
+++ src/react/Alert/index.tsx
@@ -11,7 +11,7 @@ type AlertProps = {
 }
 
 const Alert = ({text, type, isDisabled, isVisible, onClick}: AlertProps) => {
-  console.log("haha1");
+  console.log("haha2");
   
   const _getClassName = () => {
     const className = [
