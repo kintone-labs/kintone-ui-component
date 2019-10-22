6e866bb44756d649cc1f6c07097284ac65bbe88a
Pham-Gia-Huong <phamgiahuong2912@gmail.com>
2019-10-22 15:02:59 +0700

    ok

--- actions/check_version/index.js
+++ actions/check_version/index.js
@@ -1,33 +1,33 @@
 const core = require("@actions/core");
 const package = require("../../package.json");
-try {
-  let https = require("https");
-  let username = "/Pham-Gia-Huong";
-  let repo = "/kintone-ui-component";
-  let options = {
-    host: "api.github.com",
-    path: "/repos" + username + repo + "/releases",
-    method: "GET",
-    headers: { "user-agent": "node.js" }
-  };
+var querystring = require("querystring");
 
+let https = require("https");
+let username = "/Pham-Gia-Huong";
+let repo = "/kintone-ui-component";
+
+let options = {
+  host: "api.github.com",
+  path: "/repos" + username + repo + "/releases/latest",
+  method: "GET",
+  headers: { "user-agent": "node.js" }
+};
+
+try {
   let request = https.request(options, function(response) {
     let body = "";
     response.on("data", function(chunk) {
       body += chunk.toString("utf8");
     });
-
     response.on("end", function() {
+      let latestRelease = JSON.parse(body);
       let packageVersion = "v" + package.version;
-      let listRelease = JSON.parse(body).filter(release => release.tag_name.includes(packageVersion));
-      let tagVersion = listRelease && listRelease.length > 0 ? listRelease[0].tag_name : "";
-      console.log(tagVersion);
-      console.log("packageVersion",packageVersion);
-
+      let tagVersion = latestRelease.tag_name;
       if (tagVersion !== packageVersion) {
         core.setFailed("Version is invalid");
+      } else {
+        core.setOutput("version", package.version);
       }
-      core.setOutput("version", package.version);
     });
   });
   request.end();
