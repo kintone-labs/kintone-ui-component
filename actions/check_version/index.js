b86ed3179e30477641f798b933c0bde0f96c5a7c
Pham-Gia-Huong <phamgiahuong2912@gmail.com>
2019-10-23 16:41:55 +0700

    ok

--- actions/check_version/index.js
+++ actions/check_version/index.js
@@ -1,37 +1,14 @@
 const core = require("@actions/core");
 const package = require("../../package.json");
-let https = require("https");
-let username = "/Pham-Gia-Huong";
-let repo = "/kintone-ui-component";
 const github = require('@actions/github');
-const releaseTag = github.context;
-console.log(releaseTag);
-
-// let options = { 
-//   host: "api.github.com",
-//   path: "/repos" + username + repo + "/releases/latest",
-//   method: "GET",
-//   headers: { "user-agent": "node.js" }
-// };
-
-// try {
-//   let request = https.request(options, function(response) {
-//     let body = "";
-//     response.on("data", function(chunk) {
-//       body += chunk.toString("utf8");
-//     });
-//     response.on("end", function() {
-//       let latestRelease = JSON.parse(body);
-//       let packageVersion = "v" + package.version;
-//       let tagVersion = latestRelease.tag_name;
-//       if (tagVersion !== packageVersion) {
-//         core.setFailed("Version is invalid");
-//       } else {
-//         core.setOutput("version", package.version);
-//       }
-//     });
-//   });
-//   request.end();
-// } catch (e) {
-//   core.setFailed(e.message);
-// }
+try {
+  const tagVersion = github.context.payload.release.name;
+  let packageVersion = "v" + package.version;
+  if (tagVersion !== packageVersion) {
+    core.setFailed("Version is invalid");
+  } else {
+    core.setOutput("version", package.version);
+  }
+} catch (e) {
+  core.setFailed(e.message);
+}
