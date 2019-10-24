9af0e5b11f9754b4ab875d512e16a97020cb02b7
Pham-Gia-Huong <phamgiahuong2912@gmail.com>
2019-10-24 09:44:25 +0700

    config github check version package json

---
+++ actions/check_version/index.js
@@ -0,0 +1,14 @@
+const core = require("@actions/core");
+const package = require("../../package.json");
+const github = require('@actions/github');
+try {
+  const tagVersion = github.context.payload.release.name;
+  let packageVersion = "v" + package.version;
+  if (tagVersion !== packageVersion) {
+    core.setFailed("Version is invalid");
+  } else {
+    core.setOutput("version", package.version);
+  }
+} catch (e) {
+  core.setFailed(e.message);
+}
