3d8fa6556f0f040390b80b5bb169e9db8c7064b9
Pham-Gia-Huong <phamgiahuong2912@gmail.com>
2019-10-24 09:43:18 +0700

    remove dist esm folder

--- esm/js/Spinner/index.d.ts
+++
@@ -1,8 +0,0 @@
-import '../polyfill';
-import Control, { ControlProps } from '../Control';
-import '../../css/Spinner.css';
-declare class Spinner extends Control<ControlProps> {
-    constructor(params?: ControlProps);
-    private _createSpinnerElement;
-}
-export default Spinner;
