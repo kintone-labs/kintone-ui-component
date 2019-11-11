6b0e5d3e9ea896e01f49494f029d335b13044d65
Pham-Gia-Huong <phamgiahuong2912@gmail.com>
2019-11-11 13:38:55 +0700

    log

--- actions/check_version/index.js
+++ actions/check_version/index.js
@@ -3,7 +3,11 @@ const package = require("../../package.json");
 const github = require('@actions/github');
 try {
   const tagVersion = github.context.payload.release.name;
+  console.log(tagVersion);
+  
   let packageVersion = "v" + package.version;
+  console.log(packageVersion);
+
   if (tagVersion !== packageVersion) {
     core.setFailed("Version is invalid");
   } else {
