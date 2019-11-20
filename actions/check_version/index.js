14cd0d32d69ab804ece39f7dde9f466b9b6e8bee
Pham-Gia-Huong <phamgiahuong2912@gmail.com>
2019-11-20 15:23:25 +0700

    test

--- actions/check_version/index.js
+++ actions/check_version/index.js
@@ -2,12 +2,8 @@ const core = require("@actions/core");
 const package = require("../../package.json");
 const github = require('@actions/github');
 try {
-  const tagVersion = github.context.payload.release.name;
-  console.log(tagVersion);
-  
+  const tagVersion = github.context.payload.release.name;  
   let packageVersion = "v" + package.version;
-  console.log(packageVersion);
-
   if (tagVersion !== packageVersion) {
     core.setFailed("Version is invalid");
   } else {
--- actions/get_integrity/index.js
+++ actions/get_integrity/index.js
@@ -1,18 +1,21 @@
-var exec = require('child_process').exec;
+var exec = require("child_process").exec;
 const core = require("@actions/core");
-const fs = require('fs');
+const fs = require("fs");
 
-exec("npm pack --json | grep integrity", (error, stdout, stderr)=>{
-  if(stdout){
-    let integrity= stdout.replace(/['"]+/g,'').replace("integrity",'').replace(":","").replace(",","");
-    fs.writeFile("integrity.txt", integrity, (err)=> {
-      if(err) {
-          core.setFailed("write integrity fail: " +err)
+exec("npm pack --json | grep integrity", (error, stdout, stderr) => {
+  if (stdout) {
+    let integrity = stdout
+      .replace(/['"]+/g, "")
+      .replace("integrity", "")
+      .replace(":", "")
+      .replace(",", "");
+    fs.writeFile("integrity.txt", integrity, err => {
+      if (err) {
+        core.setFailed("write integrity fail: " + err);
       }
       console.log("The file was saved!");
-  }); 
-  }else{
-    core.setFailed("get integrity fail: "+ error);
+    });
+  } else {
+    core.setFailed("get integrity fail: " + error);
   }
-})
-
+});
