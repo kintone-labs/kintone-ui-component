3de5582bba97a124d33953d347a955b329fc2e6d
huong <phamgiahuong2012@gmail.com>
2019-11-22 10:40:33 +0700

    merge release

--- actions/deploy/index.js
+++ actions/deploy/index.js
@@ -1,6 +1,9 @@
 const exec = require("child_process").exec
 exec('git add .');
-exec("git commit -m 'merge release'");
+exec("git commit -m 'merge release'",(err,stdout)=>{
+  console.log(err);
+  
+});
 exec("git push origin HEAD:master --force");
 
 
