68065cde062e133c97997c6a052f0d81c7d15da4
Nhat Nguyen <phongnhat19@gmail.com>
2019-12-23 16:19:51 +0700

    commit unit test for react FieldGroup

--- src/react/FieldGroup/__test__/render.test.tsx
+++ src/react/FieldGroup/__test__/render.test.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import React from 'react';
-import {render} from '@testing-library/react';
+import {render, fireEvent} from '@testing-library/react';
 
 import FieldGroup from '../index';
 
@@ -34,20 +34,61 @@ describe('Unit test FieldGroup react', () => {
     }
   });
 
-  test('FieldGroup toggle successfully', () => {
+  test('FieldGroup show successfully', () => {
     try {
-      const {container} = render(<FieldGroup />);
+      const {container, getByRole} = render(<FieldGroup />);
       if (container.firstElementChild && container.firstElementChild.firstElementChild) {
-        const toggleSpan = container.firstElementChild.firstElementChild.firstElementChild;
-        expect(toggleSpan).toBeInstanceOf(HTMLSpanElement);
-        if (toggleSpan) {
-          // console.log(toggleSpan)
-          // toggleSpan.onclick();
-        }
+        fireEvent.click(getByRole('button'));
+        expect(getByRole('button').className).toContain('expand');
       } else {
         expect(false);
       }
     } catch (error) {
+      expect(false);
+    }
+  });
+
+  test('FieldGroup hide successfully', () => {
+    try {
+      const {container, getByRole} = render(<FieldGroup toggle="expand" />);
+      if (container.firstElementChild && container.firstElementChild.firstElementChild) {
+        fireEvent.click(getByRole('button'));
+        expect(getByRole('button').className).toContain('collapse');
+      } else {
+        expect(false);
+      }
+    } catch (error) {
+      expect(false);
+    }
+  });
+
+  test('onToggle called successfully', () => {
+    try {
+      const {container, getByRole} = render(
+        <FieldGroup
+          onToggle={(toggleState)=>{
+            expect(toggleState).toEqual('expand');
+          }}
+        />);
+      if (container.firstElementChild && container.firstElementChild.firstElementChild) {
+        fireEvent.click(getByRole('button'));
+      } else {
+        expect(false);
+      }
+    } catch (error) {
+      expect(false);
+    }
+  });
+
+  test('Render with children instead of content', () => {
+    const {container} = render(
+      <FieldGroup>
+        <span>123</span>
+      </FieldGroup>
+    );
+    if (container.firstElementChild) {
+      expect(container.firstElementChild.className).toContain('kuc-fieldgroup');
+    } else {
       expect(false);
     }
   });
