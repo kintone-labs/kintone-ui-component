529222d56c44cffac890f6f823eb179bd2cc8700
yutaro-suzuki <yutaro-suzuki@cybozu.co.jp>
2019-12-26 14:28:42 +0900

    add invisible case to snapshot test

--- src/react/Button/__test__/snapshot.test.tsx
+++ src/react/Button/__test__/snapshot.test.tsx
@@ -8,10 +8,18 @@ describe('Unit test Button react', () => {
     const {container} = render(<Button />);
     fs.writeFileSync('./src/react/Button/__test__/snapshot/without_props.html', container.outerHTML);
   });
+
   test('Render successfully with full props', () => {
     const {container} = render(
-      <Button text="Submit" type="submit" isDisabled isVisible={false} />
+      <Button text="Submit" type="submit" isDisabled isVisible />
     );
     fs.writeFileSync('./src/react/Button/__test__/snapshot/with_full_props.html', container.outerHTML);
+  });
+
+  test('Render successfully with invisible', () => {
+    const {container} = render(
+      <Button isVisible={false} />
+    );
+    fs.writeFileSync('./src/react/Button/__test__/snapshot/with_invisible.html', container.outerHTML);
   });
 });
