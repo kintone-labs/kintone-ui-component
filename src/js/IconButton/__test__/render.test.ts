c53b0eda8a32c2450cdc1edd82d82b96ed349deb
miyass <exe.miyakura0629@gmail.com>
2019-12-27 09:43:02 +0900

    prepare icon button test

---
+++ src/js/IconButton/__test__/render.test.ts
@@ -0,0 +1,17 @@
+// import IconButton from '../index';
+import '@testing-library/jest-dom/extend-expect';
+
+describe('Unit test IconButton render', () => {
+
+  test('Render successfully without props', () => {
+
+  });
+
+  test('Render successfully with full props', () => {
+
+  });
+
+  test('Render successfully with wrong props', () => {
+
+  });
+});
