12e8175bec6d2e919a24431f8d85c70ead45d2e5
miyass <exe.miyakura0629@gmail.com>
2019-12-27 13:58:28 +0900

    fix setcolor test; more strict

--- src/js/IconButton/__test__/setColor.test.ts
+++ src/js/IconButton/__test__/setColor.test.ts
@@ -2,11 +2,44 @@ import IconButton from '../index';
 
 describe('Unit test IconButton setColor', () => {
 
-  test('Function setColor run successfully', () => {
+  test('Function setColor red run successfully', () => {
     const iconButton = new IconButton({});
     const container = iconButton.render();
     iconButton.setColor('red');
+    expect(window.getComputedStyle(container).backgroundColor).toBe('rgb(228, 66, 52)');
     expect(container.className).toContain('red');
+  });
+
+  test('Function setColor green run successfully', () => {
+    const iconButton = new IconButton({});
+    const container = iconButton.render();
+    iconButton.setColor('green');
+    expect(window.getComputedStyle(container).backgroundColor).toBe('rgb(134, 187, 97)');
+    expect(container.className).toContain('green');
+  });
+
+  test('Function setColor blue run successfully', () => {
+    const iconButton = new IconButton({});
+    const container = iconButton.render();
+    iconButton.setColor('blue');
+    expect(window.getComputedStyle(container).backgroundColor).toBe('rgb(45, 141, 214)');
+    expect(container.className).toContain('blue');
+  });
+
+  test('Function setColor gray run successfully', () => {
+    const iconButton = new IconButton({});
+    const container = iconButton.render();
+    iconButton.setColor('gray');
+    expect(window.getComputedStyle(container).backgroundColor).toBe('rgb(238, 238, 238)');
+    expect(container.className).toContain('gray');
+  });
+
+  test('Function setColor transparent run successfully', () => {
+    const iconButton = new IconButton({});
+    const container = iconButton.render();
+    iconButton.setColor('transparent');
+    expect(window.getComputedStyle(container).backgroundColor).toBe('transparent');
+    expect(container.className).toContain('transparent');
   });
 
   test('Function setText run successfully with empty', () => {
