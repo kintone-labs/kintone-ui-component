7d77c89521e076bfc81fb1d5e2ba5381d15eef1a
Nhat Nguyen <phongnhat19@gmail.com>
2020-01-16 14:39:54 +0700

    verify DOM for FieldGroup render

--- src/js/FieldGroup/__test__/render.test.ts
+++ src/js/FieldGroup/__test__/render.test.ts
@@ -23,8 +23,17 @@ describe('Unit test FieldGroup render', () => {
   });
 
   test('Init successfully with name props', () => {
-    const myFieldGroup = new FieldGroup({name: 'FieldGroup name'});
-    expect(myFieldGroup.getName()).toEqual('FieldGroup name');
+    const NAME = 'FieldGroup name';
+    const myFieldGroup = new FieldGroup({name: NAME});
+    expect(myFieldGroup.getName()).toEqual(NAME);
+
+    // Verify name DOM
+    const container = myFieldGroup.render();
+    const fgTab = container.getElementsByClassName('kuc-fieldgroup-label');
+    expect(fgTab.length).toEqual(1);
+    const fgTabLabel = fgTab[0].lastElementChild;
+    expect(fgTabLabel).toBeInstanceOf(HTMLSpanElement);
+    expect((fgTabLabel! as HTMLSpanElement).innerText).toEqual(NAME);
   });
 
   test('Init successfully with content props', () => {
@@ -32,10 +41,24 @@ describe('Unit test FieldGroup render', () => {
     contentDiv.innerHTML = 'content';
     const myFieldGroup = new FieldGroup({content: contentDiv});
     expect(myFieldGroup.getContent()).toBe(contentDiv);
+
+    // Verify content DOM
+    const container = myFieldGroup.render();
+    const fgContent = container.getElementsByClassName('kuc-fieldgroup-contents');
+    expect(fgContent.length).toEqual(1);
+    expect(fgContent[0].firstElementChild).toBe(contentDiv);
   });
 
   test('Init successfully with toggle "expand"', () => {
     const myFieldGroup = new FieldGroup({toggle: 'expand'});
     expect(myFieldGroup.getToggle()).toEqual('expand');
+
+    // Verify toggle DOM
+    const container = myFieldGroup.render();
+    const fgTab = container.getElementsByClassName('kuc-fieldgroup-label');
+    expect(fgTab.length).toEqual(1);
+    expect(fgTab[0].classList).toContain('expand');
+    const fgTabArrow = fgTab[0].firstElementChild;
+    expect(fgTabArrow!.classList).toContain('down');
   });
 });
