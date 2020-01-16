4e7175a137b3db1d59a7653e5ceb486ac5b2932e
huong <phamgiahuong2012@gmail.com>
2020-01-16 16:42:03 +0700

    ok

--- actions/test_repo_dispatch/index.js
+++ actions/test_repo_dispatch/index.js
@@ -1,7 +1,8 @@
 const core = require("@actions/core");
 const github = require('@actions/github');
 try {
-  console.log(github.event);
+  console.log(github);
+  console.log(github.context);
 
   if (github.event && github.event.client_payload){
     console.log(github.event.client_payload);
