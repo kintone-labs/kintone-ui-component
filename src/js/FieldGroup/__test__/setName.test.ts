3fa805674efcb8a89ebab0b93bbd14c0b807476f
Nhat Nguyen <phongnhat19@gmail.com>
2020-01-16 14:26:43 +0700

    verify DOM for FieldGroup setName

--- src/js/FieldGroup/__test__/setName.test.ts
+++ src/js/FieldGroup/__test__/setName.test.ts
@@ -15,8 +15,16 @@ describe('Unit test FieldGroup setName', () => {
   test('setName is called successfully', () => {
 
     const myFieldGroup = new FieldGroup({name: 'name'});
+    const NEW_NAME = 'new name';
+    myFieldGroup.setName(NEW_NAME);
+    expect(myFieldGroup.getName()).toBe(NEW_NAME);
 
-    myFieldGroup.setName('new name');
-    expect(myFieldGroup.getName()).toBe('new name');
+    // Verify name DOM
+    const container = myFieldGroup.render();
+    const fgTab = container.getElementsByClassName('kuc-fieldgroup-label');
+    expect(fgTab.length).toEqual(1);
+    const fgTabLabel = fgTab[0].lastElementChild;
+    expect(fgTabLabel).toBeInstanceOf(HTMLSpanElement);
+    expect((fgTabLabel! as HTMLSpanElement).innerText).toEqual(NEW_NAME);
   });
 });
