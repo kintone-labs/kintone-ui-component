e763dbf3f7cf04a7ce162fae7222a10277371765
Nhat Nguyen <phongnhat19@gmail.com>
2020-01-16 14:26:53 +0700

    verify DOM for FieldGroup setToggle

--- src/js/FieldGroup/__test__/setToggle.test.ts
+++ src/js/FieldGroup/__test__/setToggle.test.ts
@@ -18,5 +18,13 @@ describe('Unit test FieldGroup setToggle', () => {
 
     myFieldGroup.setToggle('collapse');
     expect(myFieldGroup.getToggle()).toBe('collapse');
+
+    // Verify toggle DOM
+    const container = myFieldGroup.render();
+    const fgTab = container.getElementsByClassName('kuc-fieldgroup-label');
+    expect(fgTab.length).toEqual(1);
+    expect(fgTab[0].classList).toContain('collapse');
+    const fgTabArrow = fgTab[0].firstElementChild;
+    expect(fgTabArrow!.classList).toContain('right');
   });
 });
