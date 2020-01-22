3868bee88673987d9bf33934b8f9c894f5af44fb
shota-fujiwara <shota-fujiwara@pc-000492-1.local>
2020-01-22 12:59:49 +0900

    Add unit test of enable, disable, hide, show

--- src/js/CheckBox/__test__/enable.test.ts
+++ src/js/CheckBox/__test__/enable.test.ts
@@ -1,10 +1,41 @@
+import '@testing-library/jest-dom/extend-expect';
 import CheckBox from '../index';
 
 describe('Unit test CheckBox enable', () => {
   test('Function enable run successfully', () => {
-    //     const button = new Button({isDisabled: false});
-    //     const container = button.render();
-    //     button.enable();
-    //     expect(container).not.toBeDisabled();
+    const expectedLabels = ['Orange', 'Banana', 'Lemon'];
+    const expectedValues = ['orange', 'banana', 'lemon'];
+      const checkBox = new CheckBox({
+        items: [
+          {
+            label: expectedLabels[0],
+            value: expectedValues[0],
+          },
+          {
+            label: expectedLabels[1],
+            value: expectedValues[1],
+          },
+          {
+            label: expectedLabels[2],
+            value: expectedValues[2],
+          }
+        ],
+        isDisabled: true
+      });
+      const container = checkBox.render();
+      checkBox.enable();
+      if (!container.children || container.children.length !== 3) {
+        expect(false);
+      }
+      const items = container.children;
+      for (let index = 0; index < 3; index++) {
+        const item: Element = items[index];
+        if (!item.children || item.children.length !== 2) {
+          expect(false);
+        }
+        const inputEl = (item.children[0] as HTMLInputElement);  
+        // check input & label elements
+        expect(inputEl).not.toBeDisabled();
+      };
   });
 });
--- src/js/CheckBox/__test__/show.test.ts
+++ src/js/CheckBox/__test__/show.test.ts
@@ -2,9 +2,27 @@ import CheckBox from '../index';
 
 describe('Unit test CheckBox show', () => {
   test('Function show run successfully', () => {
-    //     const button = new Button({isVisible: false});
-    //     const container = button.render();
-    //     button.show();
-    //     expect(container).toBeVisible();
+    const expectedLabels = ['Orange', 'Banana', 'Lemon'];
+    const expectedValues = ['orange', 'banana', 'lemon'];
+      const checkBox = new CheckBox({
+        items: [
+          {
+            label: expectedLabels[0],
+            value: expectedValues[0],
+          },
+          {
+            label: expectedLabels[1],
+            value: expectedValues[1],
+          },
+          {
+            label: expectedLabels[2],
+            value: expectedValues[2],
+          }
+    ],
+        isVisible: false
+      });
+      const container = checkBox.render();
+      checkBox.show();
+      expect(container).toBeVisible();
   });
 });
