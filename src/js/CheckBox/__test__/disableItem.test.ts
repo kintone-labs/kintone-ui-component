9a9aff09b6dad44f33b992d7b4ec92e636c29e10
yutaro-suzuki <yutaro-suzuki@cybozu.co.jp>
2020-01-29 09:41:44 +0900

    code format

--- src/js/CheckBox/__test__/disableItem.test.ts
+++ src/js/CheckBox/__test__/disableItem.test.ts
@@ -17,7 +17,7 @@ describe('Unit test CheckBox disableItem', () => {
     checkBox.disableItem(expectedValues[0]);
     const items = container.children;
     const item: Element = items[0];
-    const inputEl = (item.children[0] as HTMLInputElement);
+    const inputEl = item.children[0] as HTMLInputElement;
     expect(inputEl).toBeDisabled();
   });
 
