48f929d4c2138720c23fb21d88223a498c6602f5
Ho Kim Cuc <c001106@PC-001106-4.local>
2020-02-11 13:45:57 +0700

    updatr onEvent.test.tsx

--- src/react/DateTime/__test__/onEvent.test.tsx
+++ src/react/DateTime/__test__/onEvent.test.tsx
@@ -16,48 +16,44 @@ describe('Unit test DateTime react', () => {
     console.error.mockRestore();
   });
 
-  test('onChange with full props DateTime', () => {
-    const onChange = (value: Date) => {
-      expect(true);
-    };
-    const {container} = render(
+  test('should render successfully with full props DateTime', () => {
+    const onChange = jest.fn((value: Date) => {
+      expect(value.getDate()).toBe(26);
+    });
+    const {getByText} = render(
       <DateTime
         value={new Date('16:24 06/01/2020')}
         type="datetime"
         locale="zh"
         dateFormat="dd/MM/YYYY"
         timeFormat="HH:mm"
-        isDisabled
+        isDisabled={false}
         isVisible
         onChange={onChange}
       />
     );
-    const node = container.getElementsByClassName('kuc-input-text text-input')[0] as HTMLInputElement;
-    fireEvent.change(node);
-    expect(node.value).toBe('01/06/2020');
+    fireEvent.click(getByText('26'));
+    expect(onChange).toBeCalledTimes(1);
   });
-  test('onEvent dateTextInput DateTime', () => {
-    const onChange = (value: Date) => {
-      expect(true);
-    };
+  test('should show Date picker when the DateTime input is focus', () => {
     const {container} = render(
       <DateTime
         value={new Date()}
         type="date"
         dateFormat="dd/MM/YYYY"
-        onChange={onChange}
       />
     );
-    const node = container.getElementsByClassName('kuc-input-text text-input')[0];
-    fireEvent.click(node, {target: {value: null}});
-    fireEvent.blur(node, {target: {value: null}});
-    fireEvent.blur(node, {target: {value: '02/06/2020'}});
-    fireEvent.change(node, {target: {value: '02/06/2020'}});
-    fireEvent.focus(node, {target: {value: null}});
-    fireEvent.click(node, {target: {value: null}});
-    fireEvent.blur(node, {target: {value: 'kintone'}});
+
+    const datetimeInput = container.getElementsByClassName('kuc-input-text text-input')[0];
+    const piker = container.getElementsByClassName('date-picker-container')[0];
+    expect(piker).toHaveStyle('display: none;');
+    fireEvent.focus(datetimeInput, {target: {value: null}});
+    expect(piker).toHaveStyle('display: block;');
+
+    fireEvent.keyDown(datetimeInput, {key: 'Tab', code: 9});
+    expect(piker).toHaveStyle('display: none;');
   });
-  test('onEvent datetimeTextInput DateTime', () => {
+  test('should show Time picker when the Time input is focus', () => {
     const onChange = (value: Date) => {
       expect(true);
     };
@@ -71,48 +67,52 @@ describe('Unit test DateTime react', () => {
       />
     );
     const node = container.getElementsByClassName('kuc-input-text text-input time')[0];
-    fireEvent.click(node, {target: {selectionStart: 2}});
-    fireEvent.change(node, {target: {value: '13:10'}});
-    fireEvent.change(node, {target: {selectionStart: 4}});
-    fireEvent.focus(node, {target: {value: null}});
-    fireEvent.click(node, {target: {value: null}});
-    fireEvent.blur(node, {target: {value: 'kintone'}});
-    fireEvent.change(node, {target: {value: 'kintone', selectionStart: 2}});
-    fireEvent.change(node, {target: {selectionStart: 5, value: '13:10'}});
-    fireEvent.change(node, {target: {selectionStart: 3, value: '19:30'}});
-    fireEvent.change(node, {target: {selectionStart: 4, value: '4:40'}});
-    fireEvent.change(node, {target: {selectionStart: 2, value: '10:20'}});
-    fireEvent.change(node, {target: {selectionStart: 1, value: '-33:30'}});
-    fireEvent.keyDown(node);
+
+    const piker = container.getElementsByClassName('time-picker-container')[0];
+    expect(piker).toHaveStyle('display: none;');
+    fireEvent.focus(node, {target: {selectionStart: 0}});
+
+    expect(piker).toHaveStyle('display: flex;');
   });
 
   test('onKeyDown dateTextInput DateTime', () => {
-    const onChange = (value: Date) => {
-      expect(true);
-    };
     const {container} = render(
       <DateTime
         value={new Date()}
-        type="date"
+        type="datetime"
         dateFormat="dd/MM/YYYY"
-        onChange={onChange}
+        onChange={() => {}}
       />
     );
+
+    const piker = container.getElementsByClassName('date-picker-container')[0];
     const node = container.getElementsByClassName('kuc-input-text text-input')[0];
-    fireEvent.click(node, {target: {value: '02/06/2020'}});
-    fireEvent.keyDown(node, {key: 'Tab'});
-    expect(true).toBeTruthy();
+    fireEvent.focus(node);
+    expect(piker).toHaveStyle('display: block;');
+    fireEvent.keyDown(node, {key: 'Tab', code: 9});
+
+    expect(piker).toHaveStyle('display: none;');
   });
 
   test('onEvent date-picker-container DateTime', () => {
-    const onChange = (value: Date) => {
-      expect(true);
-    };
+    const today = new Date();
+    today.setHours(0);
+    today.setSeconds(0);
+    today.setMinutes(0);
+    today.setMilliseconds(0);
+
+    const noneDate = new Date('02/02/2020');
+    noneDate.setHours(0);
+    noneDate.setSeconds(0);
+    noneDate.setMinutes(0);
+    noneDate.setMilliseconds(0);
+
+    const onChange = jest.fn(() => {});
     const {container} = render(
       <DateTime
         isVisible
         isDisabled={false}
-        value={new Date()}
+        value={new Date('02/02/2020')}
         type="datetime"
         dateFormat="dd/MM/YYYY"
         timeFormat="HH:mm"
@@ -120,14 +120,18 @@ describe('Unit test DateTime react', () => {
       />
     );
     const calendarTodayBtn = container.getElementsByClassName('today calendar-button-control')[0];
-    const calendarPreviousBtn = container.getElementsByClassName('none calendar-button-control')[0];
-    fireEvent.click(calendarTodayBtn, {target: {calendarDate: '03/02/2020'}});
-    fireEvent.click(calendarPreviousBtn, {target: {previousDate: '01/02/2020'}});
+    fireEvent.click(calendarTodayBtn);
+    expect(onChange).toHaveBeenNthCalledWith(1, today);
+
+    const noneBtn = container.getElementsByClassName('none calendar-button-control')[0];
+    fireEvent.keyUp(noneBtn);
+    expect(onChange).toHaveBeenNthCalledWith(2, noneDate);
   });
-  test('onEvent date-time-picker-container DateTime', () => {
-    const onChange = (value: Date) => {
-      expect(true);
-    };
+  test('Should change date successfully when clicking into the time picker', () => {
+    const onChange = jest.fn((date: Date) => {
+      expect(date.getHours()).toEqual(0);
+      expect(date.getMinutes()).toEqual(30);
+    });
     const {container} = render(
       <DateTime
         isVisible
@@ -139,33 +143,125 @@ describe('Unit test DateTime react', () => {
         onChange={onChange}
       />
     );
-    const timePicker = container.getElementsByClassName('kuc-time-list-item')[0];
-    fireEvent.click(timePicker, {target: {timePickerDate: '03:23 03/02/2020'}});
+    const timePicker = container.getElementsByClassName('kuc-time-list-item')[1];
+    fireEvent.click(timePicker);
+    expect(onChange).toHaveBeenCalledTimes(1);
   });
-  test('timeInputKeydownHandler date-time-picker-container DateTime', () => {
-    const onChange = (value: Date) => {
-      expect(true);
-    };
+
+  test('should selected range successfully inside the Time input when pressing Tab button', (done) => {
+    const mockFn = spyOn(HTMLInputElement.prototype, 'setSelectionRange');
     const {container} = render(
       <DateTime
-        isVisible
-        isDisabled={false}
         value={new Date()}
+        type="datetime"
+        dateFormat="dd/MM/YYYY"
+        timeFormat="HH:mm"
+        onChange={() => {}}
+      />
+    );
+    const node = container.getElementsByClassName('kuc-input-text text-input time')[0] as HTMLInputElement;
+    fireEvent.focus(node);
+    setTimeout(()=>{
+      expect(mockFn).toBeCalledWith(0, 2);
+      done();
+    }, 1);
+    fireEvent.keyDown(node, {key: 'Tab', code: 9});
+    expect(mockFn).toBeCalledWith(3, 5);
+  });
+
+  test('should reset hour:minutes to 00:00 when typing invalid value into Time input', () => {
+    const invlaidvalue = 'kintone';
+    const onChange = jest.fn(()=> {});
+
+    const today = new Date();
+    today.setHours(4);
+    today.setSeconds(0);
+    today.setMinutes(0);
+    today.setMilliseconds(0);
+
+    const {container} = render(
+      <DateTime
+        value={today}
         type="datetime"
         dateFormat="dd/MM/YYYY"
         timeFormat="HH:mm"
         onChange={onChange}
       />
     );
-    const node = container.getElementsByClassName('kuc-input-text text-input time')[0];
+    const node = container.getElementsByClassName('kuc-input-text text-input time')[0] as HTMLInputElement;
+    fireEvent.focus(node);
+    fireEvent.change(node, {target: {value: invlaidvalue}});
+    expect(node).toHaveValue('00:00');
+
+    fireEvent.keyDown(node, {key: 'Tab', code: 9});
+    fireEvent.change(node, {target: {value: 20}});
+    fireEvent.change(node, {target: {value: invlaidvalue}});
+    expect(node).toHaveValue('00:00');
+  });
+
+  test('should selected range successfully inside the Time input when pressing Arrow Right/Left button', () => {
+    const mockFn = spyOn(HTMLInputElement.prototype, 'setSelectionRange');
+
+    const {container} = render(
+      <DateTime
+        value={new Date()}
+        type="datetime"
+        dateFormat="dd/MM/YYYY"
+        timeFormat="HH:mm"
+        onChange={() => {}}
+      />
+    );
+    const node = container.getElementsByClassName('kuc-input-text text-input time')[0] as HTMLInputElement;
+    fireEvent.focus(node);
+    fireEvent.keyDown(node, {key: 'ArrowRight'});
+    expect(mockFn).toHaveBeenNthCalledWith(1, 3, 5);
+
+    fireEvent.keyDown(node, {key: 'ArrowLeft'});
+    expect(mockFn).toHaveBeenNthCalledWith(2, 0, 2);
+  });
+  test('should change time successfully when pressing Arrow Up/Down button', () => {
+    const date = new Date();
+    date.setHours(4);
+    date.setSeconds(0);
+    date.setMinutes(0);
+    date.setMilliseconds(0);
+    
+    const updateHourUp = new Date();
+    updateHourUp.setHours(5);
+    updateHourUp.setSeconds(0);
+    updateHourUp.setMinutes(0);
+    updateHourUp.setMilliseconds(0);
+
+    const updateTimeUp = new Date();
+    updateTimeUp.setHours(4);
+    updateTimeUp.setSeconds(0);
+    updateTimeUp.setMinutes(1);
+    updateTimeUp.setMilliseconds(0);
+
+    const onchange = jest.fn((value: Date) => {console.log(value, value.getHours(), value.getMinutes());
+    });
+
+    const {container} = render(
+      <DateTime
+        value={date}
+        type="datetime"
+        dateFormat="dd/MM/YYYY"
+        timeFormat="HH:mm"
+        onChange={onchange}
+      />
+    );
+    const node = container.getElementsByClassName('kuc-input-text text-input time')[0] as HTMLInputElement;
+    fireEvent.focus(node);
+    fireEvent.keyDown(node, {key: 'ArrowUp'});
+    expect(onchange).toHaveBeenNthCalledWith(1, updateHourUp);
+    fireEvent.keyDown(node, {key: 'ArrowDown'});
+    expect(onchange).toHaveBeenNthCalledWith(2, date);
+
     fireEvent.keyDown(node, {key: 'Tab'});
-    fireEvent.keyDown(node, {key: 'kintone'});
-    fireEvent.keyDown(node, {key: 'ArrowLeft', target: {selectionStart: 1, selectionEnd: 4}});
-    fireEvent.keyDown(node, {key: 'ArrowRight', target: {selectionStart: 1, selectionEnd: 4}});
-    fireEvent.keyDown(node, {key: 'ArrowUp', target: {selectionStart: 1, selectionEnd: 4}});
-    fireEvent.keyDown(node, {key: 'ArrowUp', target: {selectionStart: 2, selectionEnd: 4}});
-    fireEvent.keyDown(node, {key: 'ArrowDown', target: {selectionStart: 2, selectionEnd: 4}});
-    fireEvent.keyDown(node, {key: 'ArrowDown', target: {selectionStart: 1, selectionEnd: 4}});
+    fireEvent.keyDown(node, {key: 'ArrowUp'});
+    expect(onchange).toHaveBeenNthCalledWith(3, updateTimeUp);
+    fireEvent.keyDown(node, {key: 'ArrowDown'});
+    expect(onchange).toHaveBeenNthCalledWith(4, date);
   });
   test('should be show error when the dateFormat and timeFormat props is invalid', () => {
     const {getByText} = render(
