64be7b85016cd891feab8742499537e9363c85c4
Nhat Nguyen <phongnhat19@gmail.com>
2020-03-11 15:57:15 +0700

    remove unneccessary test case

--- src/js/Attachment/__test__/removeFile.test.ts
+++ src/js/Attachment/__test__/removeFile.test.ts
@@ -13,28 +13,6 @@ describe('Unit test Attachment react removeFile', () => {
     console.error.mockRestore();
   });
 
-  test('removeFile is called successfully', () => {
-    const file = new File([''], 'file1.png', {
-      type: 'image/png',
-    });
-    const handleFileRemove = jest.fn((files: any) => {
-      expect(files).toBeInstanceOf(Array);
-      expect(files.length).toEqual(0);
-    });
-    const myAttachment = new Attachment({
-      files: [file],
-      onFileRemove: handleFileRemove
-    });
-    const container = myAttachment.render();
-    const removeButton = container.getElementsByClassName('kuc-attachment_file_action')[0].getElementsByTagName('button');
-    try {
-      fireEvent.click(removeButton[0]);
-      expect(handleFileRemove.mock.calls.length).toBe(1);
-    } catch (error) {
-      expect(error).toBeFalsy();
-    }
-  });
-
   test('removeFile is called when there\'s no onFileRemove', () => {
     const file = new File([''], 'file1.png', {
       type: 'image/png',
