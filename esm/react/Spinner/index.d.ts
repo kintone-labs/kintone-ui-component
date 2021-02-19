0439f347caa9f4725fb4a2243dc7f1a94fe46e70
TomokoMiyake <t.smile297327@gmail.com>
2021-02-19 15:30:15 +0900

    Delete dist and esm folders

--- esm/react/Spinner/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-import '../../css/Spinner.css';
-declare type SpinnerProps = {
-    isVisible?: boolean;
-};
-declare const Spinner: ({ isVisible }: SpinnerProps) => JSX.Element | null;
-export default Spinner;
