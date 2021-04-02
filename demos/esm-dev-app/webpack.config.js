87966bf4cd7d80ed818d982ec025a7aebffbe480
Nguyen Hoang Anh <nmanhnit@gmail.com>
2021-04-02 14:00:55 +0700

    For polyfills that work with the Custom Elements at end-user

--- demos/esm-dev-app/webpack.config.js
+++ demos/esm-dev-app/webpack.config.js
@@ -4,9 +4,46 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve("dist"),
-    filename: "main.js",
+    filename: "main.js"
   },
   resolve: {
-    extensions: [".js"],
+    extensions: [".js"]
   },
+  target: ["web", "es5"],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules\/(?!(lit-element|lit-html)\/).*/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  modules: false,
+                  targets: {
+                    ie: 11
+                  }
+                }
+              ]
+            ],
+            plugins: [
+              [
+                "@babel/plugin-transform-runtime",
+                {
+                  regenerator: true
+                }
+              ]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+      }
+    ]
+  }
 };
