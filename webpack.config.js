879a5e8e6516cc8f56fe8f4de93d3bfbb457f33c
Nguyen Hoang Anh <nmanhnit@gmail.com>
2021-04-13 10:12:10 +0700

    update version webcomponents and entry in webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   entry: {
-    "kuc.polyfill.min": "./src/polyfill/index.ts",
+    "polyfill.min": "./src/polyfill/index.ts",
     "kuc.min": "./src/index.bundle.ts"
   },
   output: {
