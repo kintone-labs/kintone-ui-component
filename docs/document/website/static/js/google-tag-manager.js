10366c0631c94c4dbe28b1ddc880ce66d84ce6f9
phongnm-dev <p001842@PC-001842-1.local>
2021-06-14 13:35:54 +0700

    Update google tag manager script

---
+++ docs/document/website/static/js/google-tag-manager.js
@@ -0,0 +1,17 @@
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id=' i dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5N49D3');
+
+(function() {
+  var noscript = document.createElement('noscript');
+  var iframe = document.createElement('iframe');
+  iframe.setAttribute('height', "0")
+  iframe.setAttribute('width', "0")
+  iframe.src = "https://www.googletagmanager.com/ns.html?id=GTM-5N49D3";
+  iframe.style.display = "none";
+  iframe.style.visibility = "hidden";
+  noscript.appendChild(iframe);
+  document.body.prepend(noscript);
+  })();
