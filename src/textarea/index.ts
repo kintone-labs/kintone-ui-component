1bf24489e9f048df12f20da47450bcb6bd704676
trung-doan <trung-doan@cybozu.vn>
2021-07-06 12:25:36 +0700

    Revert "Fix "aria-invalid" for textarea"
    This reverts commit 5f0914d1ed4a3ff9caaded544a75ae0c2798c9b2.

--- src/textarea/index.ts
+++ src/textarea/index.ts
@@ -146,7 +146,7 @@ export class TextArea extends LitElement {
           .value=${this.value}
           aria-describedby="${this._GUID}-error"
           aria-required=${this.requiredIcon}
-          aria-invalid="${this.error !== ""}"
+          aria-invalid="${!this.error}"
           @change="${this._handleChangeTextarea}"
           @focus="${this._handleFocusTextarea}"
           ?disabled="${this.disabled}"
