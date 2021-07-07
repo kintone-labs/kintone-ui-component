dbab4034d9362067ca793f8c90c365bfdef67186
Nguyen Hoang Anh <nmanhnit@gmail.com>
2021-07-07 09:37:08 +0700

    fixed unit test for error props

--- src/mobile/dropdown/test/error.test.ts
+++ src/mobile/dropdown/test/error.test.ts
@@ -3,7 +3,7 @@ import { MobileDropdown } from "../index";
 
 describe("MobileDropdown", () => {
   describe("error", () => {
-    it("It does not display when initializing without props option", async () => {
+    it("does not display when initializing without props option", async () => {
       const container = new MobileDropdown({});
       const el = await fixture(container);
       const errorEl = el.querySelector(
@@ -12,7 +12,7 @@ describe("MobileDropdown", () => {
       await expect(errorEl).not.to.be.displayed;
     });
 
-    it('It should be equal "error-message" when initializing error with "error-message"', async () => {
+    it('should be equal "error-message" when initializing error with "error-message"', async () => {
       const container = new MobileDropdown({ error: "error-message" });
       const el = await fixture(container);
       const errorEl = el.querySelector(
@@ -22,7 +22,7 @@ describe("MobileDropdown", () => {
       await expect(errorEl).to.be.displayed;
     });
 
-    it('It should be replace by "replace-error" when changing by setter', async () => {
+    it('should be replace by "replace-error" when changing by setter', async () => {
       const container = new MobileDropdown({
         error: "error-message"
       });
@@ -33,31 +33,6 @@ describe("MobileDropdown", () => {
       ) as HTMLDivElement;
       await expect(errorEl.innerText).to.have.equal("replace-error");
       await expect(errorEl).to.be.displayed;
-    });
-
-    it("It does not display when initializing error value is null", async () => {
-      const container = new MobileDropdown({
-        // @ts-ignore
-        error: null
-      });
-      const el = await fixture(container);
-      const errorEl = el.querySelector(
-        ".kuc-mobile-dropdown__error"
-      ) as HTMLDivElement;
-      await expect(errorEl).not.to.be.displayed;
-    });
-
-    it("It does not display when changing by setter is null", async () => {
-      const container = new MobileDropdown({
-        error: "error-message"
-      });
-      // @ts-ignore
-      container.error = null;
-      const el = await fixture(container);
-      const errorEl = el.querySelector(
-        ".kuc-mobile-dropdown__error"
-      ) as HTMLDivElement;
-      await expect(errorEl).not.to.be.displayed;
     });
   });
 });
