eaaa68065cfcbce2dbd686a7445d7c41cb865347
Nguyen Hoang Anh <nmanhnit@gmail.com>
2021-07-07 09:38:18 +0700

    fixed unit test for label props

--- src/mobile/dropdown/test/label.test.ts
+++ src/mobile/dropdown/test/label.test.ts
@@ -3,7 +3,7 @@ import { MobileDropdown } from "../index";
 
 describe("MobileDropdown", () => {
   describe("label", () => {
-    it("It visible when initializing without props option", async () => {
+    it("visible when initializing without props option", async () => {
       const container = new MobileDropdown({});
       const el = await fixture(container);
       const labelEl = el.querySelector(
@@ -12,7 +12,7 @@ describe("MobileDropdown", () => {
       await expect(labelEl).to.be.visible;
     });
 
-    it('It should be equal "options-label" when initializing label with "options-label"', async () => {
+    it('should be equal "options-label" when initializing label with "options-label"', async () => {
       const container = new MobileDropdown({ label: "options-label" });
       const el = await fixture(container);
       const labelEl = el.querySelector(
@@ -21,7 +21,7 @@ describe("MobileDropdown", () => {
       await expect(labelEl.innerText).to.have.equal("options-label");
     });
 
-    it('It should be replace by "replace-label" when changing by setter', async () => {
+    it('should be replace by "replace-label" when changing by setter', async () => {
       const container = new MobileDropdown({
         label: "options-label"
       });
@@ -31,31 +31,6 @@ describe("MobileDropdown", () => {
         ".kuc-mobile-dropdown__label__text"
       ) as HTMLSpanElement;
       await expect(labelEl.textContent).to.have.equal("replace-label");
-    });
-
-    it("It visible when initializing lable value is null", async () => {
-      const container = new MobileDropdown({
-        // @ts-ignore
-        label: null
-      });
-      const el = await fixture(container);
-      const labelEl = el.querySelector(
-        ".kuc-mobile-dropdown__label__text"
-      ) as HTMLSpanElement;
-      await expect(labelEl).to.be.visible;
-    });
-
-    it("It visible when changing by setter is null", async () => {
-      const container = new MobileDropdown({
-        label: "options-label"
-      });
-      // @ts-ignore
-      container.label = null;
-      const el = await fixture(container);
-      const labelEl = el.querySelector(
-        ".kuc-mobile-dropdown__label__text"
-      ) as HTMLSpanElement;
-      await expect(labelEl).to.be.visible;
     });
   });
 });
