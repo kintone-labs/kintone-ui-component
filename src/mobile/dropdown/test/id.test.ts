4d322047a946f641b72a503c31e552487510fed1
Nguyen Hoang Anh <nmanhnit@gmail.com>
2021-07-07 09:37:26 +0700

    fixed unit test for id props

--- src/mobile/dropdown/test/id.test.ts
+++ src/mobile/dropdown/test/id.test.ts
@@ -3,40 +3,25 @@ import { MobileDropdown } from "../index";
 
 describe("MobileDropdown", () => {
   describe("id", () => {
-    it("It should be set to empty string when initializing without props option", async () => {
+    it("should be set to empty string when initializing without props option", async () => {
       const container = new MobileDropdown({});
       const el = await fixture(container);
       expect(el.id).to.be.equal("");
     });
 
-    it('It should be equal "options-id" string when initializing id with "options-id" value', async () => {
+    it('should be equal "options-id" string when initializing id with "options-id" value', async () => {
       const container = new MobileDropdown({ id: "options-id" });
       const el = await fixture(container);
       expect(el.id).to.have.equal("options-id");
     });
 
-    it('It should be replace by "replace-id" when changing by setter', async () => {
+    it('should be replace by "replace-id" when changing by setter', async () => {
       const container = new MobileDropdown({
         id: "options-id"
       });
       container.id = "replace-id";
       const el = await fixture(container);
       expect(el.id).to.have.equal("replace-id");
-    });
-
-    it('It should be equal "null" string when initializing id with null value', async () => {
-      // @ts-ignore
-      const container = new MobileDropdown({ id: null });
-      const el = await fixture(container);
-      expect(el.id).to.be.equal("null");
-    });
-
-    it('It should be replace by "null" when changing by setter', async () => {
-      const container = new MobileDropdown({});
-      // @ts-ignore
-      container.id = null;
-      const el = await fixture(container);
-      expect(el.id).to.have.equal("null");
     });
   });
 });
