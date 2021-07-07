9b695dad960dec214e58defc48075485b89139ef
Nguyen Hoang Anh <nmanhnit@gmail.com>
2021-07-07 09:38:50 +0700

    init unit test for render props

---
+++ src/mobile/dropdown/test/render.test.ts
@@ -0,0 +1,11 @@
+import { expect, fixture } from "@open-wc/testing";
+import { MobileDropdown } from "../index";
+
+describe("render successfully without props", () => {
+  const container = new MobileDropdown();
+  it("have 'kuc-mobile-dropdown'", async () => {
+    const el = await fixture(container);
+    const tagname = el.tagName;
+    await expect(tagname.toLowerCase()).to.be.equal("kuc-mobile-dropdown");
+  });
+});
