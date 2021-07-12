26cea3a76382177dcc49f4d10a781793d94fba18
trung-doan <trung-doan@cybozu.vn>
2021-07-12 14:37:14 +0700

    Improve unit test for Dropdown id

--- src/dropdown/test/id.test.ts
+++ src/dropdown/test/id.test.ts
@@ -1,53 +1,28 @@
 import { expect, fixture } from "@open-wc/testing";
 import { Dropdown } from "../index";
 
-describe("id default prop is null", () => {
-  const container = new Dropdown({});
+describe("Dropdown", () => {
+  describe("id", () => {
+    it('should be "" when not assigning on constructor', async () => {
+      const container = new Dropdown({});
 
-  it("id default prop is null", async () => {
-    const el = await fixture(container);
-    expect(el.id).to.be.equal("");
-  });
-});
+      const el = await fixture(container);
+      expect(el.id).to.be.equals("");
+    });
 
-describe("id default prop set successfully", () => {
-  const container = new Dropdown({ id: "options-id" });
+    it('should be "options-id" when assigning on constructor', async () => {
+      const container = new Dropdown({ id: "options-id" });
 
-  it("id default prop set successfully", async () => {
-    const el = await fixture(container);
-    expect(el.id).to.have.equal("options-id");
-  });
-});
+      const el = await fixture(container);
+      expect(el.id).to.have.equals("options-id");
+    });
 
-describe("id prop replace successfully", () => {
-  const container = new Dropdown({
-    id: "options-id"
-  });
-  container.id = "replace-id";
+    it('should be replaced to "replace-id" after changing by setter', async () => {
+      const container = new Dropdown({ id: "options-id" });
 
-  it("id prop replace successfully", async () => {
-    const el = await fixture(container);
-    expect(el.id).to.have.equal("replace-id");
-  });
-});
-
-describe("id default prop set to null", () => {
-  // @ts-ignore
-  const container = new Dropdown({ id: null });
-
-  it("id default prop set to null", async () => {
-    const el = await fixture(container);
-    expect(el.id).to.be.equal("null");
-  });
-});
-
-describe("id prop set to null", () => {
-  const container = new Dropdown({});
-  // @ts-ignore
-  container.id = null;
-
-  it("id prop set to null", async () => {
-    const el = await fixture(container);
-    expect(el.id).to.have.equal("null");
+      const el = await fixture(container);
+      container.id = "replace-id";
+      expect(el.id).to.have.equals("replace-id");
+    });
   });
 });
