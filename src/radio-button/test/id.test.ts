f1987eee3c482f7b04cf35c07c96882a7c014de5
trung-doan <trung-doan@cybozu.vn>
2021-07-12 14:45:51 +0700

    Improve unit test for id of MultiChoice, Radio, Text, Text Area

--- src/radio-button/test/id.test.ts
+++ src/radio-button/test/id.test.ts
@@ -1,61 +1,28 @@
 import { expect, fixture } from "@open-wc/testing";
 import { RadioButton } from "../index";
 
-describe("confirm id default value is null", () => {
-  const container = new RadioButton();
+describe("RadioButton", () => {
+  describe("id", () => {
+    it('should be "" when not assigning on constructor', async () => {
+      const container = new RadioButton({});
 
-  it("confirm id default value is null", async () => {
-    const el = await fixture(container);
-    await expect(el.id).to.be.equal("");
-  });
-});
+      const el = await fixture(container);
+      expect(el.id).to.be.equals("");
+    });
 
-describe("id constructor set successfully", () => {
-  const container = new RadioButton({ id: "options-id" });
+    it('should be "options-id" when assigning on constructor', async () => {
+      const container = new RadioButton({ id: "options-id" });
 
-  it("id constructor set successfully", async () => {
-    const el = await fixture(container);
-    await expect(el.id).to.be.equal("options-id");
-  });
-});
+      const el = await fixture(container);
+      expect(el.id).to.have.equals("options-id");
+    });
 
-describe("id prop set successfully", () => {
-  const container = new RadioButton();
-  container.id = "options-id";
+    it('should be replaced to "replace-id" after changing by setter', async () => {
+      const container = new RadioButton({ id: "options-id" });
 
-  it("id prop set successfully", async () => {
-    const el = await fixture(container);
-    await expect(el.id).to.be.equal("options-id");
-  });
-});
-
-describe("id prop replace successfully", () => {
-  const container = new RadioButton({ id: "options-id" });
-  container.id = "replace-id";
-
-  it("id prop replace successfully", async () => {
-    const el = await fixture(container);
-    await expect(el.id).to.be.equal("replace-id");
-  });
-});
-
-describe("id constructor set to null successfully", () => {
-  // @ts-ignore
-  const container = new RadioButton({ id: null });
-
-  it("id constructor set to null successfully", async () => {
-    const el = await fixture(container);
-    await expect(el.id).to.be.equal("null");
-  });
-});
-
-describe("id prop set to null successfully", () => {
-  const container = new RadioButton({ id: "options-id" });
-  // @ts-ignore
-  container.id = null;
-
-  it("id prop set to null successfully", async () => {
-    const el = await fixture(container);
-    await expect(el.id).to.be.equal("null");
+      const el = await fixture(container);
+      container.id = "replace-id";
+      expect(el.id).to.have.equals("replace-id");
+    });
   });
 });
