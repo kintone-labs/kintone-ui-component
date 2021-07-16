378d1afc19344413ada888577aedf525eb869854
trung-doan <trung-doan@cybozu.vn>
2021-07-16 15:02:14 +0700

    Improve unit test for mobile-radio-button borderVisible

--- src/mobile/radio-button/test/borderVisible.test.ts
+++ src/mobile/radio-button/test/borderVisible.test.ts
@@ -1,79 +1,52 @@
 import { expect, fixture } from "@open-wc/testing";
 import { MobileRadioButton } from "../index";
 
-describe("confirm borderVisible default value is true", () => {
-  const container = new MobileRadioButton();
+describe("MobileRadioButton", () => {
+  describe("borderVisible", () => {
+    it("should be default(true) when not set on constructor", async () => {
+      const container = new MobileRadioButton({});
 
-  it("confirm borderVisible default value is true", async () => {
-    const el = await fixture(container);
-    const selectMenuEl = el.querySelector(
-      ".kuc-mobile-radio-button__group__select-menu"
-    ) as HTMLDivElement;
-    await expect(selectMenuEl.hasAttribute("borderVisible")).to.be.equal(true);
+      const el = await fixture(container);
+      const selectMenuEl = el.querySelector(
+        ".kuc-mobile-radio-button__group__select-menu"
+      ) as HTMLDivElement;
+
+      expect(selectMenuEl.hasAttribute("borderVisible")).to.be.equal(true);
+    });
   });
-});
 
-describe("borderVisible constructor set successfully", () => {
-  const container = new MobileRadioButton({ borderVisible: false });
+  it("should be set to false if set false on constructor", async () => {
+    const container = new MobileRadioButton({ borderVisible: false });
 
-  it("borderVisible constructor set successfully", async () => {
     const el = await fixture(container);
     const selectMenuEl = el.querySelector(
       ".kuc-mobile-radio-button__group__select-menu"
     ) as HTMLDivElement;
-    await expect(selectMenuEl.hasAttribute("borderVisible")).to.be.equal(false);
+
+    expect(selectMenuEl.hasAttribute("borderVisible")).to.be.equal(false);
   });
-});
 
-describe("borderVisible prop set to true successfully", () => {
-  const container = new MobileRadioButton({ borderVisible: false });
-  container.borderVisible = true;
+  it("should be change to true if set true by setter", async () => {
+    const container = new MobileRadioButton({ borderVisible: false });
+    container.borderVisible = true;
 
-  it("borderVisible prop set to true successfully", async () => {
     const el = await fixture(container);
     const selectMenuEl = el.querySelector(
       ".kuc-mobile-radio-button__group__select-menu"
     ) as HTMLDivElement;
-    await expect(selectMenuEl.hasAttribute("borderVisible")).to.be.equal(true);
+
+    expect(selectMenuEl.hasAttribute("borderVisible")).to.be.equal(true);
   });
-});
 
-describe("borderVisible prop set to false successfully", () => {
-  const container = new MobileRadioButton({ borderVisible: true });
-  container.borderVisible = false;
+  it("should be change to false if set false by setter", async () => {
+    const container = new MobileRadioButton({ borderVisible: true });
+    container.borderVisible = false;
 
-  it("borderVisible prop set to false successfully", async () => {
     const el = await fixture(container);
     const selectMenuEl = el.querySelector(
       ".kuc-mobile-radio-button__group__select-menu"
     ) as HTMLDivElement;
-    await expect(selectMenuEl.hasAttribute("borderVisible")).to.be.equal(false);
-  });
-});
 
-describe("borderVisible constructor set to null successfully", () => {
-  // @ts-expect-error
-  const container = new MobileRadioButton({ borderVisible: null });
-
-  it("borderVisible constructor set to null successfully", async () => {
-    const el = await fixture(container);
-    const selectMenuEl = el.querySelector(
-      ".kuc-mobile-radio-button__group__select-menu"
-    ) as HTMLDivElement;
-    await expect(selectMenuEl.hasAttribute("borderVisible")).to.be.equal(false);
-  });
-});
-
-describe("borderVisible prop set to null successfully", () => {
-  const container = new MobileRadioButton({ borderVisible: true });
-  // @ts-expect-error
-  container.borderVisible = null;
-
-  it("borderVisible prop set to null successfully", async () => {
-    const el = await fixture(container);
-    const selectMenuEl = el.querySelector(
-      ".kuc-mobile-radio-button__group__select-menu"
-    ) as HTMLDivElement;
-    await expect(selectMenuEl.hasAttribute("borderVisible")).to.be.equal(false);
+    expect(selectMenuEl.hasAttribute("borderVisible")).to.be.equal(false);
   });
 });
