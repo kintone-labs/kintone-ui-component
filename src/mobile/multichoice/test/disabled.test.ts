506960b73c399d3dbc71903b9fb68f48a3fde326
Nguyen Hoang Anh <nmanhnit@gmail.com>
2021-07-21 13:38:14 +0700

    multichoice unit test for disabled props

---
+++ src/mobile/multichoice/test/disabled.test.ts
@@ -0,0 +1,61 @@
+import { expect, fixture } from "@open-wc/testing";
+import { MobileMultiChoice } from "../index";
+
+describe("MobileMultiChoice", () => {
+  describe("disabled", () => {
+    const initItems = [
+      { label: "-----", value: "-----" },
+      { label: "Orange", value: "orange" },
+      { label: "Apple", value: "apple" }
+    ];
+
+    it("does not exists on element when initializing without props option", async () => {
+      const container = new MobileMultiChoice({
+        items: initItems
+      });
+      const el = await fixture(container);
+      const inputEl = el.querySelector(
+        ".kuc-mobile-multi-choice__input-form__select__input"
+      );
+      expect(inputEl).not.to.have.attr("disabled");
+    });
+
+    it("exists on element when initializing disabled value is true", async () => {
+      const container = new MobileMultiChoice({
+        items: initItems,
+        disabled: true
+      });
+      const el = await fixture(container);
+      const inputEl = el.querySelector(
+        ".kuc-mobile-multi-choice__input-form__select__input"
+      );
+      expect(inputEl).to.have.attr("disabled");
+    });
+
+    it("exists on element when changing by setter", async () => {
+      const container = new MobileMultiChoice({
+        items: initItems,
+        disabled: false
+      });
+      container.disabled = true;
+      const el = await fixture(container);
+      const inputEl = el.querySelector(
+        ".kuc-mobile-multi-choice__input-form__select__input"
+      );
+      expect(inputEl).to.have.attr("disabled");
+    });
+
+    it("does not exists on element when changing by setter", async () => {
+      const container = new MobileMultiChoice({
+        items: initItems,
+        disabled: true
+      });
+      container.disabled = false;
+      const el = await fixture(container);
+      const inputEl = el.querySelector(
+        ".kuc-mobile-multi-choice__input-form__select__input"
+      );
+      expect(inputEl).not.to.have.attr("disabled");
+    });
+  });
+});
