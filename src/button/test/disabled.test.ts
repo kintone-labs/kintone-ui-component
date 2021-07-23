013fdde3815bf3aeb6370ec56d5eabec181e1898
trung-doan <trung-doan@cybozu.vn>
2021-07-23 16:08:07 +0700

    Improving button disabled unit test following BDD

--- src/button/test/disabled.test.ts
+++ src/button/test/disabled.test.ts
@@ -1,75 +1,48 @@
 import { expect, fixture } from "@open-wc/testing";
 import { Button } from "../index";
 
-describe("disabled default prop is false", () => {
-  const container = new Button({});
+describe("Button", () => {
+  describe("disabled", () => {
+    it("should not be added into button element when not set on constructor", async () => {
+      const container = new Button();
+      const el = await fixture(container);
+      const buttonEl = el.querySelector(
+        ".kuc-button__button"
+      ) as HTMLButtonElement;
 
-  it("disabled default prop is false", async () => {
-    const el = await fixture(container);
-    const buttonEl = (await el.querySelector(
-      ".kuc-button__button"
-    )) as HTMLButtonElement;
-    await expect(buttonEl.hasAttribute("disabled")).to.have.equal(false);
-  });
-});
+      expect(buttonEl).to.not.have.attr("disabled");
+    });
 
-describe("disabled default prop set successfully", () => {
-  const container = new Button({ disabled: true });
+    it("should be added into button element when set to true on constructor", async () => {
+      const container = new Button({ disabled: true });
+      const el = await fixture(container);
+      const buttonEl = el.querySelector(
+        ".kuc-button__button"
+      ) as HTMLButtonElement;
 
-  it("disabled default prop set successfully", async () => {
-    const el = await fixture(container);
-    const buttonEl = (await el.querySelector(
-      ".kuc-button__button"
-    )) as HTMLButtonElement;
-    await expect(buttonEl.hasAttribute("disabled")).to.have.equal(true);
-  });
-});
+      expect(buttonEl).to.have.attr("disabled");
+    });
 
-describe("disabled prop set to true successfully", () => {
-  const container = new Button({ disabled: false });
-  container.disabled = true;
+    it("should be added into button element when changed to true by setter", async () => {
+      const container = new Button({ disabled: false });
+      container.disabled = true;
+      const el = await fixture(container);
+      const buttonEl = el.querySelector(
+        ".kuc-button__button"
+      ) as HTMLButtonElement;
 
-  it("disabled prop set to true successfully", async () => {
-    const el = await fixture(container);
-    await expect(el.querySelector(".kuc-button__button")).to.have.attr(
-      "disabled"
-    );
-  });
-});
+      expect(buttonEl).to.have.attr("disabled");
+    });
 
-describe("disabled prop set to false successfully", () => {
-  const container = new Button({ disabled: true });
-  container.disabled = false;
+    it("should not be added into button element when changed to false by setter", async () => {
+      const container = new Button({ disabled: true });
+      container.disabled = false;
+      const el = await fixture(container);
+      const buttonEl = el.querySelector(
+        ".kuc-button__button"
+      ) as HTMLButtonElement;
 
-  it("disabled prop set to false successfully", async () => {
-    const el = await fixture(container);
-    await expect(el.querySelector(".kuc-button__button")).not.to.have.attr(
-      "disabled"
-    );
-  });
-});
-
-describe("disabled default prop set to null", () => {
-  // @ts-ignore
-  const container = new Button({ disabled: null });
-
-  it("disabled default prop set to null", async () => {
-    const el = await fixture(container);
-    await expect(el.querySelector(".kuc-button__button")).not.to.have.attr(
-      "disabled"
-    );
-  });
-});
-
-describe("disabled prop set to null", () => {
-  const container = new Button({});
-  // @ts-ignore
-  container.disabled = null;
-
-  it("disabled prop set to null", async () => {
-    const el = await fixture(container);
-    await expect(el.querySelector(".kuc-button__button")).not.to.have.attr(
-      "disabled"
-    );
+      expect(buttonEl).to.not.have.attr("disabled");
+    });
   });
 });
