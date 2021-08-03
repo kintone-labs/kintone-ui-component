ed273a90a3a99557a61d7a1851fad693a492134e
trung-doan <trung-doan@cybozu.vn>
2021-08-03 14:20:35 +0700

    (BDD) Improve unit test open() method

--- src/dialog/test/open.test.ts
+++ src/dialog/test/open.test.ts
@@ -5,14 +5,12 @@ describe("Dialog", () => {
   describe("open", () => {
     it("should be display when call open() method", async () => {
       const container = new Dialog();
-      const el = await fixture(container);
       container.open();
-      await elementUpdated(el);
 
-      const parrentEl = el.parentNode as HTMLElement;
+      const parrentEl = container.parentNode as HTMLElement;
       expect(parrentEl.nodeName).to.equal("BODY");
 
-      expect(el.hasAttribute("opened")).to.equal(true);
+      expect(container.hasAttribute("opened")).to.equal(true);
     });
   });
 });
