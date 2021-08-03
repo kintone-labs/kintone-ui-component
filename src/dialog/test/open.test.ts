ba7123c2b45f78e10f8b1111edbdf99900f5ae1e
trung-doan <trung-doan@cybozu.vn>
2021-08-03 14:16:59 +0700

    (BDD) Improve unit test open() method

--- src/dialog/test/open.test.ts
+++ src/dialog/test/open.test.ts
@@ -1,15 +1,18 @@
 import { expect, fixture, elementUpdated } from "@open-wc/testing";
 import { Dialog } from "../index";
 
-describe("open method successfully", () => {
-  const container = new Dialog();
+describe("Dialog", () => {
+  describe("open", () => {
+    it("should be display when call open() method", async () => {
+      const container = new Dialog();
+      const el = await fixture(container);
+      container.open();
+      await elementUpdated(el);
 
-  it("open method successfully", async () => {
-    const el = await fixture(container);
-    container.open();
-    await elementUpdated(el);
-    const bodyel = document.querySelector("BODY");
-    await expect(bodyel!.querySelector("kuc-dialog")).to.not.be.null;
-    await expect(el.hasAttribute("opened")).to.be.equal(true);
+      const parrentEl = el.parentNode as HTMLElement;
+      expect(parrentEl.nodeName).to.equal("BODY");
+
+      expect(el.hasAttribute("opened")).to.equal(true);
+    });
   });
 });
--- src/mobile/notification/test/open.test.ts
+++ src/mobile/notification/test/open.test.ts
@@ -1,21 +1,17 @@
 import { expect, fixture, elementUpdated } from "@open-wc/testing";
 import { MobileNotification } from "../index";
 
-describe("open method successfully", () => {
-  const container = new MobileNotification();
+describe("MobileNotification", () => {
+  describe("open", () => {
+    it("should be display when call open() method", async () => {
+      const container = new MobileNotification();
+      container.open();
 
-  it("open method successfully", async () => {
-    const el = await fixture(container);
-    container.open();
-    await elementUpdated(el);
-    const bodyel = document.querySelector("BODY");
-    await expect(bodyel!.querySelector("kuc-mobile-notification")).to.not.be
-      .null;
-    await expect(
-      [
-        "kuc-mobile-notification-fadein",
-        "kuc-mobile-notification-fadeout"
-      ].every(c => el.classList.contains(c))
-    ).to.be.false;
+      const parrentEl = container.parentNode as HTMLElement;
+      expect(parrentEl.nodeName).to.equal("BODY");
+
+      expect(container.classList.length).to.equal(1);
+      expect(container.classList[0]).to.equal("kuc-mobile-notification-fadein");
+    });
   });
 });
