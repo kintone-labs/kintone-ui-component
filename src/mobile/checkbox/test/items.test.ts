ecc4f91e709fe066450e34b4b4c6328505018617
trung-doan <trung-doan@cybozu.vn>
2021-08-06 14:26:00 +0700

    BDD - MobileCheckbox items

--- src/mobile/checkbox/test/items.test.ts
+++ src/mobile/checkbox/test/items.test.ts
@@ -101,8 +101,44 @@ describe("MobileCheckbox", () => {
       expect(labelEl2.innerText).to.equal(initItemsWithoutLabel[2].value);
     });
 
+    it("should set items when assigned items on constructor", async () => {
+      const container = new MobileCheckbox({ items: initItems });
+      const el = await fixture(container);
+      const itemsEl = el.querySelectorAll(
+        ".kuc-mobile-checkbox__group__select-menu__item"
+      );
+
+      expect(itemsEl.length).to.equal(3);
+
+      const inputEl0 = itemsEl[0].querySelector("input") as HTMLInputElement;
+      expect(inputEl0.hasAttribute("checked")).to.equal(false);
+      expect(inputEl0.value).to.equal(initItems[0].value);
+      const labelEl0 = itemsEl[0].querySelector(
+        ".kuc-mobile-checkbox__group__select-menu__item__label"
+      ) as HTMLDivElement;
+      expect(labelEl0.innerText).to.equal(initItems[0].label);
+
+      const inputEl1 = itemsEl[1].querySelector("input") as HTMLInputElement;
+      expect(inputEl1.hasAttribute("checked")).to.equal(false);
+      expect(inputEl1.value).to.equal(initItems[1].value);
+      const labelEl1 = itemsEl[1].querySelector(
+        ".kuc-mobile-checkbox__group__select-menu__item__label"
+      ) as HTMLDivElement;
+      expect(labelEl1.innerText).to.equal(initItems[1].label);
+
+      const inputEl2 = itemsEl[2].querySelector("input") as HTMLInputElement;
+      expect(inputEl2.hasAttribute("checked")).to.equal(false);
+      expect(inputEl2.value).to.equal(initItems[2].value);
+      const labelEl2 = itemsEl[2].querySelector(
+        ".kuc-mobile-checkbox__group__select-menu__item__label"
+      ) as HTMLDivElement;
+      expect(labelEl2.innerText).to.equal(initItems[2].label);
+    });
+
     it("should set items when assigned items by setter", async () => {
       const container = new MobileCheckbox({ items: initItems });
+      container.items = initItems;
+
       const el = await fixture(container);
       const itemsEl = el.querySelectorAll(
         ".kuc-mobile-checkbox__group__select-menu__item"
