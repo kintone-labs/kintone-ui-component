259a36e19a45bd34290df6c829e046ce29d39e8a
TomokoMiyake <t.smile297327@gmail.com>
2021-08-09 00:05:28 +0900

    improve prefix and suffix prop for Text

--- src/text/test/suffix.test.ts
+++ src/text/test/suffix.test.ts
@@ -1,69 +1,46 @@
 import { expect, fixture } from "@open-wc/testing";
 import { Text } from "../index";
 
-describe("confirm suffix default prop is null", () => {
-  const container = new Text();
+describe("Text", () => {
+  describe("suffix", () => {
+    it("should be null when not assigning on constructor", async () => {
+      const container = new Text();
+      const el = await fixture(container);
+      const suffixEl = el.querySelector(
+        ".kuc-text__group__input-form__suffix-outer__suffix"
+      ) as HTMLSpanElement;
+      expect(suffixEl.hasAttribute("hidden")).to.equal(true);
+    });
 
-  it("confirm suffix default prop is null", async () => {
-    const el = await fixture(container);
-    const suffixEl = el.querySelector(
-      ".kuc-text__group__input-form__suffix-outer__suffix"
-    ) as HTMLSpanElement;
-    await expect(suffixEl.hasAttribute("hidden")).to.be.equal(true);
-  });
-});
+    it("should be 'yen' when assigning on constructor", async () => {
+      const container = new Text();
+      const el = await fixture(container);
+      const suffixEl = el.querySelector(
+        ".kuc-text__group__input-form__suffix-outer__suffix"
+      ) as HTMLSpanElement;
+      expect(suffixEl.hasAttribute("hidden")).to.equal(true);
+    });
 
-describe("suffix constructor set successfully", () => {
-  const container = new Text({ suffix: "yen" });
+    it("should be 'yen' when assigning by setter", async () => {
+      const container = new Text();
+      container.suffix = "yen";
+      const el = await fixture(container);
+      const suffixEl = el.querySelector(
+        ".kuc-text__group__input-form__suffix-outer__suffix"
+      ) as HTMLSpanElement;
+      expect(suffixEl.hasAttribute("hidden")).to.equal(false);
+      expect(suffixEl.innerText).to.equal("yen");
+    });
 
-  it("suffix constructor set successfully'", async () => {
-    const el = await fixture(container);
-    const suffixEl = el.querySelector(
-      ".kuc-text__group__input-form__suffix-outer__suffix"
-    ) as HTMLSpanElement;
-    await expect(suffixEl.hasAttribute("hidden")).to.be.equal(false);
-    await expect(suffixEl.innerText).to.be.equal("yen");
-  });
-});
-
-describe("suffix prop set successfully", () => {
-  const container = new Text();
-  container.suffix = "yen";
-
-  it("suffix prop set successfully'", async () => {
-    const el = await fixture(container);
-    const suffixEl = el.querySelector(
-      ".kuc-text__group__input-form__suffix-outer__suffix"
-    ) as HTMLSpanElement;
-    await expect(suffixEl.hasAttribute("hidden")).to.be.equal(false);
-    await expect(suffixEl.innerText).to.be.equal("yen");
-  });
-});
-
-describe("suffix prop replace successfully", () => {
-  const container = new Text({ suffix: "$" });
-  container.suffix = "yen";
-
-  it("suffix prop replace successfully'", async () => {
-    const el = await fixture(container);
-    const suffixEl = el.querySelector(
-      ".kuc-text__group__input-form__suffix-outer__suffix"
-    ) as HTMLSpanElement;
-    await expect(suffixEl.hasAttribute("hidden")).to.be.equal(false);
-    await expect(suffixEl.innerText).to.be.equal("yen");
-  });
-});
-
-describe("suffix prop set to null successfully", () => {
-  const container = new Text({ suffix: "$" });
-  // @ts-ignore
-  container.suffix = null;
-
-  it("suffix prop set to null successfully", async () => {
-    const el = await fixture(container);
-    const suffixEl = el.querySelector(
-      ".kuc-text__group__input-form__suffix-outer__suffix"
-    ) as HTMLSpanElement;
-    await expect(suffixEl.hasAttribute("hidden")).to.be.equal(true);
+    it("should be replaced by 'yen' when changed by setter", async () => {
+      const container = new Text({ suffix: "$" });
+      container.suffix = "yen";
+      const el = await fixture(container);
+      const suffixEl = el.querySelector(
+        ".kuc-text__group__input-form__suffix-outer__suffix"
+      ) as HTMLSpanElement;
+      expect(suffixEl.hasAttribute("hidden")).to.equal(false);
+      expect(suffixEl.innerText).to.equal("yen");
+    });
   });
 });
