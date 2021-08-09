30138fa92172fcf49598129212732ea383c9ba80
trung-doan <trung-doan@cybozu.vn>
2021-08-09 15:18:19 +0700

    BDD - Improve focusEvent.test.ts

--- src/text/test/focusEvent.test.ts
+++ src/text/test/focusEvent.test.ts
@@ -1,17 +1,22 @@
 import { expect, fixture } from "@open-wc/testing";
 import { Text } from "../index";
 
-describe("Function focus event run successfully", () => {
-  const container = new Text({ value: "Orange" });
-  container.addEventListener("focus", (event: any) => {
-    expect(event.detail.value).to.have.equal("Orange");
-  });
+describe("Text", () => {
+  describe("focusEvent", () => {
+    it("should be triggered when focusing to the input", async () => {
+      const container = new Text({ value: "Orange" });
+      container.addEventListener("focus", (event: any) => {
+        expect(event.detail.value).to.have.equal("Orange");
+        container.value = "Apple";
+      });
 
-  it("Function focus event run successfully", async () => {
-    const el = await fixture(container);
-    const inputEl = el.querySelector(
-      ".kuc-text__group__input-form__input-outer__input"
-    ) as HTMLInputElement;
-    inputEl.focus();
+      const el = await fixture(container);
+      const inputEl = el.querySelector(
+        ".kuc-text__group__input-form__input-outer__input"
+      ) as HTMLInputElement;
+      inputEl.focus();
+
+      expect(container.value).to.equal("Apple");
+    });
   });
 });
