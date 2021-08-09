5dec8be806fdcb8c6e43075eab2ab0e29a118f1c
trung-doan <trung-doan@cybozu.vn>
2021-08-09 15:52:23 +0700

    BDD - Improve Checkbox changeEvent.test.ts

---
+++ src/checkbox/test/accessibility.test.ts
@@ -0,0 +1,38 @@
+import {
+  triggerFocusFor,
+  triggerBlurFor,
+  expect,
+  fixture
+} from "@open-wc/testing";
+import { Checkbox } from "../index";
+
+const initItems = [
+  { label: "-----", value: "-----" },
+  { label: "Orange", value: "orange" },
+  { label: "Apple", value: "apple" }
+];
+
+describe("Checkbox", () => {
+  describe("changeEvent", () => {
+    it("can be focused and blured", async () => {
+      const container = new Checkbox({
+        items: initItems,
+        value: [initItems[1].value]
+      });
+
+      const el: HTMLElement = await fixture(container);
+      const inputEl = el.querySelector(
+        ".kuc-checkbox__group__select-menu__item__input"
+      ) as HTMLInputElement;
+      await triggerFocusFor(inputEl);
+      expect(document.activeElement?.className).to.equal(
+        "kuc-checkbox__group__select-menu__item__input"
+      );
+
+      triggerBlurFor(inputEl);
+      expect(document.activeElement?.className).to.not.equal(
+        "kuc-checkbox__group__select-menu__item__input"
+      );
+    });
+  });
+});
--- src/checkbox/test/changeEvent.test.ts
+++ src/checkbox/test/changeEvent.test.ts
@@ -1,61 +1,37 @@
-import {
-  expect,
-  fixture,
-  triggerFocusFor,
-  triggerBlurFor
-} from "@open-wc/testing";
+import { expect, fixture } from "@open-wc/testing";
 import { Checkbox } from "../index";
 
-describe("Function change event run successfully by mouse/keyboard event", () => {
-  const expectedLabels = ["-----", "Orange", "Apple"];
-  const expectedValues = ["-----", "orange", "apple"];
+const initItems = [
+  { label: "-----", value: "-----" },
+  { label: "Orange", value: "orange" },
+  { label: "Apple", value: "apple" }
+];
 
-  const container = new Checkbox({
-    items: [
-      {
-        label: expectedLabels[0],
-        value: expectedValues[0]
-      },
-      {
-        label: expectedLabels[1],
-        value: expectedValues[1]
-      },
-      {
-        label: expectedLabels[2],
-        value: expectedValues[2]
-      }
-    ],
-    value: [expectedValues[1]]
-  });
-  container.addEventListener("change", (event: any) => {
-    expect(event.detail.value)
-      .to.be.an("array")
-      .that.includes(expectedValues[2], expectedValues[1]);
-    expect(event.detail.oldValue)
-      .to.be.an("array")
-      .that.includes(expectedValues[1]);
-  });
+describe("Checkbox", () => {
+  describe("changeEvent", () => {
+    it("should be triggered when input element triggered change event", async () => {
+      const container = new Checkbox({
+        items: initItems,
+        value: [initItems[1].value],
+        error: ""
+      });
+      container.addEventListener("change", (event: any) => {
+        expect(event.detail.oldValue).to.deep.equal([initItems[1].value]);
+        expect(event.detail.value).to.deep.equal([
+          initItems[1].value,
+          initItems[2].value
+        ]);
+        container.error = "changed";
+      });
 
-  it("Function change event run successfully by mouse/keyboard event", async () => {
-    const el = await fixture(container);
-    const itemsEl = el.querySelector(".kuc-checkbox__group__select-menu")!
-      .children as HTMLCollection;
-    if (itemsEl.length > 0) {
-      const secondItemEl = itemsEl[2] as HTMLElement;
-      const inputEl = secondItemEl.children[0] as HTMLInputElement;
-      inputEl.dispatchEvent(new Event("change"));
-    }
-  });
+      const el = await fixture(container);
+      const inputsEl = el.querySelectorAll(
+        ".kuc-checkbox__group__select-menu__item__input"
+      );
+      expect(inputsEl.length).equal(3);
+      inputsEl[2].dispatchEvent(new Event("change"));
 
-  it("can be focused and blured", async () => {
-    const el: HTMLElement = await fixture(container);
-    const itemsEl = el.querySelector(
-      ".kuc-checkbox__group__select-menu .kuc-checkbox__group__select-menu__item .kuc-checkbox__group__select-menu__item__input"
-    ) as HTMLInputElement;
-    await triggerFocusFor(itemsEl);
-    await expect(document.activeElement?.isSameNode(itemsEl)).to.be.true;
-
-    await triggerBlurFor(itemsEl);
-    await expect(document.activeElement?.isSameNode(itemsEl)).to.be.false;
+      expect(container.error).to.equal("changed");
+    });
   });
 });
