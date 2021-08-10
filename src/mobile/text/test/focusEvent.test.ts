ba797084dfcb5f8e4925428241912c4ec210414c
Doan Minh Trung <doanminhtrung1991@gmail.com>
2021-08-10 07:37:53 +0700

    Update src/mobile/text/test/focusEvent.test.ts
    Co-authored-by: Tomoko Miyake <t.smile297327@gmail.com>

--- src/mobile/text/test/focusEvent.test.ts
+++ src/mobile/text/test/focusEvent.test.ts
@@ -6,7 +6,7 @@ describe("MobileText", () => {
     it("should be triggered when focusing to the input", async () => {
       const container = new MobileText({ value: "Orange" });
       container.addEventListener("focus", (event: any) => {
-        expect(event.detail.value).to.have.equal("Orange");
+        expect(event.detail.value).to.equal("Orange");
         container.value = "Apple";
       });
 
