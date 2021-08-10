64f84beed779daed565ad77acf9b212dbd772586
trung-doan <trung-doan@cybozu.vn>
2021-08-10 09:20:31 +0700

    BDD - Improve Notification changeEvent.test.ts

--- src/mobile/notification/test/close.test.ts
+++ src/mobile/notification/test/close.test.ts
@@ -17,5 +17,23 @@ describe("MobileNotification", () => {
         "kuc-mobile-notification-fadeout"
       );
     });
+
+    it("should be fadeout when clicked close button", async () => {
+      const container = new MobileNotification();
+      container.open();
+
+      const closeBtnEl = container.querySelector(
+        ".kuc-mobile-notification__notification__closeButton"
+      ) as HTMLButtonElement;
+      closeBtnEl.click();
+
+      const parrentEl = container.parentNode as HTMLElement;
+      expect(parrentEl.nodeName).to.equal("BODY");
+
+      expect(container.classList.length).to.equal(1);
+      expect(container.classList[0]).to.equal(
+        "kuc-mobile-notification-fadeout"
+      );
+    });
   });
 });
