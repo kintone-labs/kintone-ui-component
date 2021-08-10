0d3bd3800dcffcb92721281ff9a3268fa113441f
Doan Minh Trung <doanminhtrung1991@gmail.com>
2021-08-10 07:38:13 +0700

    Update src/text/test/focusEvent.test.ts
    Co-authored-by: Tomoko Miyake <t.smile297327@gmail.com>

--- src/text/test/focusEvent.test.ts
+++ src/text/test/focusEvent.test.ts
@@ -6,7 +6,7 @@ describe("Text", () => {
     it("should be triggered when focusing to the input", async () => {
       const container = new Text({ value: "Orange" });
       container.addEventListener("focus", (event: any) => {
-        expect(event.detail.value).to.have.equal("Orange");
+        expect(event.detail.value).to.equal("Orange");
         container.value = "Apple";
       });
 
