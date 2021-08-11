1a913495a492648193e9d8c314c5fc9179785037
trung-doan <trung-doan@cybozu.vn>
2021-08-11 11:44:18 +0700

    Improve spinner/test/render.test.ts

--- src/spinner/test/render.test.ts
+++ src/spinner/test/render.test.ts
@@ -5,8 +5,7 @@ describe("Spinner", () => {
   describe("render", () => {
     it('should have "KUC-SPINNER" tag name when not assigning any prop in constructor', async () => {
       const container = new Spinner({});
-      const el = await fixture(container);
-      expect(el.tagName).to.equal("KUC-SPINNER");
+      expect(container.tagName).to.equal("KUC-SPINNER");
     });
   });
 });
