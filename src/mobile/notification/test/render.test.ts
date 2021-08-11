03660e630c64ab5d70a8b1824404acf0a4b5131f
trung-doan <trung-doan@cybozu.vn>
2021-08-11 11:42:49 +0700

    Improve import { expect } from @open-wc/testing;

--- src/mobile/notification/test/render.test.ts
+++ src/mobile/notification/test/render.test.ts
@@ -1,12 +1,11 @@
-import { expect, fixture } from "@open-wc/testing";
+import { expect } from "@open-wc/testing";
 import { MobileNotification } from "../index";
 
 describe("MobileNotification", () => {
   describe("render", () => {
     it('should have "KUC-MOBILE-NOTIFICATION" tag name when not assigning any prop in constructor', async () => {
       const container = new MobileNotification({});
-      const el = await fixture(container);
-      expect(el.tagName).to.equal("KUC-MOBILE-NOTIFICATION");
+      expect(container.tagName).to.equal("KUC-MOBILE-NOTIFICATION");
     });
   });
 });
--- src/notification/test/render.test.ts
+++ src/notification/test/render.test.ts
@@ -1,12 +1,11 @@
-import { expect, fixture } from "@open-wc/testing";
+import { expect } from "@open-wc/testing";
 import { Notification } from "../index";
 
 describe("Notification", () => {
   describe("render", () => {
     it('should have "KUC-NOTIFICATION" tag name when not assigning any prop in constructor', async () => {
       const container = new Notification({});
-      const el = await fixture(container);
-      expect(el.tagName).to.equal("KUC-NOTIFICATION");
+      expect(container.tagName).to.equal("KUC-NOTIFICATION");
     });
   });
 });
