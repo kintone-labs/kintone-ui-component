6301bf6d46acbbcd71b3b3b05d7aaa75a4d5626a
Nguyen Hoang Anh <nmanhnit@gmail.com>
2021-08-16 11:17:10 +0700

    role="group" attached to a div that contains multiple form elements.

--- src/checkbox/index.ts
+++ src/checkbox/index.ts
@@ -211,13 +211,12 @@ export class Checkbox extends LitElement {
     this._updateVisible();
     return html`
       ${this._getStyleTagTemplate()}
-      <div class="kuc-checkbox__group">
-        <div
-          class="kuc-checkbox__group__label"
-          ?hidden="${!this.label}"
-          role="group"
-          aria-labelledby="${this._GUID}-group-label"
-        >
+      <div
+        class="kuc-checkbox__group"
+        role="group"
+        aria-labelledby="${this._GUID}-group-label"
+      >
+        <div class="kuc-checkbox__group__label" ?hidden="${!this.label}">
           <span
             id="${this._GUID}-group-label"
             class="kuc-checkbox__group__label__text"
