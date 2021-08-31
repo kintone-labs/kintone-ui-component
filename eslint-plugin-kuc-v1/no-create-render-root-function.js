d450bdd0ff59c91e5bf9dbab78c6435b344daf25
Nguyen Hoang Anh <nmanhnit@gmail.com>
2021-09-01 00:46:34 +0700

    removing nested if statements

--- eslint-plugin-kuc-v1/no-create-render-root-function.js
+++ eslint-plugin-kuc-v1/no-create-render-root-function.js
@@ -3,19 +3,18 @@ module.exports = {
     return {
       ClassDeclaration: function(node) {
         const superClass = node.superClass.name;
-        if (superClass === "LitElement") {
-          const sourceCode = context.getSourceCode().getText();
-          const pattern = "public createRenderRoot\\(\\)";
-          const regex = new RegExp(pattern, "i");
-          if (regex.test(sourceCode)) return;
-          context.report({
-            node: node,
-            message: `Please add createRenderRoot function to this file
-            ex: public createRenderRoot() {
-              return this;
-            }`
-          });
-        }
+        if (superClass !== "LitElement") return;
+        const sourceCode = context.getSourceCode().getText();
+        const pattern = "public createRenderRoot\\(\\)";
+        const regex = new RegExp(pattern, "i");
+        if (regex.test(sourceCode)) return;
+        context.report({
+          node: node,
+          message: `Please add createRenderRoot function to this file
+          ex: public createRenderRoot() {
+            return this;
+          }`
+        });
       }
     };
   }
--- eslint-plugin-kuc-v1/no-using-custom-event.js
+++ eslint-plugin-kuc-v1/no-using-custom-event.js
@@ -3,25 +3,24 @@ module.exports = {
     return {
       ClassDeclaration: function(node) {
         const superClass = node.superClass.name;
-        if (superClass === "LitElement") {
-          const sourceCode = context.getSourceCode().getText();
-          const pattern = "new CustomEvent\\(";
-          const regex = new RegExp(pattern, "i");
-          if (regex.test(sourceCode)) return;
-          context.report({
-            node: node,
-            message: `Please using CustomEvent to implement events handling
-            ex: export const dispatchCustomEvent(el: HTMLElement, eventName: string, detail?: CustomEventDetail) {
-              const event = new CustomEvent(eventName, {
-                detail,
-                bubbles: true,
-                composed: true
-              });
-              return el.dispatchEvent(event);
-            }`
-          });
-        }
-      }
+        if (superClass !== "LitElement") return;
+        const sourceCode = context.getSourceCode().getText();
+        const pattern = "new CustomEvent\\(";
+        const regex = new RegExp(pattern, "i");
+        if (regex.test(sourceCode)) return;
+        context.report({
+          node: node,
+          message: `Please using CustomEvent to implement events handling
+          ex: export const dispatchCustomEvent(el: HTMLElement, eventName: string, detail?: CustomEventDetail) {
+            const event = new CustomEvent(eventName, {
+              detail,
+              bubbles: true,
+              composed: true
+            });
+            return el.dispatchEvent(event);
+          }`,
+        });
+      },
     };
-  }
+  },
 };
