4c62bcacb09c712b1956916c023586e39b00b646
Nguyen Hoang Anh <nmanhnit@gmail.com>
2021-09-06 13:55:39 +0700

    update CustomEvent Eslint rule

--- eslint-plugin-kuc-v1/no-using-custom-event.js
+++ eslint-plugin-kuc-v1/no-using-custom-event.js
@@ -3,23 +3,34 @@ module.exports = {
     return {
       ClassDeclaration: function(node) {
         const superClass = node.superClass.name;
-        if (superClass !== "LitElement") return;
         const sourceCode = context.getSourceCode().getText();
         const pattern = "new CustomEvent\\(";
         const regex = new RegExp(pattern, "i");
-        if (regex.test(sourceCode)) return;
-        context.report({
-          node: node,
-          message: `Please using CustomEvent to implement events handling
-          ex: export const dispatchCustomEvent(el: HTMLElement, eventName: string, detail?: CustomEventDetail) {
-            const event = new CustomEvent(eventName, {
-              detail,
-              bubbles: true,
-              composed: true
-            });
-            return el.dispatchEvent(event);
-          }`
-        });
+        if (superClass === "KucBase" && regex.test(sourceCode)) {
+          context.report({
+            node: node,
+            message: `Please using dispatchCustomEvent from kuc-base
+            import { dispatchCustomEvent } from "../base/kuc-base";
+            private _handleUpdateValue(value: string) {
+              dispatchCustomEvent(this, "change", {});
+            }
+            `
+          });
+        }
+        if (superClass === "LitElement" && !regex.test(sourceCode)) {
+          context.report({
+            node: node,
+            message: `Please using CustomEvent to implement events handling
+            ex: export const dispatchCustomEvent(el: HTMLElement, eventName: string, detail?: CustomEventDetail) {
+              const event = new CustomEvent(eventName, {
+                detail,
+                bubbles: true,
+                composed: true
+              });
+              return el.dispatchEvent(event);
+            }`
+          });
+        }
       }
     };
   }
