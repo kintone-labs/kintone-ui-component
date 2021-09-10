158385c88d063b45e8cd34627b00bf1b57a67fcb
trung-doan <trung-doan@cybozu.vn>
2021-09-10 13:29:36 +0700

    create base calendar body index

---
+++ src/base/datetime/calendar/body/index.ts
@@ -0,0 +1,41 @@
+import { html, property, query, svg } from "lit-element";
+import { KucBase } from "../../../kuc-base";
+
+export class BaseDateTimeMenu extends KucBase {
+  render() {
+    return html`
+      <div>CALENDAR BODY</div>
+    `;
+  }
+
+  private _getStyleTagTemplate() {
+    return html`
+      <style>
+        kuc-base-datetime-calendar-body,
+        kuc-base-datetime-calendar-body *,
+        :lang(en) kuc-base-datetime-calendar-body,
+        :lang(en) kuc-base-datetime-calendar-body * {
+          font-family: "HelveticaNeueW02-45Ligh", Arial,
+            "Hiragino Kaku Gothic ProN", Meiryo, sans-serif;
+        }
+        :lang(ja) kuc-base-datetime-calendar-body,
+        :lang(ja) kuc-base-datetime-calendar-body * {
+          font-family: "メイリオ", "Hiragino Kaku Gothic ProN", Meiryo,
+            sans-serif;
+        }
+        :lang(zh) kuc-base-datetime-calendar-body,
+        :lang(zh) kuc-base-datetime-calendar-body * {
+          font-family: "微软雅黑", "Microsoft YaHei", "新宋体", NSimSun, STHeiti,
+            Hei, "Heiti SC", sans-serif;
+        }
+      </style>
+    `;
+  }
+}
+
+if (!window.customElements.get("kuc-base-datetime-calendar-body")) {
+  window.customElements.define(
+    "kuc-base-datetime-calendar-body",
+    BaseDateTimeMenu
+  );
+}
