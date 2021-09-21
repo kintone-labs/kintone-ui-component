47765fcf62698012e0cb6fa2b64056b42ac793a7
ldong <ldong@cybozu.cn>
2021-09-21 13:29:44 +0800

    Calendar header: modify unit test code, change the function order

--- src/base/datetime/calendar/header/index.ts
+++ src/base/datetime/calendar/header/index.ts
@@ -19,25 +19,180 @@ export class BaseDateTimeCalendarHeader extends KucBase {
 
   private _locale = en;
 
-  private _getLocale(language: string) {
-    switch (language) {
-      case "en":
-        return en;
-      case "zh":
-        return zh;
-      case "ja":
-        return ja;
-      default:
-        return en;
-    }
-  }
-
   update(changedProperties: PropertyValues) {
     changedProperties.forEach((_oldValue, propName) => {
       propName === "language" &&
         (this._locale = this._getLocale(this.language));
     });
     super.update(changedProperties);
+  }
+
+  render() {
+    return html`
+      ${this._getStyleTagTemplate()}
+      <div class="kuc-base-datetime-calendar-header__group">
+        <button
+          aria-label="previous month"
+          type="button"
+          class="kuc-base-datetime-calendar-header__group__button kuc-base-datetime-calendar-header__group__button-previous-month"
+          @click=${this._handleClickCalendarPrevMonthBtn}
+        >
+          ${this._getLeftArrowIconSvgTemplate()}
+        </button>
+        <span class="kuc-base-datetime-calendar-header__group__center"
+          >${this._getYearMonthTemplate()}</span
+        >
+        <button
+          aria-label="next month"
+          type="button"
+          class="kuc-base-datetime-calendar-header__group__button kuc-base-datetime-calendar-header__group__button-next-month"
+          @click=${this._handleClickCalendarNextMonthBtn}
+        >
+          ${this._getRightArrowIconSvgTemplate()}
+        </button>
+      </div>
+    `;
+  }
+
+  private _getStyleTagTemplate() {
+    return html`
+      <style>
+        kuc-base-datetime-calendar-header,
+        kuc-base-datetime-calendar-header *,
+        :lang(en) kuc-base-datetime-calendar-header,
+        :lang(en) kuc-base-datetime-calendar-header * {
+          font-family: "HelveticaNeueW02-45Ligh", Arial,
+            "Hiragino Kaku Gothic ProN", Meiryo, sans-serif;
+        }
+        :lang(ja) kuc-base-datetime-calendar-header,
+        :lang(ja) kuc-base-datetime-calendar-header * {
+          font-family: "メイリオ", "Hiragino Kaku Gothic ProN", Meiryo,
+            sans-serif;
+        }
+        :lang(zh) kuc-base-datetime-calendar-header,
+        :lang(zh) kuc-base-datetime-calendar-header * {
+          font-family: "微软雅黑", "Microsoft YaHei", "新宋体", NSimSun, STHeiti,
+            Hei, "Heiti SC", sans-serif;
+        }
+        .kuc-base-datetime-calendar-header__group {
+          display: flex;
+          align-items: center;
+          box-sizing: border-box;
+          border-bottom: 1px solid #e3e7e8;
+          padding: 0;
+          white-space: nowrap;
+          width: 272px;
+          height: 44px;
+        }
+        .kuc-base-datetime-calendar-header__group__button {
+          background: transparent;
+          border: none;
+          cursor: pointer;
+          outline: none;
+          width: 38px;
+          height: 32px;
+          padding: 1px;
+          margin: 0;
+          text-align: center;
+        }
+        .kuc-base-datetime-calendar-header__group__button:focus {
+          padding: 1px;
+          border: 1px solid #e3e7e8;
+          border-radius: 4px;
+          outline: none;
+        }
+        .kuc-base-datetime-calendar-header__group__button-icon {
+          vertical-align: middle;
+        }
+        .kuc-base-datetime-calendar-header__group__center {
+          width: 196px;
+          text-align: center;
+        }
+      </style>
+    `;
+  }
+
+  private _getLeftArrowIconSvgTemplate() {
+    return svg`
+      <svg
+        class="kuc-base-datetime-calendar-header__group__button-icon"
+        width="9"
+        height="14"
+        viewBox="0 0 9 14" 
+        fill="none" 
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path 
+          fill-rule="evenodd" 
+          clip-rule="evenodd" 
+          d="M3.06077 7L8.53044 1.53033L7.46978 0.469666L0.939453 7L7.46978 13.5303L8.53044 12.4697L3.06077 7Z" 
+          fill="#888888"
+        />
+      </svg>`;
+  }
+
+  private _getRightArrowIconSvgTemplate() {
+    return svg`
+      <svg
+        class="kuc-base-datetime-calendar-header__group__button-icon"
+        width="9" 
+        height="14" 
+        viewBox="0 0 9 14" 
+        fill="none" 
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path 
+          fill-rule="evenodd" 
+          clip-rule="evenodd" 
+          d="M5.93923 7L0.469557 1.53033L1.53022 0.469666L8.06055 7L1.53022 13.5303L0.469557 12.4697L5.93923 7Z"
+          fill="#888888"
+        />
+      </svg>`;
+  }
+
+  private _getYearTemplate() {
+    const yearSelectPostfix = this._locale.YEAR_SELECT_POSTFIX;
+    return html`
+      <select
+        class="kuc-base-datetime-calendar-header__group__year"
+        @change="${this._handleChangeCalendarHeaderYearSelect}"
+      >
+        ${this._getYearSelectOptions().map((year: number) => {
+          return html`
+            <option ?selected="${this.year === year}" value="${year}"
+              >${year}${yearSelectPostfix}</option
+            >
+          `;
+        })}
+      </select>
+    `;
+  }
+
+  private _getMonthTemplate() {
+    return html`
+      <select
+        class="kuc-base-datetime-calendar-header__group__month"
+        @change="${this._handleChangeCalendarHeaderMonthSelect}"
+      >
+        ${this._locale.MONTHS_SELECT.map((month: string, index: number) => {
+          return html`
+            <option ?selected="${this.month === index + 1}" value="${index + 1}"
+              >${month}</option
+            >
+          `;
+        })}
+      </select>
+    `;
+  }
+
+  private _getYearMonthTemplate() {
+    return this.language === "zh" || this.language === "ja"
+      ? html`
+          ${this._getYearTemplate()}${this._getMonthTemplate()}
+        `
+      : html`
+          ${this._getMonthTemplate()}${this._getYearTemplate()}
+        `;
   }
 
   private _handleClickCalendarPrevMonthBtn(event: MouseEvent) {
@@ -98,172 +253,17 @@ export class BaseDateTimeCalendarHeader extends KucBase {
     return yearSelectOptions;
   }
 
-  private _getYearTemplate() {
-    const yearSelectPostfix = this._locale.YEAR_SELECT_POSTFIX;
-    return html`
-      <select
-        class="kuc-base-datetime-calendar-header__group__year"
-        @change="${this._handleChangeCalendarHeaderYearSelect}"
-      >
-        ${this._getYearSelectOptions().map((year: number) => {
-          return html`
-            <option ?selected="${this.year === year}" value="${year}"
-              >${year}${yearSelectPostfix}</option
-            >
-          `;
-        })}
-      </select>
-    `;
-  }
-
-  private _getMonthTemplate() {
-    return html`
-      <select
-        class="kuc-base-datetime-calendar-header__group__month"
-        @change="${this._handleChangeCalendarHeaderMonthSelect}"
-      >
-        ${this._locale.MONTHS_SELECT.map((month: string, index: number) => {
-          return html`
-            <option ?selected="${this.month === index + 1}" value="${index + 1}"
-              >${month}</option
-            >
-          `;
-        })}
-      </select>
-    `;
-  }
-
-  private _getYearMonthTemplate() {
-    return this.language === "zh" || this.language === "ja"
-      ? html`
-          ${this._getYearTemplate()}${this._getMonthTemplate()}
-        `
-      : html`
-          ${this._getMonthTemplate()}${this._getYearTemplate()}
-        `;
-  }
-
-  render() {
-    return html`
-      ${this._getStyleTagTemplate()}
-      <div class="kuc-base-datetime-calendar-header__group">
-        <button
-          aria-label="previous month"
-          type="button"
-          class="kuc-base-datetime-calendar-header__group__button kuc-base-datetime-calendar-header__group__button-previous-month"
-          @click=${this._handleClickCalendarPrevMonthBtn}
-        >
-          ${this._getLeftArrowIconSvgTemplate()}
-        </button>
-        <span class="kuc-base-datetime-calendar-header__group__center"
-          >${this._getYearMonthTemplate()}</span
-        >
-        <button
-          aria-label="next month"
-          type="button"
-          class="kuc-base-datetime-calendar-header__group__button kuc-base-datetime-calendar-header__group__button-next-month"
-          @click=${this._handleClickCalendarNextMonthBtn}
-        >
-          ${this._getRightArrowIconSvgTemplate()}
-        </button>
-      </div>
-    `;
-  }
-
-  private _getLeftArrowIconSvgTemplate() {
-    return svg`
-      <svg
-        class="kuc-base-datetime-calendar-header__group__button-icon"
-        width="9"
-        height="14"
-        viewBox="0 0 9 14" 
-        fill="none" 
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path 
-          fill-rule="evenodd" 
-          clip-rule="evenodd" 
-          d="M3.06077 7L8.53044 1.53033L7.46978 0.469666L0.939453 7L7.46978 13.5303L8.53044 12.4697L3.06077 7Z" 
-          fill="#888888"
-        />
-      </svg>`;
-  }
-
-  private _getRightArrowIconSvgTemplate() {
-    return svg`
-      <svg
-        class="kuc-base-datetime-calendar-header__group__button-icon"
-        width="9" 
-        height="14" 
-        viewBox="0 0 9 14" 
-        fill="none" 
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path 
-          fill-rule="evenodd" 
-          clip-rule="evenodd" 
-          d="M5.93923 7L0.469557 1.53033L1.53022 0.469666L8.06055 7L1.53022 13.5303L0.469557 12.4697L5.93923 7Z"
-          fill="#888888"
-        />
-      </svg>`;
-  }
-
-  private _getStyleTagTemplate() {
-    return html`
-      <style>
-        kuc-base-datetime-calendar-header,
-        kuc-base-datetime-calendar-header *,
-        :lang(en) kuc-base-datetime-calendar-header,
-        :lang(en) kuc-base-datetime-calendar-header * {
-          font-family: "HelveticaNeueW02-45Ligh", Arial,
-            "Hiragino Kaku Gothic ProN", Meiryo, sans-serif;
-        }
-        :lang(ja) kuc-base-datetime-calendar-header,
-        :lang(ja) kuc-base-datetime-calendar-header * {
-          font-family: "メイリオ", "Hiragino Kaku Gothic ProN", Meiryo,
-            sans-serif;
-        }
-        :lang(zh) kuc-base-datetime-calendar-header,
-        :lang(zh) kuc-base-datetime-calendar-header * {
-          font-family: "微软雅黑", "Microsoft YaHei", "新宋体", NSimSun, STHeiti,
-            Hei, "Heiti SC", sans-serif;
-        }
-        .kuc-base-datetime-calendar-header__group {
-          display: flex;
-          align-items: center;
-          box-sizing: border-box;
-          border-bottom: 1px solid #e3e7e8;
-          padding: 0;
-          white-space: nowrap;
-          width: 272px;
-          height: 44px;
-        }
-        .kuc-base-datetime-calendar-header__group__button {
-          background: transparent;
-          border: none;
-          cursor: pointer;
-          outline: none;
-          width: 38px;
-          height: 32px;
-          padding: 1px;
-          margin: 0;
-          text-align: center;
-        }
-        .kuc-base-datetime-calendar-header__group__button:focus {
-          padding: 1px;
-          border: 1px solid #e3e7e8;
-          border-radius: 4px;
-          outline: none;
-        }
-        .kuc-base-datetime-calendar-header__group__button-icon {
-          vertical-align: middle;
-        }
-        .kuc-base-datetime-calendar-header__group__center {
-          width: 196px;
-          text-align: center;
-        }
-      </style>
-    `;
+  private _getLocale(language: string) {
+    switch (language) {
+      case "en":
+        return en;
+      case "zh":
+        return zh;
+      case "ja":
+        return ja;
+      default:
+        return en;
+    }
   }
 }
 
--- src/base/datetime/calendar/header/test/changeEvent.test.ts
+++ src/base/datetime/calendar/header/test/changeEvent.test.ts
@@ -4,14 +4,14 @@ import "../index";
 describe("BaseDateTimeCalendarHeader", () => {
   describe("kuc:calendar-header-change event", () => {
     it("should be triggered when click the previous month button", async () => {
+      let triggeredEvent: any = null;
       const container = document.createElement(
         "kuc-base-datetime-calendar-header"
       );
       container.setAttribute("month", "6");
       container.setAttribute("year", "2021");
       container.addEventListener("kuc:calendar-header-change", (event: any) => {
-        expect(event.type).to.equal("kuc:calendar-header-change");
-        expect(event.detail.value).to.equal("2021-5");
+        triggeredEvent = event;
       });
 
       const el = await fixture(container);
@@ -19,17 +19,20 @@ describe("BaseDateTimeCalendarHeader", () => {
         ".kuc-base-datetime-calendar-header__group__button-previous-month"
       ) as HTMLButtonElement;
       buttonEl.click();
+
+      expect(triggeredEvent.type).to.equal("kuc:calendar-header-change");
+      expect(triggeredEvent.detail.value).to.equal("2021-5");
     });
 
     it("should be return DECEMBER of previous year when month is 1 and click the previous month button", async () => {
+      let triggeredEvent: any = null;
       const container = document.createElement(
         "kuc-base-datetime-calendar-header"
       );
       container.setAttribute("month", "1");
       container.setAttribute("year", "2021");
       container.addEventListener("kuc:calendar-header-change", (event: any) => {
-        expect(event.type).to.equal("kuc:calendar-header-change");
-        expect(event.detail.value).to.equal("2020-12");
+        triggeredEvent = event;
       });
 
       const el = await fixture(container);
@@ -37,17 +40,20 @@ describe("BaseDateTimeCalendarHeader", () => {
         ".kuc-base-datetime-calendar-header__group__button-previous-month"
       ) as HTMLButtonElement;
       buttonEl.click();
+
+      expect(triggeredEvent.type).to.equal("kuc:calendar-header-change");
+      expect(triggeredEvent.detail.value).to.equal("2020-12");
     });
 
     it("should be triggered when click the next month button", async () => {
+      let triggeredEvent: any = null;
       const container = document.createElement(
         "kuc-base-datetime-calendar-header"
       );
       container.setAttribute("month", "6");
       container.setAttribute("year", "2021");
       container.addEventListener("kuc:calendar-header-change", (event: any) => {
-        expect(event.type).to.equal("kuc:calendar-header-change");
-        expect(event.detail.value).to.equal("2021-7");
+        triggeredEvent = event;
       });
 
       const el = await fixture(container);
@@ -55,17 +61,20 @@ describe("BaseDateTimeCalendarHeader", () => {
         ".kuc-base-datetime-calendar-header__group__button-next-month"
       ) as HTMLButtonElement;
       buttonEl.click();
+
+      expect(triggeredEvent.type).to.equal("kuc:calendar-header-change");
+      expect(triggeredEvent.detail.value).to.equal("2021-7");
     });
 
     it("should be return JANUARY of next year when month is 12 and click the next month button", async () => {
+      let triggeredEvent: any = null;
       const container = document.createElement(
         "kuc-base-datetime-calendar-header"
       );
       container.setAttribute("month", "12");
       container.setAttribute("year", "2021");
       container.addEventListener("kuc:calendar-header-change", (event: any) => {
-        expect(event.type).to.equal("kuc:calendar-header-change");
-        expect(event.detail.value).to.equal("2022-1");
+        triggeredEvent = event;
       });
 
       const el = await fixture(container);
@@ -73,16 +82,20 @@ describe("BaseDateTimeCalendarHeader", () => {
         ".kuc-base-datetime-calendar-header__group__button-next-month"
       ) as HTMLButtonElement;
       buttonEl.click();
+
+      expect(triggeredEvent.type).to.equal("kuc:calendar-header-change");
+      expect(triggeredEvent.detail.value).to.equal("2022-1");
     });
 
     it("should be triggered when select a year", async () => {
+      let triggeredEvent: any = null;
       const container = document.createElement(
         "kuc-base-datetime-calendar-header"
       );
       container.setAttribute("month", "6");
       container.setAttribute("year", "2021");
       container.addEventListener("kuc:calendar-header-change", (event: any) => {
-        expect(event.type).to.equal("kuc:calendar-header-change");
+        triggeredEvent = event;
       });
 
       const el = await fixture(container);
@@ -90,16 +103,19 @@ describe("BaseDateTimeCalendarHeader", () => {
         ".kuc-base-datetime-calendar-header__group__year"
       ) as HTMLSelectElement;
       selectEl.dispatchEvent(new Event("change"));
+
+      expect(triggeredEvent.type).to.equal("kuc:calendar-header-change");
     });
 
     it("should be triggered when select a month", async () => {
+      let triggeredEvent: any = null;
       const container = document.createElement(
         "kuc-base-datetime-calendar-header"
       );
       container.setAttribute("month", "6");
       container.setAttribute("year", "2021");
       container.addEventListener("kuc:calendar-header-change", (event: any) => {
-        expect(event.type).to.equal("kuc:calendar-header-change");
+        triggeredEvent = event;
       });
 
       const el = await fixture(container);
@@ -107,6 +123,8 @@ describe("BaseDateTimeCalendarHeader", () => {
         ".kuc-base-datetime-calendar-header__group__month"
       ) as HTMLSelectElement;
       selectEl.dispatchEvent(new Event("change"));
+
+      expect(triggeredEvent.type).to.equal("kuc:calendar-header-change");
     });
   });
 });
