55710f8443c0b21bd49db264e942ba0a74ea1419
trung-doan <trung-doan@cybozu.vn>
2021-10-13 16:39:40 +0700

    use lit-html for storybook

--- src/base/datetime/calendar/body/index.stories.js
+++ src/base/datetime/calendar/body/index.stories.js
@@ -1,5 +1,5 @@
 import "./index.ts";
-import { html } from "lit";
+import { html } from "lit-html";
 
 export default {
   title: "base/datetime/calendar/body",
--- src/base/datetime/calendar/header/dropdown/year/index.stories.js
+++ src/base/datetime/calendar/header/dropdown/year/index.stories.js
@@ -1,4 +1,4 @@
-import { html } from "lit";
+import { html } from "lit-html";
 
 import "./index.ts";
 
