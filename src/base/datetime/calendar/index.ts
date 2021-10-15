8938fb3a9d80d50ad137fa88eb4f5cb3fc665cde
trung-doan <trung-doan@cybozu.vn>
2021-10-15 15:53:31 +0700

    update css

--- src/base/datetime/calendar/index.ts
+++ src/base/datetime/calendar/index.ts
@@ -33,7 +33,7 @@ export class BaseDateTimeCalendar extends KucBase {
           box-sizing: border-box;
           width: 336px;
           padding: 32px 32px 24px;
-          background: #fff;
+          background: #ffffff;
           box-shadow: 0 0 8px 2px rgb(0 0 0 / 10%);
           text-align: center;
           font-size: 13px;
