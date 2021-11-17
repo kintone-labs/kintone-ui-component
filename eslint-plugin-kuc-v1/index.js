d47005bc73b9f22e118c764ac31b05600bdb03c0
Dong Lin <ldong@cybozu.cn>
2021-11-17 10:48:06 +0800

    add eslint custom rule "validator-in-update"

--- eslint-plugin-kuc-v1/index.js
+++ eslint-plugin-kuc-v1/index.js
@@ -1,11 +1,13 @@
 const noCreateRenderRootFunction = require("./no-create-render-root-function");
 const noUsingCustomEvent = require("./no-using-custom-event");
 const noUsingGenerateGUIDFunction = require("./no-using-generate-guid-function");
+const validatorInUpdate = require("./validator-in-update");
 
 module.exports = {
   rules: {
     "no-create-render-root-function": noCreateRenderRootFunction,
     "no-using-custom-event": noUsingCustomEvent,
-    "no-using-generate-guid-function": noUsingGenerateGUIDFunction
+    "no-using-generate-guid-function": noUsingGenerateGUIDFunction,
+    "validator-in-update": validatorInUpdate
   }
 };
---
+++ eslint-plugin-kuc-v1/validator-in-update.js
@@ -0,0 +1,45 @@
+module.exports = {
+  create: function(context) {
+    return {
+      ClassDeclaration: function(node) {
+        const superClass = node.superClass.name;
+        if (superClass !== "KucBase") return;
+
+        const nodeBody = node.body;
+        if (nodeBody.type !== "ClassBody") return;
+
+        const sourceCode = context.getSourceCode();
+        const pattern = "validate[a-zA-z]+\\(";
+        const regex = new RegExp(pattern);
+        if (!regex.test(sourceCode.getText())) return;
+
+        nodeBody.body.forEach(body => {
+          if (
+            body.type === "MethodDefinition" &&
+            body.key.type === "Identifier" &&
+            !body.computed
+          ) {
+            const functionSourceCode = sourceCode.getText(body.value);
+            if (
+              (body.key.name === "update" && !regex.test(functionSourceCode)) ||
+              (body.kind !== "constructor" &&
+                body.key.name !== "update" &&
+                regex.test(functionSourceCode))
+            ) {
+              context.report({
+                node: body,
+                message: `Please call validator function inside update() lifecycle
+                ex: update(changedProperties: PropertyValues) {
+                      if (changedProperties.has("items")) {
+                        validateItems(this.items);
+                      }
+                      super.update(changedProperties);
+                    }`
+              });
+            }
+          }
+        });
+      }
+    };
+  }
+};
