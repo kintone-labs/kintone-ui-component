c2dba210e5513bade26154e4b1e46db704740761
voduchau <h002250@cybozu.onmicrosoft.com>
2021-12-05 16:19:05 +0700

    implement unit test for base/time

---
+++ src/base/datetime/time/test/hour12.test.ts
@@ -0,0 +1,59 @@
+import { elementUpdated, expect, fixture } from "@open-wc/testing";
+import "../index";
+
+describe("BaseTime", () => {
+  describe("hour12", () => {
+    it("should be using 12-hour clock when not assigning", async () => {
+      const container = document.createElement("kuc-base-time");
+      container.setAttribute("value", "13:15");
+      const el = await fixture(container);
+      const inputHourEl = el.querySelector(
+        ".kuc-base-time__group__hours"
+      ) as HTMLInputElement;
+      const inputMinuteEl = el.querySelector(
+        ".kuc-base-time__group__minutes"
+      ) as HTMLInputElement;
+
+      expect(inputHourEl.value).to.be.equal("13");
+      expect(inputMinuteEl.value).to.be.equal("15");
+    });
+
+    it("should be using 12-hour clock when assigned true by setter", async () => {
+      const container = document.createElement("kuc-base-time");
+      container.setAttribute("hour12", "true");
+      container.setAttribute("value", "13:15");
+      const el = await fixture(container);
+      const inputHourEl = el.querySelector(
+        ".kuc-base-time__group__hours"
+      ) as HTMLInputElement;
+      const inputMinuteEl = el.querySelector(
+        ".kuc-base-time__group__minutes"
+      ) as HTMLInputElement;
+      const inputSuffixEl = el.querySelector(
+        ".kuc-base-time__group__suffix"
+      ) as HTMLInputElement;
+
+      expect(inputHourEl.value).to.be.equal("01");
+      expect(inputMinuteEl.value).to.be.equal("15");
+      expect(inputSuffixEl.value).to.be.equal("PM");
+    });
+
+    it("should be using 24-hour clock when change to false", async () => {
+      const container = document.createElement("kuc-base-time");
+      container.setAttribute("value", "13:15");
+      container.setAttribute("hour12", "true");
+      const el = await fixture(container);
+      el.removeAttribute("hour12");
+      await elementUpdated(el);
+      const inputHourEl = el.querySelector(
+        ".kuc-base-time__group__hours"
+      ) as HTMLInputElement;
+      const inputMinuteEl = el.querySelector(
+        ".kuc-base-time__group__minutes"
+      ) as HTMLInputElement;
+
+      expect(inputHourEl.value).to.be.equal("13");
+      expect(inputMinuteEl.value).to.be.equal("15");
+    });
+  });
+});
