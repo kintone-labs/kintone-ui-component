c6bf5f7cd89f19fc69a6dca76c8e2f12e8e373c7
Nguyen Hoang Anh <nmanhnit@gmail.com>
2022-01-04 10:21:33 +0700

    created unit test for date time picker

---
+++ src/datetime-picker/test/hour12.test.ts
@@ -0,0 +1,19 @@
+import { expect, fixture, elementUpdated } from "@open-wc/testing";
+import { DateTimePicker } from "../index";
+
+describe("DatePicker", () => {
+  describe("error", () => {
+    it("should be changed hour when change time format to 12 hours", async () => {
+      const container = new DateTimePicker({ value: "2021-12-22T14:30:00" });
+      container.hour12 = true;
+
+      const el = await fixture(container);
+      await elementUpdated(container);
+      await elementUpdated(el);
+      const inputHourEl = el.querySelector(
+        ".kuc-base-time__group__hours"
+      ) as HTMLInputElement;
+      expect(inputHourEl.value).to.equal("02");
+    });
+  });
+});
---
+++ src/datetime-picker/test/value.test.ts
@@ -0,0 +1,99 @@
+import { expect, fixture, elementUpdated } from "@open-wc/testing";
+import { DateTimePicker } from "../index";
+import { getTodayStringByLocale } from "../../base/datetime/utils";
+
+describe("DateTimePicker", () => {
+  describe("value", () => {
+    it("should be empty string when not assigning", async () => {
+      const container = new DateTimePicker();
+      const el = await fixture(container);
+      const inputEl = el.querySelector(
+        ".kuc-base-date__input"
+      ) as HTMLInputElement;
+      const inputHourEl = el.querySelector(
+        ".kuc-base-time__group__hours"
+      ) as HTMLInputElement;
+      const inputMinuteEl = el.querySelector(
+        ".kuc-base-time__group__minutes"
+      ) as HTMLInputElement;
+
+      expect(inputEl.value).to.be.equal("");
+      expect(inputHourEl.value).to.be.equal("");
+      expect(inputMinuteEl.value).to.be.equal("");
+    });
+
+    it('should be "12/12/2021" when assigning on constructor', async () => {
+      const container = new DateTimePicker({ value: "2021-12-12" });
+      const el = await fixture(container);
+      const inputEl = el.querySelector(
+        ".kuc-base-date__input"
+      ) as HTMLInputElement;
+      const inputHourEl = el.querySelector(
+        ".kuc-base-time__group__hours"
+      ) as HTMLInputElement;
+      const inputMinuteEl = el.querySelector(
+        ".kuc-base-time__group__minutes"
+      ) as HTMLInputElement;
+
+      expect(inputEl.value).to.be.equal("12/12/2021");
+      expect(inputHourEl.value).to.be.equal("00");
+      expect(inputMinuteEl.value).to.be.equal("00");
+    });
+
+    it('should be "12/13/2021" when changed to "12/13/2021" by setter', async () => {
+      const container = new DateTimePicker({ value: "2021-12-12" });
+      container.value = "2021-12-13";
+      const el = await fixture(container);
+      el.setAttribute("value", "13:15");
+      const inputEl = el.querySelector(
+        ".kuc-base-date__input"
+      ) as HTMLInputElement;
+
+      expect(inputEl.value).to.be.equal("12/13/2021");
+    });
+
+    it("should throw error when set invalid value", async () => {
+      const container = new DateTimePicker({ value: "2021-02-28T09:30:61" });
+      try {
+        const el = await fixture(container);
+      } catch (error) {
+        let errorMessage = "";
+        if (error instanceof Error) {
+          errorMessage = error.message;
+        }
+        expect(errorMessage).to.be.equal("Format is not valid.");
+      }
+    });
+
+    it("should be today value when press today button on calendar", async () => {
+      const container = new DateTimePicker({
+        value: "2021-02-28T09:30:00",
+        language: "ja"
+      });
+      const el = await fixture(container);
+      const inputDateEl = el.querySelector(
+        ".kuc-base-date__input"
+      ) as HTMLInputElement;
+      inputDateEl.click();
+      await elementUpdated(container);
+      await elementUpdated(el);
+
+      const todayBtnEl = el.querySelector(
+        ".kuc-base-datetime-calendar-footer__group__button--today"
+      ) as HTMLButtonElement;
+      todayBtnEl.click();
+      await elementUpdated(el);
+      const todayStr = getTodayStringByLocale();
+      expect(inputDateEl.value).to.be.equal(todayStr);
+
+      const inputHourEl = el.querySelector(
+        ".kuc-base-time__group__hours"
+      ) as HTMLInputElement;
+      const inputMinuteEl = el.querySelector(
+        ".kuc-base-time__group__minutes"
+      ) as HTMLInputElement;
+      expect(inputHourEl.value).to.be.equal("09");
+      expect(inputMinuteEl.value).to.be.equal("30");
+    });
+  });
+});
