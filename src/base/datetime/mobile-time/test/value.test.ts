57a9868cfa2443e5d4773449f6442e3b36a2a594
voduchau <h002250@cybozu.onmicrosoft.com>
2022-01-19 10:33:42 +0700

    fix review

--- src/base/datetime/mobile-time/test/value.test.ts
+++ src/base/datetime/mobile-time/test/value.test.ts
@@ -1,10 +1,10 @@
-import { expect, fixture } from "@open-wc/testing";
+import { expect, fixture, elementUpdated } from "@open-wc/testing";
 import { BaseMobileTime } from "../index";
 
 describe("BaseMobileTime", () => {
   describe("value", () => {
     it("should be empty string when not assigning", async () => {
-      const container = new BaseMobileTime();
+      const container = document.createElement("kuc-base-mobile-time");
       const el = await fixture(container);
       const selectHourEl = el.querySelector(
         ".kuc-base-mobile-time__group__hours"
@@ -18,7 +18,8 @@ describe("BaseMobileTime", () => {
     });
 
     it('should be "13:15" when assigned "13:15" by setter', async () => {
-      const container = new BaseMobileTime({ value: "13:15" });
+      const container = document.createElement("kuc-base-mobile-time");
+      container.setAttribute("value", "13:15");
       const el = await fixture(container);
       const selectHourEl = el.querySelector(
         ".kuc-base-mobile-time__group__hours"
@@ -32,9 +33,12 @@ describe("BaseMobileTime", () => {
     });
 
     it('should be "13:15" when changed to "13:15" by setter', async () => {
-      const container = new BaseMobileTime({ value: "11:15" });
-      container.value = "13:15";
+      const container = document.createElement("kuc-base-mobile-time");
+      container.setAttribute("value", "11:15");
       const el = await fixture(container);
+      container.setAttribute("value", "13:15");
+      await elementUpdated(container);
+
       el.setAttribute("value", "13:15");
       const selectHourEl = el.querySelector(
         ".kuc-base-mobile-time__group__hours"
@@ -48,7 +52,7 @@ describe("BaseMobileTime", () => {
     });
 
     it("should throw error when set invalid value", async () => {
-      const container = new BaseMobileTime({ value: "12,12" });
+      const container = document.createElement("kuc-base-mobile-time");
       try {
         const el = await fixture(container);
       } catch (error) {
