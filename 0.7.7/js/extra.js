5c4d37bc43b80ab76c10168193bc702a1efcb573
Nguyen Hoang Anh <nmanhnit@gmail.com>
2022-01-21 13:29:41 +0700

    Deployed a46e2221 to 0.7.7 with MkDocs 1.1.2 and mike 0.3.5

---
+++ 0.7.7/js/extra.js
@@ -0,0 +1,65 @@
+(function() {
+
+    function createTabNavEl(headerEls) {
+        var tabNavEl = document.createElement('div');
+        tabNavEl.classList.add('tab-nav');
+        var i = 0;
+        while (headerEls.length > 0) {
+            headerEls[0].dataset.index = i;
+            headerEls[0].classList.add("tab-btn");
+            tabNavEl.appendChild(headerEls[0]);
+            i++;
+        }
+        return tabNavEl;
+    }
+
+    function createTabSessionEl(sessionEls) {
+        var tabSessionEl = document.createElement('div');
+        tabSessionEl.classList.add('tab-session');
+        var i = 0;
+        while (sessionEls.length > 0) {
+            sessionEls[0].classList.add('index-' + i);
+            tabSessionEl.appendChild(sessionEls[0]);
+            i++;
+        }
+        return tabSessionEl;
+    }
+
+    function hideActivatedTab(headerEls, sessionEls) {
+        for (var i = 0; i < headerEls.length; i++) {
+            headerEls[i].classList.remove('active');
+        }
+        for (var i = 0; i < sessionEls.length; i++) {
+            sessionEls[i].classList.remove('active');
+        }
+    }
+
+    function showTabLayout(containerEl) {
+        var headerEls = containerEl.getElementsByTagName('strong');
+        var tabNavEl = createTabNavEl(headerEls);
+        containerEl.appendChild(tabNavEl);
+
+        var sessionEls = containerEl.getElementsByTagName('pre');
+        var tabSessionEl = createTabSessionEl(sessionEls);
+        containerEl.appendChild(tabSessionEl);
+
+        tabNavEl.addEventListener('click', function(e) {
+            var target = e.target;
+            if (!target.classList.contains('tab-btn')) {
+                return;
+            }
+
+            hideActivatedTab(headerEls, sessionEls);
+            target.classList.add('active');
+            sessionEls[target.dataset.index].classList.add('active');
+        });
+
+        headerEls[0].classList.add('active');
+        sessionEls[0].classList.add('active');
+    }
+
+    var tabContainers = document.getElementsByClassName('tab-container');
+    for (var i = 0; i < tabContainers.length; i++) {
+        showTabLayout(tabContainers[i]);
+    }
+})();
