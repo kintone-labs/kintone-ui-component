a8cf86019aeae9ba494fad15a3a212991428244f
voduchau <h002250@cybozu.onmicrosoft.com>
2022-01-25 09:45:42 +0700

    fix review

--- src/base/mobile-label/index.ts
+++ src/base/mobile-label/index.ts
@@ -1,6 +1,6 @@
 import { html } from "lit";
 import { property } from "lit/decorators.js";
-import { KucBase } from "../../base/kuc-base";
+import { KucBase } from "../kuc-base";
 
 export class BaseMobileLabel extends KucBase {
   @property({ type: Boolean }) requiredIcon = false;
