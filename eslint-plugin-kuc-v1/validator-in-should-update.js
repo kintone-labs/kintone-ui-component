9e1ca6dbb9996c22d22075c42c7edc90956f3aa9
Nguyen Hoang Anh <nmanhnit@gmail.com>
2022-03-28 15:41:31 +0700

    update eslint rule

---
+++ eslint-plugin-kuc-v1/validator-in-should-update.js
@@ -0,0 +1,52 @@
+module.exports = {
+  create: function(context) {
+    return {
+      ClassDeclaration: function(node) {
+        const superClass = node.superClass.name;
+        if (superClass !== "KucBase") return;
+
+        const nodeBody = node.body;
+        if (nodeBody.type !== "ClassBody") return;
+
+        const sourceCode = context.getSourceCode();
+        const baseValidatorPattern = '\\./base/validator"';
+        const baseValidatorRegex = new RegExp(baseValidatorPattern);
+        if (!baseValidatorRegex.test(sourceCode.getText(node.parent.parent)))
+          return;
+
+        const pattern = "validate[a-zA-z]+\\(";
+        const regex = new RegExp(pattern);
+        if (!regex.test(sourceCode.getText())) return;
+
+        nodeBody.body.forEach(body => {
+          if (
+            body.type === "MethodDefinition" &&
+            body.key.type === "Identifier" &&
+            !body.computed
+          ) {
+            const functionSourceCode = sourceCode.getText(body.value);
+            if (
+              body.kind !== "constructor" &&
+              body.key.name !== "shouldUpdate" &&
+              regex.test(functionSourceCode)
+            ) {
+              context.report({
+                node: body,
+                message: `Please call validator function inside shouldUpdate() lifecycle
+                ex: shouldUpdate(changedProperties: PropertyValues):boolean {
+                      if (changedProperties.has("items")) {
+                        if (!validateItems(this.items)) {
+                          throwErrorAfterUpdateComplete(this, ERROR_MESSAGE.ITEMS.IS_NOT_ARRAY);
+                          return false;
+                        }
+                      }
+                      return true;
+                    }`
+              });
+            }
+          }
+        });
+      }
+    };
+  }
+};
