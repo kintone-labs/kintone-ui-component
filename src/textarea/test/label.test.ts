70a59b560558ceeaa23643a71aece8acd8b654f1
voduchau <h002250@cybozu.onmicrosoft.com>
2022-04-18 13:15:13 +0700

    apply base label/error for text area

--- src/textarea/test/label.test.ts
+++ src/textarea/test/label.test.ts
@@ -13,7 +13,7 @@ describe("TextArea", () => {
       expect(labelEl.hasAttribute("hidden")).to.equal(true);
 
       const labelTextEl = el.querySelector(
-        ".kuc-textarea__group__label__text"
+        ".kuc-base-label__text"
       ) as HTMLSpanElement;
       expect(labelTextEl.textContent).to.equal("");
     });
@@ -28,7 +28,7 @@ describe("TextArea", () => {
       expect(labelEl.hasAttribute("hidden")).to.equal(false);
 
       const labelTextEl = el.querySelector(
-        ".kuc-textarea__group__label__text"
+        ".kuc-base-label__text"
       ) as HTMLSpanElement;
       expect(labelTextEl.textContent).to.equal("options-label");
     });
@@ -46,7 +46,7 @@ describe("TextArea", () => {
       expect(labelEl.hasAttribute("hidden")).to.equal(false);
 
       const labelTextEl = el.querySelector(
-        ".kuc-textarea__group__label__text"
+        ".kuc-base-label__text"
       ) as HTMLSpanElement;
       expect(labelTextEl.textContent).to.equal("replace-label");
     });
