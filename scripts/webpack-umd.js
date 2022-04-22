2d0076bf317fa1c7e08a041406d3499d18a3f9f6
趙华锋 <hfzhao@cybozu.cn>
2022-04-22 17:42:41 +0800

    SSR-1845 update for version conflicts
    add scripts file &  modify the config for webpack
    add version for button

---
+++ scripts/webpack-umd.js
@@ -0,0 +1,49 @@
+const webpack = require("webpack");
+const configuration = require('../webpack.config.js')
+const path = require("path");
+const packageJSON = require("../package.json");
+
+const fs = require("fs");
+configuration.mode= process.argv[2];
+const classNamePattern = /(kuc(-[a-z]+)+__)|(kuc(-[a-z]+)+\\)|(kuc(-[a-z]+)+\>)|(kuc(-[a-z]+)+\")|(kuc(-[a-z]+)+\[)|(kuc(-[a-z]+)+\s)|(kuc(-[a-z]+)+,)/g
+const suffixs = ['\\','\>','__','\"','=',',',';',' ','['];
+const classNameVersion = `-${packageJSON.version.replaceAll('.','-')}`;
+const getChangedValue = (str, version)=>{
+    let changedValue = str;
+    suffixs.forEach(suffix =>{
+        if(changedValue.indexOf(suffix) !== -1){
+            changedValue = changedValue.replace(suffix, version + suffix);
+            return true;
+        };
+    });
+    return changedValue;
+}
+const replaceAllByPattern = (data, pattern, version)=>{
+    let tempData = data;
+    const matchedValues = Array.from(new Set(data.match(pattern)));
+    matchedValues.forEach((matchedValue)=>{
+        // ignore the base file "base/kuc-base"
+        const tempChangedValue =
+            matchedValue === "kuc-base "
+            ? matchedValue
+            : getChangedValue(matchedValue, version);
+        tempData = tempData.replaceAll(matchedValue, tempChangedValue);
+    })
+    return tempData;
+}
+const main = async () => {
+    webpack(configuration , async (error,stats) => {
+        if (error || stats.hasErrors()) {
+            console.error(error || stats.toString({ colors: true }))
+          } else {
+            console.log(stats.toString({ colors: true }))
+            let data = fs.readFileSync(path.resolve(__dirname, "../umd/kuc.min.js")).toString();
+            data = replaceAllByPattern(data,classNamePattern,classNameVersion);
+            fs.writeFileSync(path.resolve(__dirname, "../umd/kuc.min.js"), data);
+        }
+    });
+};
+
+main()
+
+
---
+++ src/button/type.d.ts
@@ -0,0 +1,25 @@
+import { KucBase } from "../base/kuc-base";
+
+declare type ButtonProps = {
+  className?: string;
+  id?: string;
+  text?: string;
+  type?: "normal" | "submit" | "alert";
+  disabled?: boolean;
+  visible?: boolean;
+};
+// eslint-disable-next-line kuc-v1/no-describe-style-tag-inside-html
+declare class Button extends KucBase {
+  className: string;
+  id: string;
+  text: string;
+  type: string;
+  disabled: boolean;
+  visible: boolean;
+  constructor(props?: ButtonProps);
+  private _handleClickButton;
+  private _getButtonColorType;
+  render(): import("lit").TemplateResult<1>;
+  private _getStyleTagTemplate;
+}
+export { Button };
