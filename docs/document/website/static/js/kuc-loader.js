828592d0ddd2fc08a436a50bb2ab90755d46c39e
voduchau <h002250@cybozu.onmicrosoft.com>
2022-04-25 13:48:35 +0700

    wip

--- docs/document/website/static/js/kuc-loader.js
+++ docs/document/website/static/js/kuc-loader.js
@@ -1,12 +1,13 @@
 (function() {
-  window.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener("DOMContentLoaded", () => {
     const docVersion = window.location.pathname.match(/\d+\.\d+\.\d+/);
     const urlVersion = docVersion ? `@${docVersion[0]}` : "";
-    const kucLink = `https://unpkg.com/kintone-ui-component${urlVersion}/umd/kuc.min.js`;
+    // const kucLink = `https://unpkg.com/kintone-ui-component${urlVersion}/umd/kuc.min.js`;
+    const kucLink = `https://127.0.0.1:8000/kuc.min.js`;
 
-    const kucScript = document.createElement('script');
+    const kucScript = document.createElement("script");
     kucScript.onload = function() {
-      const loadedEvent = new Event('kuc:loaded');
+      const loadedEvent = new Event("kuc:loaded");
       document.dispatchEvent(loadedEvent);
     };
 
