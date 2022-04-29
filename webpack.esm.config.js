7f7ae230273dfd53533de5a41b95d77fc334044a
voduchau <h002250@cybozu.onmicrosoft.com>
2022-04-29 10:34:06 +0700

    separate css

--- webpack.esm.config.js
+++ webpack.esm.config.js
@@ -23,6 +23,10 @@ module.exports = {
       {
         test: /\.ts$/,
         use: "ts-loader"
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
