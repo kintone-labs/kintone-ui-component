29ee5460fbb9f526120aae1263ef9aac6469499c
voduchau <h002250@cybozu.onmicrosoft.com>
2022-05-09 11:19:31 +0700

    desktop multichoice & separate css

---
+++ src/multichoice/type.ts
@@ -0,0 +1,38 @@
+import { PropertyValues } from "lit";
+import { KucBase } from "../base/kuc-base";
+
+export declare type Item = {
+  label?: string;
+  value?: string;
+};
+export declare type MultiChoiceProps = {
+  className?: string;
+  error?: string;
+  id?: string;
+  label?: string;
+  value?: string[];
+  selectedIndex?: number[];
+  disabled?: boolean;
+  requiredIcon?: boolean;
+  visible?: boolean;
+  items?: Item[];
+};
+export declare class MultiChoice extends KucBase {
+  className: string;
+  error: string;
+  id: string;
+  label: string;
+  disabled: boolean;
+  requiredIcon: boolean;
+  visible: boolean;
+  items: Item[];
+  selectedIndex: number[];
+  value: string[];
+  constructor(props?: MultiChoiceProps);
+  private _setInitialValue;
+  shouldUpdate(changedProperties: PropertyValues): boolean;
+  willUpdate(changedProperties: PropertyValues): void;
+  // eslint-disable-next-line kuc-v1/super-update
+  update(changedProperties: PropertyValues): void;
+  render(): import("lit").TemplateResult<1>;
+}
