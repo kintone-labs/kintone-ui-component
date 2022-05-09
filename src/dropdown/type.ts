ebb1df42948c3877af59da7fdf8047f2a6c91ad8
voduchau <h002250@cybozu.onmicrosoft.com>
2022-05-09 10:55:12 +0700

    implement dropdown & separate css

---
+++ src/dropdown/type.ts
@@ -0,0 +1,38 @@
+import { PropertyValues } from "lit";
+import { KucBase } from "../base/kuc-base";
+export declare type Item = {
+  label?: string;
+  value?: string;
+};
+export declare type DropdownProps = {
+  className?: string;
+  error?: string;
+  id?: string;
+  label?: string;
+  value?: string;
+  selectedIndex?: number;
+  disabled?: boolean;
+  requiredIcon?: boolean;
+  visible?: boolean;
+  items?: Item[];
+};
+export declare class Dropdown extends KucBase {
+  className: string;
+  error: string;
+  id: string;
+  label: string;
+  value: string;
+  selectedIndex: number;
+  disabled: boolean;
+  requiredIcon: boolean;
+  visible: boolean;
+  items: Item[];
+  constructor(props?: DropdownProps);
+  shouldUpdate(changedProperties: PropertyValues): boolean;
+  willUpdate(changedProperties: PropertyValues): void;
+  // eslint-disable-next-line kuc-v1/super-update
+  update(changedProperties: PropertyValues): void;
+  render(): import("lit").TemplateResult<1>;
+  firstUpdated(): void;
+  updated(): void;
+}
