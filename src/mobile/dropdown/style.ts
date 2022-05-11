a6ded361d8ea8aaa3ab8a241fc5556af3b6a27e7
Nguyen Hoang Anh <nmanhnit@gmail.com>
2022-05-11 13:30:06 +0700

    mobile dropdown

---
+++ src/mobile/dropdown/style.ts
@@ -0,0 +1,97 @@
+export const MOBILE_DROPDOWN_CSS = `
+  kuc-mobile-dropdown,
+  kuc-mobile-dropdown * {
+    font-size: 13px;
+    color: #333333;
+    font-family: "メイリオ", Meiryo, "Hiragino Kaku Gothic ProN",
+      "ヒラギノ角ゴ ProN W3", "ＭＳ Ｐゴシック", "Lucida Grande",
+      "Lucida Sans Unicode", Arial, Verdana, sans-serif;
+  }
+
+  :lang(zh) kuc-mobile-dropdown,
+  :lang(zh) kuc-mobile-dropdown * {
+    font-family: "微软雅黑", "Microsoft YaHei", "新宋体", NSimSun, STHeiti,
+      Hei, "Heiti SC", "Lucida Grande", "Lucida Sans Unicode", Arial,
+      Verdana, sans-serif;
+  }
+
+  kuc-mobile-dropdown {
+    display: inline-block;
+    width: 100%;
+  }
+
+  kuc-mobile-dropdown[hidden] {
+    display: none;
+  }
+
+  .kuc-mobile-dropdown__label {
+    display: inline-block;
+    font-size: 86%;
+    font-weight: bold;
+    line-height: 1.5;
+    padding: 0px;
+    margin: 0 0 4px 0;
+    white-space: nowrap;
+  }
+
+  .kuc-mobile-dropdown__label[hidden] {
+    display: none;
+  }
+
+  .kuc-mobile-dropdown__label__text {
+    text-shadow: 0 1px 0 #ffffff;
+    color: #888888;
+    white-space: normal;
+    font-size: inherit;
+  }
+
+  .kuc-mobile-dropdown__label__required-icon {
+    color: #d01212;
+    left: 3px;
+    position: relative;
+  }
+
+  .kuc-mobile-dropdown__label__required-icon[hidden] {
+    display: none;
+  }
+
+  .kuc-mobile-dropdown__input-form {
+    word-wrap: break-word;
+    min-height: 1em;
+    padding-left: 0.5em;
+    padding-right: 0.5em;
+  }
+
+  .kuc-mobile-dropdown__input-form__select {
+    display: inline-block;
+    border-radius: 0.4em;
+    max-width: 100%;
+  }
+
+  .kuc-mobile-dropdown__input-form__select.kuc--required {
+    border: 1px solid #cf4a38;
+  }
+
+  .kuc-mobile-dropdown__input-form__select__input {
+    min-width: 100px;
+    max-width: 100%;
+  }
+
+  .kuc-mobile-dropdown__input-form__select__input:disabled {
+    color: #999999;
+    -webkit-text-fill-color: #999999;
+    background-color: #d5d7d9;
+    opacity: 1;
+  }
+
+  .kuc-mobile-dropdown__error {
+    line-height: 1.5;
+    color: #000000;
+    background-color: #fdffc9;
+    border: 1px solid #e5db68;
+    border-radius: 0.4em;
+    padding: 0.4em 1em;
+    margin-top: 0.3em;
+    margin-left: 0.5em;
+  }
+`;
