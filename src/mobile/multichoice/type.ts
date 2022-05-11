78cd002e7eebf95ea8f52b32af505f0c3d14128a
Nguyen Hoang Anh <nmanhnit@gmail.com>
2022-05-11 13:46:31 +0700

    mobile multi choice

---
+++ src/mobile/multichoice/type.ts
@@ -0,0 +1,37 @@
+import { PropertyValues } from "lit";
+import { KucBase } from "../../base/kuc-base";
+
+export declare type MobileMultiChoiceItem = { label?: string; value?: string };
+export declare type MobileMultiChoiceValueMapping = { [key: number]: string };
+
+export declare type MobileMultiChoiceProps = {
+  className?: string;
+  error?: string;
+  id?: string;
+  label?: string;
+  disabled?: boolean;
+  requiredIcon?: boolean;
+  visible?: boolean;
+  items?: MobileMultiChoiceItem[];
+  value?: string[];
+  selectedIndex?: number[];
+};
+
+export declare class MobileMultiChoice extends KucBase {
+  className: string;
+  error: string;
+  id: string;
+  label: string;
+  disabled: boolean;
+  requiredIcon: boolean;
+  visible: boolean;
+  items: MobileMultiChoiceItem[];
+  selectedIndex: number[];
+  value: string[];
+  constructor(props?: MobileMultiChoiceProps);
+  shouldUpdate(changedProperties: PropertyValues): boolean;
+  willUpdate(changedProperties: PropertyValues): void;
+  // eslint-disable-next-line kuc-v1/super-update
+  update(changedProperties: PropertyValues): void;
+  render(): import("lit").TemplateResult<1>;
+}
