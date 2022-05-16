d026f6813803ece3fb3f9f2c4059bd3fc2bd0f77
voduchau <h002250@cybozu.onmicrosoft.com>
2022-05-16 13:15:14 +0700

    Desktop dialog and separate css

---
+++ src/dialog/type.ts
@@ -0,0 +1,20 @@
+import { PropertyValues } from "lit";
+import { KucBase } from "../base/kuc-base";
+export declare type DialogProps = {
+  icon?: "" | "info" | "success" | "error" | "warning" | "question";
+  title?: string;
+  content?: string | HTMLElement;
+  footer?: string | HTMLElement;
+};
+export declare class Dialog extends KucBase {
+  icon: "" | "info" | "success" | "error" | "warning" | "question";
+  title: string;
+  content: string | HTMLElement;
+  footer: string | HTMLElement;
+  constructor(props?: DialogProps);
+  // eslint-disable-next-line kuc-v1/super-update
+  update(changedProperties: PropertyValues): void;
+  open(): void;
+  close(): void;
+  render(): import("lit").TemplateResult<1>;
+}
