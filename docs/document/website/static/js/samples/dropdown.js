b4353f60ca50dbbf0494faa554e6750e0ff8de53
Nguyen Hoang Anh <nmanhnit@gmail.com>
2022-05-24 11:05:07 +0700

    dropdown

--- docs/document/website/static/js/samples/dropdown.js
+++ docs/document/website/static/js/samples/dropdown.js
@@ -3,15 +3,15 @@ document.addEventListener("kuc:loaded", function () {
   const dropdown = new Kuc.Dropdown({
     items: [
       {
-        label: 'orange',
-        value: 'Orange'
+        label: "orange",
+        value: "Orange"
       },
       {
-        label: 'apple',
-        value: 'Apple'
+        label: "apple",
+        value: "Apple"
       }
     ],
-    value: 'Orange'
+    value: "Orange"
   });
   container.appendChild(dropdown);
 });
