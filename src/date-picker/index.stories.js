dacc379318369f2d2e6beffa5d8935fcbecd6af8
voduchau <h002250@cybozu.onmicrosoft.com>
2022-06-01 09:46:22 +0700

    investigate storybook for setter and getter the same old way

--- src/date-picker/index.stories.js
+++ src/date-picker/index.stories.js
@@ -1,5 +1,4 @@
-import "./index.ts";
-import { html } from "lit-html";
+import { DatePicker } from "./index.ts";
 
 export default {
   title: "desktop/date-picker",
@@ -26,28 +25,27 @@ export default {
   }
 };
 
-const Template = args => {
-  const handleDateChange = event => {
+const ConstructorTemplate = args => {
+  const datePicker = new DatePicker({
+    disabled: args.disabled,
+    language: args.language,
+    value: args.value,
+    id: args.id,
+    label: args.label,
+    requiredIcon: args.requiredIcon,
+    visible: args.visible,
+    className: args.className,
+    error: args.error
+  });
+
+  datePicker.addEventListener("change", event => {
     console.log(event);
-  };
-  return html`
-    <kuc-date-picker
-      .disabled="${args.disabled}"
-      .language="${args.language}"
-      .value="${args.value}"
-      .id="${args.id}"
-      .label="${args.label}"
-      .requiredIcon="${args.requiredIcon}"
-      .visible="${args.visible}"
-      .className="${args.className}"
-      .error="${args.error}"
-      @change="${handleDateChange}"
-    ></kuc-date-picker>
-  `;
+  });
+  return datePicker;
 };
 
-export const BaseLanguageEN = Template.bind({});
-BaseLanguageEN.args = {
+export const BaseConstructor = ConstructorTemplate.bind({});
+BaseConstructor.args = {
   className: "date-picker-class",
   error: "",
   id: "date-picker-id",
@@ -58,38 +56,50 @@ BaseLanguageEN.args = {
   visible: true
 };
 
-export const BaseLanguageJA = Template.bind({});
-BaseLanguageJA.args = {
+const SetTemplate = args => {
+  const wrapperEl = document.createElement("div");
+
+  const datePicker = new DatePicker({});
+  datePicker.disabled = args.disabled;
+  datePicker.language = args.language;
+  datePicker.value = args.value;
+  datePicker.id = args.id;
+  datePicker.label = args.label;
+  datePicker.requiredIcon = args.requiredIcon;
+  datePicker.visible = args.visible;
+  datePicker.className = args.className;
+  datePicker.error = args.error;
+  datePicker.addEventListener("change", event => {
+    console.log(event);
+  });
+
+  const buttonSetValue = document.createElement("button");
+  buttonSetValue.innerText = "Set value";
+  buttonSetValue.addEventListener("click", () => {
+    datePicker.value = "1997-01-01";
+  });
+
+  const buttonGetValue = document.createElement("button");
+  buttonGetValue.innerText = "Get value";
+  buttonGetValue.addEventListener("click", () => {
+    console.log(datePicker.value);
+  });
+
+  wrapperEl.appendChild(datePicker);
+  wrapperEl.appendChild(buttonSetValue);
+  wrapperEl.appendChild(buttonGetValue);
+
+  return wrapperEl;
+};
+
+export const BaseSetter = SetTemplate.bind({});
+BaseSetter.args = {
   className: "date-picker-class",
   error: "",
   id: "date-picker-id",
   label: "Date Picker Label",
-  language: "ja",
-  requiredIcon: false,
-  value: "2021-03-31",
-  visible: true
-};
-
-export const BaseLanguageZH = Template.bind({});
-BaseLanguageZH.args = {
-  className: "date-picker-class",
-  error: "",
-  id: "date-picker-id",
-  label: "Date Picker Label",
-  language: "zh",
-  requiredIcon: false,
-  value: "2021-03-31",
-  visible: true
-};
-
-export const BaseError = Template.bind({});
-BaseError.args = {
-  className: "date-picker-class",
-  error: "Date picker error",
-  id: "date-picker-id",
-  label: "Date Picker Label",
-  requiredIcon: true,
   language: "en",
+  requiredIcon: false,
   value: "2021-03-31",
   visible: true
 };
