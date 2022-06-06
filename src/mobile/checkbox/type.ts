03fa47243ab227eb29aee0d0493455ba1886008c
voduchau <h002250@cybozu.onmicrosoft.com>
2022-06-06 14:44:22 +0700

    remove and update eslint

--- src/mobile/checkbox/type.ts
+++ src/mobile/checkbox/type.ts
@@ -18,7 +18,6 @@ export declare type MobileCheckboxProps = {
   selectedIndex?: number[];
 };
 
-// eslint-disable-next-line kuc-v1/no-describe-style-tag-inside-html
 export declare class MobileCheckbox extends KucBase {
   className: string;
   error: string;
@@ -34,7 +33,6 @@ export declare class MobileCheckbox extends KucBase {
   constructor(props?: MobileCheckboxProps);
   shouldUpdate(changedProperties: PropertyValues): boolean;
   willUpdate(changedProperties: PropertyValues): void;
-  // eslint-disable-next-line kuc-v1/super-update
   update(changedProperties: PropertyValues): void;
   render(): import("lit").TemplateResult<1>;
   updated(): void;
