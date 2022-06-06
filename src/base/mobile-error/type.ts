b6425ebf35072afb0be2fdd5b4c658fcc46b383b
voduchau <h002250@cybozu.onmicrosoft.com>
2022-06-06 13:23:59 +0700

    implement for base mobile label/error

---
+++ src/base/mobile-error/type.ts
@@ -0,0 +1,7 @@
+import { KucBase } from "../kuc-base";
+export declare class BaseMobileError extends KucBase {
+  ariaLive: string;
+  guid: string;
+  text: string;
+  render(): import("lit").TemplateResult<1>;
+}
---
+++ src/base/mobile-label/type.ts
@@ -0,0 +1,7 @@
+import { KucBase } from "../kuc-base";
+export declare class BaseMobileLabel extends KucBase {
+  requiredIcon: boolean;
+  guid: string;
+  text: string;
+  render(): import("lit").TemplateResult<1>;
+}
