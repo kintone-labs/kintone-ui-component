4ae8a886b3d737ae402f69a03a5f229f0a6123f1
Chi Nguyen <chi-nguyen@cybozu.vn>
2022-06-10 09:31:49 +0700

    SSR-1845 - MobileButton - resolve versions conflict

--- src/mobile/button/index.ts
+++ src/mobile/button/index.ts
@@ -10,52 +10,62 @@ import { validateProps } from "../../base/validator";
 import { MobileButtonProps } from "./type";
 import { MOBILE_BUTTON_CSS } from "./style";
 
-export class MobileButton extends KucBase {
-  @property({ type: String, reflect: true, attribute: "class" }) className = "";
-  @property({ type: String, reflect: true, attribute: "id" }) id = "";
-  @property({ type: String }) text = "";
-  @property({ type: String }) type = "normal";
-  @property({ type: Boolean }) disabled = false;
-  @property({
-    type: Boolean,
-    attribute: "hidden",
-    reflect: true,
-    converter: visiblePropConverter
-  })
-  visible = true;
-
-  constructor(props?: MobileButtonProps) {
-    super();
-    const validProps = validateProps(props);
-    Object.assign(this, validProps);
+let exportMobileButton;
+(() => {
+  exportMobileButton = window.customElements.get("kuc-mobile-button");
+  if (exportMobileButton) {
+    return;
   }
 
-  private _handleClickButton(event: MouseEvent) {
-    event.stopPropagation();
-    dispatchCustomEvent(this, "click");
-  }
+  class KucMobileButton extends KucBase {
+    @property({ type: String, reflect: true, attribute: "class" })
+    className = "";
+    @property({ type: String, reflect: true, attribute: "id" }) id = "";
+    @property({ type: String }) text = "";
+    @property({ type: String }) type = "normal";
+    @property({ type: Boolean }) disabled = false;
+    @property({
+      type: Boolean,
+      attribute: "hidden",
+      reflect: true,
+      converter: visiblePropConverter
+    })
+    visible = true;
 
-  private _getButtonColorType() {
-    if (this.type === "normal" || this.type === "submit") {
-      return this.type;
+    constructor(props?: MobileButtonProps) {
+      super();
+      const validProps = validateProps(props);
+      Object.assign(this, validProps);
     }
-    return "normal";
-  }
 
-  render() {
-    return html`
-      <button
-        type="button"
-        class="kuc-mobile-button__button kuc-mobile-button__button--${this._getButtonColorType()}"
-        ?disabled="${this.disabled}"
-        @click="${this._handleClickButton}"
-      >
-        ${this.text}
-      </button>
-    `;
+    private _handleClickButton(event: MouseEvent) {
+      event.stopPropagation();
+      dispatchCustomEvent(this, "click");
+    }
+
+    private _getButtonColorType() {
+      if (this.type === "normal" || this.type === "submit") {
+        return this.type;
+      }
+      return "normal";
+    }
+
+    render() {
+      return html`
+        <button
+          type="button"
+          class="kuc-mobile-button__button kuc-mobile-button__button--${this._getButtonColorType()}"
+          ?disabled="${this.disabled}"
+          @click="${this._handleClickButton}"
+        >
+          ${this.text}
+        </button>
+      `;
+    }
   }
-}
-if (!window.customElements.get("kuc-mobile-button")) {
-  window.customElements.define("kuc-mobile-button", MobileButton);
+  window.customElements.define("kuc-mobile-button", KucMobileButton);
   createStyleOnHeader(MOBILE_BUTTON_CSS);
-}
+  exportMobileButton = KucMobileButton;
+})();
+const MobileButton = exportMobileButton as any;
+export { MobileButton };
