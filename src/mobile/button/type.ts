e625fe1092dbc75fc0b65c4a07a3b05b9aa59374
Chi Nguyen <chi-nguyen@cybozu.vn>
2022-06-10 09:03:08 +0700

    SSR-1845 - MobileButton - Separate style and type

---
+++ src/mobile/button/type.ts
@@ -0,0 +1,23 @@
+import { KucBase } from "../../base/kuc-base";
+
+export declare type MobileButtonProps = {
+  className?: string;
+  id?: string;
+  text?: string;
+  type?: "normal" | "submit";
+  disabled?: boolean;
+  visible?: boolean;
+};
+
+export declare class MobileButton extends KucBase {
+  className: string;
+  id: string;
+  text: string;
+  type: string;
+  disabled: boolean;
+  visible: boolean;
+  constructor(props?: MobileButtonProps);
+  private _handleClickButton;
+  private _getButtonColorType;
+  render(): import("lit").TemplateResult<1>;
+}
