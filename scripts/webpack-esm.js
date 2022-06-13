7aed2607001c996444ce831c0045f9dfdc73f2b5
趙华锋 <38276156+h000780@users.noreply.github.com>
2022-06-13 15:51:37 +0800

    v1.4.0_SSR-1845_SSR-2378_Mobile-Text

--- scripts/webpack-esm.js
+++ scripts/webpack-esm.js
@@ -26,7 +26,8 @@ const componentDirectories = [
   "base/mobile-error",
   "mobile/button",
   "mobile/checkbox",
-  "mobile/notification"
+  "mobile/notification",
+  "mobile/text"
 ];
 
 const classNamePattern = /(kuc(-[a-z]+)+)__|(kuc(-[a-z]+)+)\>|(kuc(-[a-z]+)+)(\s|,|\[)|(kuc(-[a-z]+)+)\n|(kuc(-[a-z]+)+)\"/g;
