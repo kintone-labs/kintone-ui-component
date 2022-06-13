198ee1a2374a31e2374ff8fb2bd795ea0c1b2de7
voduchau <h002250@cybozu.onmicrosoft.com>
2022-06-13 08:49:39 +0700

    mobile timePicker

---
+++ src/mobile/time-picker/type.ts
@@ -0,0 +1,35 @@
+import { PropertyValues } from "lit";
+import { KucBase } from "../../base/kuc-base";
+import "../../base/mobile-error";
+import "../../base/datetime/mobile-time";
+import "../../base/mobile-label";
+export declare type MobileTimePickerProps = {
+  className?: string;
+  error?: string;
+  id?: string;
+  label?: string;
+  language?: "ja" | "en" | "zh" | "auto";
+  value?: string;
+  disabled?: boolean;
+  hour12?: boolean;
+  requiredIcon?: boolean;
+  visible?: boolean;
+};
+export declare class MobileTimePicker extends KucBase {
+  className: string;
+  error: string;
+  id: string;
+  label: string;
+  language: string;
+  value?: string | undefined;
+  disabled: boolean;
+  hour12: boolean;
+  requiredIcon: boolean;
+  visible: boolean;
+  constructor(props?: MobileTimePickerProps);
+  protected shouldUpdate(changedProperties: PropertyValues): boolean;
+  willUpdate(): void;
+  update(changedProperties: PropertyValues): void;
+  render(): import("lit").TemplateResult<1>;
+  updated(): void;
+}
