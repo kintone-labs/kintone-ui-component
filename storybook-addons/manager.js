01ccdd16a4a8f82614224b8d3c0db7115b9b1077
voduchau <h002250@cybozu.onmicrosoft.com>
2022-06-15 21:34:03 +0700

    update new way get value

--- storybook-addons/manager.js
+++ storybook-addons/manager.js
@@ -1,34 +1,30 @@
-import React from "react";
-import { addons, types } from "@storybook/addons";
-import { AddonPanel } from "@storybook/components";
-import { useParameter } from "@storybook/api";
-import { useArgs } from "@storybook/api";
+import React from 'react';
+import { addons, types } from '@storybook/addons';
+import { AddonPanel } from '@storybook/components';
+import { useArgs } from '@storybook/api';
 
-const ADDON_ID = "get-value";
-const PANEL_ID = `${ADDON_ID}/panel`;
-const PARAM_KEY = "getValue";
+export const Panel = () => {
+  const [args] = useArgs();
 
-const ButtonGetValue = () => {
-  const value = useParameter(PARAM_KEY, null);
-  const isShowGet = value && value.get ? true : false;
-  const iframeStorybookEl = document.getElementById("storybook-preview-iframe");
-  const _handleClick = () => {
-    const btnGetValue = iframeStorybookEl.contentDocument.getElementById(
-      value.id
-    );
-    btnGetValue.click();
-  };
-  return isShowGet ? <button onClick={_handleClick}>Get value</button> : "";
+  function _handleClick() {
+    const iframe = document.getElementById("storybook-preview-iframe");
+    const component = iframe.contentWindow.document.getElementById(args.id);
+    Object.keys(args).forEach(key => {
+      console.log(`"${key}" value: ${component[key]}`);
+    })
+  }
+
+  return <button onClick={_handleClick}>get prop value</button>;
 };
 
-addons.register(ADDON_ID, api => {
-  addons.add(PANEL_ID, {
+addons.register('get-prop-value', (api) => {
+  addons.add('get-prop-value/panel', {
     type: types.PANEL,
-    title: "Addons",
+    title: 'Get Prop Value',
     render: ({ active, key }) => (
       <AddonPanel active={active} key={key}>
-        <ButtonGetValue />
+        <Panel />
       </AddonPanel>
-    )
+    ),
   });
 });
