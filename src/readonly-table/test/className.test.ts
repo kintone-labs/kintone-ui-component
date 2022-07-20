9b2037d095fc599ee98652204cf2f981d0277da8
Chi Nguyen <chi-nguyen@cybozu.vn>
2022-07-20 10:00:26 +0700

    SSR-2122 checking unit test

--- src/readonly-table/test/className.test.ts
+++ src/readonly-table/test/className.test.ts
@@ -1,4 +1,4 @@
-import { expect, fixture } from "@open-wc/testing";
+import { elementUpdated, expect, fixture } from "@open-wc/testing";
 import { ReadOnlyTable } from "../index";
 
 describe("ReadOnlyTable", () => {
@@ -12,14 +12,16 @@ describe("ReadOnlyTable", () => {
     it("should be 'options-class' when assigning on constructor", async () => {
       const container = new ReadOnlyTable({ className: "options-class" });
       const el = await fixture(container);
+      await elementUpdated(el);
       expect(el.classList.length).to.equal(1);
       expect(el.className).to.equal("options-class");
     });
 
     it("should be replaced by 'replace-class' when changed by setter", async () => {
       const container = new ReadOnlyTable({ className: "options-class" });
-      container.className = "replace-class";
       const el = await fixture(container);
+      container.className = "replace-class";
+      await elementUpdated(el);
       expect(el.classList.length).to.equal(1);
       expect(el.className).to.equal("replace-class");
     });
