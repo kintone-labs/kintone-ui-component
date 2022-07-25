46a11ee97aaf3a617b3eb3e928d682e049c40f8a
Chi Nguyen <chi-nguyen@cybozu.vn>
2022-07-25 09:23:36 +0700

    SSR-2112 added type.ts

---
+++ src/readonly-table/type.ts
@@ -0,0 +1,34 @@
+import { PropertyValues } from "lit";
+import { KucBase } from "../base/kuc-base";
+
+export declare type Column = {
+  headerName?: string;
+  visible?: boolean;
+  maxWidth?: string;
+};
+
+export declare type ReadOnlyTableProps = {
+  className?: string;
+  id?: string;
+  label?: string;
+  columns?: Column[];
+  data?: string[][];
+  pagination?: boolean;
+  rowsPerPage?: number;
+  visible?: boolean;
+};
+
+export declare class ReadOnlyTable extends KucBase {
+  className: string;
+  id: string;
+  label: string;
+  columns: Column[];
+  data: string[][];
+  pagination: boolean;
+  rowsPerPage: number;
+  visible: boolean;
+  constructor(props?: ReadOnlyTableProps);
+  update(changedProperties: PropertyValues): void;
+  render(): import("lit-html").TemplateResult<1>;
+  updated(): void;
+}
