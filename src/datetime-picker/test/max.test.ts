f08df5ec7ab448109573d406de13a6ac4f15e548
Chi Nguyen <chi-nguyen@cybozu.vn>
2022-07-26 14:03:54 +0700

    SSR-2319 updated max.test.ts for Desktop DateTimePicker

--- src/datetime-picker/test/max.test.ts
+++ src/datetime-picker/test/max.test.ts
@@ -1,6 +1,6 @@
 import { expect, fixture, elementUpdated } from "@open-wc/testing";
-import { DateTimePicker } from "../index";
 import { INVALID_FORMAT_MESSAGE } from "../../base/datetime/resource/constant";
+import { DateTimePicker } from "../index";
 
 describe("DateTimePicker", () => {
   describe("max", () => {
@@ -72,7 +72,7 @@ describe("DateTimePicker", () => {
         if (error instanceof Error) {
           errorMessage = error.message;
         }
-        expect(errorMessage).to.be.equal(INVALID_FORMAT_MESSAGE.VALUE);
+        expect(errorMessage).to.be.equal(INVALID_FORMAT_MESSAGE.MAX);
       }
     });
 
