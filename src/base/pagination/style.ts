079dd38d53025ad2621a807824e28cc70c4a73a5
Chi Nguyen <chi-nguyen@cybozu.vn>
2022-08-04 15:18:23 +0700

    SSR-2112 fixed typo in style for pagination

--- src/base/pagination/style.ts
+++ src/base/pagination/style.ts
@@ -5,20 +5,20 @@ export const PAGINATION_CSS = `
   .kuc-base-pagination__group button {
     cursor: pointer;
   }
-  .kuc-base-pagination__group__pagenation-prev {
+  .kuc-base-pagination__group__pagination-prev {
     border: none;
     background-color: transparent;
     visibility: visible;
   }
-  .kuc-base-pagination__group__pagenation-next {
+  .kuc-base-pagination__group__pagination-next {
     border: none;
     background-color: transparent;
     visibility: visible;
   }
-  .kuc-base-pagination__group__pagenation-next:hover svg path,
-  .kuc-base-pagination__group__pagenation-prev:hover svg path,
-  .kuc-base-pagination__group__pagenation-next:focus svg path,
-  .kuc-base-pagination__group__pagenation-prev:focus svg path {
+  .kuc-base-pagination__group__pagination-next:hover svg path,
+  .kuc-base-pagination__group__pagination-prev:hover svg path,
+  .kuc-base-pagination__group__pagination-next:focus svg path,
+  .kuc-base-pagination__group__pagination-prev:focus svg path {
     fill: #3498db;
   }
   .pager-disable {
