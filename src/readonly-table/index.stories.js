a1ddc1dd56cba1032b49d60f028a0ee92ea06d1a
Chi Nguyen <chi-nguyen@cybozu.vn>
2022-08-05 09:32:43 +0700

    SSR-2112 updated storybook for more example

--- src/readonly-table/index.stories.js
+++ src/readonly-table/index.stories.js
@@ -51,7 +51,152 @@ Base.args = {
     },
     {
       index: "3",
+      name: "Vinh Long",
+      country: "Vietnam",
+      population: "200,000",
+    },
+    {
+      index: "4",
+      name: "Rach Gia",
+      country: "Vietnam",
+      population: "400,000",
+    },
+    {
+      index: "5",
+      name: "Edmonton",
+      country: "Canada",
+      population: "981,000",
+    },
+    {
+      index: "6",
+      name: "Calgary",
+      country: "Canada",
+      population: "1,336,000",
+    },
+    {
+      index: "7",
+      name: "Vancouver",
+      country: "Canada",
+      population: "675,000",
+    },
+  ],
+};
+
+export const Base2 = Template.bind({});
+Base2.args = {
+  id: "sample-id",
+  className: "sample-class",
+  label: "My ReadOnly Table",
+  visible: true,
+  pagination: true,
+  rowsPerPage: 3,
+  columns: [
+    {
+      headerName: "Number",
+      field: "index",
+    },
+    {
+      headerName: "City",
+      field: "name",
+    },
+    {
+      headerName: "Country",
+      field: "country",
+    },
+    {
+      headerName: "Population",
+      field: "population",
+    },
+  ],
+  data: [
+    {
+      index: "1",
+      name: "Ho Chi Minh",
+      country: "Vietnam",
+      population: "8,993,000",
+    },
+    {
+      index: "2",
+      name: "Can Tho",
+      country: "Vietnam",
+      population: "1,282,000",
+    },
+    {
+      index: "3",
       name: "An imaginary long long long city name as an exameple for long content in a cell",
+      country: "Vietnam",
+      population: "200,000",
+    },
+    {
+      index: "4",
+      name: "Rach Gia",
+      country: "Vietnam",
+      population: "400,000",
+    },
+    {
+      index: "5",
+      name: "Edmonton",
+      country: "Canada",
+      population: "981,000",
+    },
+    {
+      index: "6",
+      name: "Calgary",
+      country: "Canada",
+      population: "1,336,000",
+    },
+    {
+      index: "7",
+      name: "Vancouver",
+      country: "Canada",
+      population: "675,000",
+    },
+  ],
+};
+
+export const Base3 = Template.bind({});
+Base3.args = {
+  id: "sample-id",
+  className: "sample-class",
+  label: "My ReadOnly Table",
+  visible: true,
+  pagination: true,
+  rowsPerPage: 3,
+  columns: [
+    {
+      headerName: "Number",
+      field: "index",
+    },
+    {
+      headerName:
+        "A very very long header name as an example of long content in header",
+      field: "name",
+    },
+    {
+      headerName: "Country",
+      field: "country",
+    },
+    {
+      headerName: "Population",
+      field: "population",
+    },
+  ],
+  data: [
+    {
+      index: "1",
+      name: "Ho Chi Minh",
+      country: "Vietnam",
+      population: "8,993,000",
+    },
+    {
+      index: "2",
+      name: "Can Tho",
+      country: "Vietnam",
+      population: "1,282,000",
+    },
+    {
+      index: "3",
+      name: "Vinh Long",
       country: "Vietnam",
       population: "200,000",
     },
