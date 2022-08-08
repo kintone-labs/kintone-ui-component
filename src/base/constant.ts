831cf704bf4744e033b2e9d65236b5b451b70469
Chi Nguyen <chi-nguyen@cybozu.vn>
2022-08-08 15:36:49 +0700

    SSR-2112 move private validation messages to base/constant

--- src/base/constant.ts
+++ src/base/constant.ts
@@ -10,4 +10,11 @@ export const ERROR_MESSAGE = {
     IS_NOT_ARRAY: "'selectedIndex' property is not array",
     IS_NOT_NUMBER: "'selectedIndex' property is not number",
   },
+  COLUMNS: {
+    INVALID: "'columns' property is invalid",
+  },
+  ROWS_PER_PAGE: {
+    INVALID:
+      "'rowsPerPage' property must be a positive integer! Set to 5 by default.",
+  },
 };
