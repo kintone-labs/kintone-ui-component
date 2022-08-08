2af4cb3f5df4c59e794d10369dca58436a1df362
Chi Nguyen <chi-nguyen@cybozu.vn>
2022-08-08 16:49:36 +0700

    SSR-2112 removed outline when focus and applied foucs-visible to pagination

--- src/base/pagination/style.ts
+++ src/base/pagination/style.ts
@@ -4,6 +4,7 @@ export const PAGINATION_CSS = `
   }
   .kuc-base-pagination__group button {
     cursor: pointer;
+    outline: none;
   }
   .kuc-base-pagination__group__pagination-prev {
     border: none;
@@ -17,8 +18,8 @@ export const PAGINATION_CSS = `
   }
   .kuc-base-pagination__group__pagination-next:hover svg path,
   .kuc-base-pagination__group__pagination-prev:hover svg path,
-  .kuc-base-pagination__group__pagination-next:focus svg path,
-  .kuc-base-pagination__group__pagination-prev:focus svg path {
+  .kuc-base-pagination__group__pagination-next:focus-visible svg path,
+  .kuc-base-pagination__group__pagination-prev:focus-visible svg path {
     fill: #3498db;
   }
   .pager-disable {
