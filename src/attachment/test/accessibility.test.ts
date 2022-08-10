ac36d1061b13adc41afc7c5e27fad54c9603ee96
趙华锋 <38276156+h000780@users.noreply.github.com>
2022-08-10 14:56:02 +0800

    Update accessibility.test.ts

--- src/attachment/test/accessibility.test.ts
+++ src/attachment/test/accessibility.test.ts
@@ -3,7 +3,7 @@ import { Attachment } from "../index";
 
 describe("Attachment", () => {
   describe("accessibility", () => {
-    it("should be focused the next remove button in files when press tab key on any remove button of file", async () => {
+    it("should be focused the input element when press tab key on last remove button in files", async () => {
       let triggeredEvent: any = null;
       const container = new Attachment({
         files: [
@@ -17,30 +17,6 @@ describe("Attachment", () => {
       const buttonsEl = el.querySelectorAll(
         ".kuc-attachment__group__files__display-area__item__remove-button__container__button"
       );
-      console.log(buttonsEl);
-      const divsEl = el.querySelectorAll(
-        ".kuc-attachment__group__files__display-area__item__remove-button__container"
-      );
-      console.log(divsEl);
-
-      // divsEl[1].addEventListener("focus", (event: Event) => {
-      //   triggeredEvent = event;
-      //   console.log(triggeredEvent);
-      // });
-      (buttonsEl[1] as HTMLButtonElement).addEventListener(
-        "focus",
-        (event: Event) => {
-          triggeredEvent = event;
-          console.log(triggeredEvent);
-        }
-      );
-      (divsEl[1] as HTMLDivElement).addEventListener(
-        "focus",
-        (event: Event) => {
-          triggeredEvent = event;
-          console.log(triggeredEvent);
-        }
-      );
       const inputEl = el.querySelector(
         ".kuc-attachment__group__files__browse-button__input-container__input"
       );
@@ -48,19 +24,14 @@ describe("Attachment", () => {
         "focus",
         (event: Event) => {
           triggeredEvent = event;
-          console.log(triggeredEvent);
+          console.log(event);
         }
       );
-      // (buttonsEl[0] as HTMLButtonElement).dispatchEvent(
-      //   new KeyboardEvent("keydown", { key: "Tab" })
-      // );
+      (buttonsEl[1] as HTMLButtonElement).focus();
       (buttonsEl[1] as HTMLButtonElement).dispatchEvent(
         new KeyboardEvent("keydown", { key: "Tab" })
       );
-
-      // await elementUpdated(el);
-      // expect(triggeredEvent.type).to.equal("focus");
-
+      // (buttonsEl[1] as HTMLButtonElement).click();
       await elementUpdated(el);
       expect(triggeredEvent.type).to.equal("focus");
     });
