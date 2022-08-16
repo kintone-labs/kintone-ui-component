236b23a9b1f3478b7cbf0bb99be0aa35ecd6fa22
趙华锋 <38276156+h000780@users.noreply.github.com>
2022-08-16 18:24:20 +0800

    Update web-test-runner.config.js

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,17 +1,15 @@
 
-module.exports = config => {
-    return {  
-        nodeResolve: { mainFields: ["module", "main", "browser"] },
-        files: './unit_test/**/test/*.test.js',
-        coverage: true,
-        coverageConfig: {
-            threshold:{
-                statements: 90,
-                branches: 90,
-                functions: 90,
-                lines: 90
-            }
-        },
-    };
-}
+module.exports = {
+    nodeResolve: { mainFields: ["module", "main", "browser"] },
+    files: './unit_test/**/test/*.test.js',
+    coverage: true,
+    coverageConfig: {
+        threshold:{
+            statements: 90,
+            branches: 90,
+            functions: 90,
+            lines: 90
+        }
+    },
+};
   
