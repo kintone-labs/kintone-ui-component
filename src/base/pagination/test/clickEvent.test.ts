dad503264e7b937e3ae4f134931988932dac9f70
Chi Nguyen <chi-nguyen@cybozu.vn>
2022-08-19 14:49:28 +0700

    SSR-2112 added clickEvent unit test

---
+++ src/base/pagination/test/clickEvent.test.ts
@@ -0,0 +1,40 @@
+import { elementUpdated, expect, fixture } from "@open-wc/testing";
+import "../index";
+
+describe("BasePagination", () => {
+  describe("Click events", () => {
+    it("should trigger click event when clicking on Next button ", async () => {
+      const container = document.createElement(
+        "kuc-base-pagination"
+      ) as HTMLInputElement;
+      let dispatchedEventName;
+      container.addEventListener("kuc:pagination-click-next", (event) => {
+        dispatchedEventName = event.type;
+      });
+      await fixture(container);
+      const nextEl = document.querySelector(
+        "button.kuc-base-pagination__group__pagination-next"
+      ) as HTMLElement;
+      nextEl.click();
+      await elementUpdated(nextEl);
+      expect(dispatchedEventName).to.equal("kuc:pagination-click-next");
+    });
+
+    it("should trigger click event when clicking on Previous button ", async () => {
+      const container = document.createElement(
+        "kuc-base-pagination"
+      ) as HTMLInputElement;
+      let dispatchedEventName;
+      container.addEventListener("kuc:pagination-click-prev", (event) => {
+        dispatchedEventName = event.type;
+      });
+      await fixture(container);
+      const nextEl = document.querySelector(
+        "button.kuc-base-pagination__group__pagination-prev"
+      ) as HTMLElement;
+      nextEl.click();
+      await elementUpdated(nextEl);
+      expect(dispatchedEventName).to.equal("kuc:pagination-click-prev");
+    });
+  });
+});
