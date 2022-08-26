196257afda6773734ece1345c7f2e20265086dc6
Chi Nguyen <chi-nguyen@cybozu.vn>
2022-08-26 16:45:43 +0700

    SSR-2112 updated pagination clickEvent unit test

--- src/base/pagination/test/clickEvent.test.ts
+++ src/base/pagination/test/clickEvent.test.ts
@@ -1,40 +1,40 @@
 import { elementUpdated, expect, fixture } from "@open-wc/testing";
-import "../index";
+import { BasePagination } from "../index";
 
 describe("BasePagination", () => {
   describe("Click events", () => {
-    it("should trigger click event when clicking on Next button ", async () => {
-      const container = document.createElement(
-        "kuc-base-pagination"
-      ) as HTMLInputElement;
-      let dispatchedEventName;
-      container.addEventListener("kuc:pagination-click-next", (event) => {
-        dispatchedEventName = event.type;
+    it("should trigger click event when clicking on Prev button", (done) => {
+      const handleEvent = (event: any) => {
+        expect(event.type).to.equal("kuc:pagination-click-prev");
+        done();
+      };
+
+      const container = new BasePagination();
+      fixture(container).then(() => {
+        container.addEventListener("kuc:pagination-click-prev", handleEvent);
+
+        const nextEl = container.querySelector(
+          ".kuc-base-pagination__group__pager-prev"
+        ) as HTMLElement;
+        nextEl.click();
       });
-      await fixture(container);
-      const nextEl = document.querySelector(
-        "button.kuc-base-pagination__group__pager-next"
-      ) as HTMLElement;
-      nextEl.click();
-      await elementUpdated(nextEl);
-      expect(dispatchedEventName).to.equal("kuc:pagination-click-next");
     });
 
-    it("should trigger click event when clicking on Previous button ", async () => {
-      const container = document.createElement(
-        "kuc-base-pagination"
-      ) as HTMLInputElement;
-      let dispatchedEventName;
-      container.addEventListener("kuc:pagination-click-prev", (event) => {
-        dispatchedEventName = event.type;
+    it("should trigger click event when clicking on Next button", (done) => {
+      const handleEvent = (event: any) => {
+        expect(event.type).to.equal("kuc:pagination-click-next");
+        done();
+      };
+
+      const container = new BasePagination();
+      fixture(container).then(() => {
+        container.addEventListener("kuc:pagination-click-next", handleEvent);
+
+        const nextEl = container.querySelector(
+          ".kuc-base-pagination__group__pager-next"
+        ) as HTMLElement;
+        nextEl.click();
       });
-      await fixture(container);
-      const nextEl = document.querySelector(
-        "button.kuc-base-pagination__group__pager-prev"
-      ) as HTMLElement;
-      nextEl.click();
-      await elementUpdated(nextEl);
-      expect(dispatchedEventName).to.equal("kuc:pagination-click-prev");
     });
   });
 });
