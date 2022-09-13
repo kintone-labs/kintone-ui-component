0047f2fade0ee4293df9e6574abab1a7da37d53b
voduchau <h002250@cybozu.onmicrosoft.com>
2022-09-13 15:05:51 +0700

    column and data props

---
+++ src/table/test/columns.test.ts
@@ -0,0 +1,184 @@
+import { expect, fixture } from "@open-wc/testing";
+import { Table } from "../index";
+import { Dropdown } from "../../dropdown";
+import { Text } from "../../text";
+
+const renderAge = (cellData: any) => {
+  const text = new Text({ value: cellData });
+  return text;
+};
+
+const renderName = (cellData: any) => {
+  const dropdown = new Dropdown({
+    items: [
+      {
+        label: "Sample1",
+        value: "sample1",
+      },
+      {
+        label: "Sample2",
+        value: "sample2",
+      },
+    ],
+    value: cellData,
+  });
+  return dropdown;
+};
+
+const columns = [
+  { field: "name", title: "Name" },
+  { field: "age", title: "Age" },
+];
+const data = [
+  { name: "sample1", age: 1 },
+  { name: "sample2", age: 2 },
+];
+
+const replacedColumns = [
+  { field: "city", title: "City" },
+  { field: "country", title: "Country" },
+];
+const columnsWithoutField = [{ title: "Name" }, { title: "Age" }];
+const columnsWithoutTitle = [{ field: "name" }, { field: "age" }];
+const columnsDuplicationField = [{ field: "name" }, { field: "name" }];
+const columnsWithRender = [
+  { field: "name", title: "Name", render: renderName },
+  { field: "age", title: "Age", render: renderAge },
+];
+
+describe("Table", () => {
+  describe("columns", () => {
+    it("should be empty header when not assigned on constructor", async () => {
+      const container = new Table();
+      const el = await fixture(container);
+      const columnsEl = el.querySelectorAll(".kuc-table__table__header th");
+      expect(columnsEl.length).to.equal(0);
+    });
+
+    it("should be set header when assigned columns without title prop on constructor", async () => {
+      const container = new Table({ data: data, columns: columnsWithoutTitle });
+      const el = await fixture(container);
+      const columnsEl = el.querySelectorAll(".kuc-table__table__header__cell");
+
+      expect(columnsEl.length).to.equal(2);
+      expect((columnsEl[0] as HTMLTableCellElement).innerText).to.equal("");
+      expect((columnsEl[1] as HTMLTableCellElement).innerText).to.equal("");
+    });
+
+    it("should be set header when assigned columns without title prop by setter", async () => {
+      const container = new Table();
+      container.columns = columnsWithoutTitle;
+      const el = await fixture(container);
+      const columnsEl = el.querySelectorAll(".kuc-table__table__header th");
+
+      expect(columnsEl.length).to.equal(2);
+      expect((columnsEl[0] as HTMLTableCellElement).innerText).to.equal("");
+      expect((columnsEl[1] as HTMLTableCellElement).innerText).to.equal("");
+    });
+
+    it("should be set header when assigned columns without render props on constructor", async () => {
+      const container = new Table({ columns: columns });
+      const el = await fixture(container);
+      const columnsEl = el.querySelectorAll(".kuc-table__table__header th");
+
+      expect(columnsEl.length).to.equal(2);
+      expect((columnsEl[0] as HTMLTableCellElement).innerText).to.equal("Name");
+      expect((columnsEl[1] as HTMLTableCellElement).innerText).to.equal("Age");
+    });
+
+    it("should be set header when assigned columns with render function on constructor", async () => {
+      const container = new Table();
+      container.columns = columnsWithRender;
+      container.data = data;
+      const el = await fixture(container);
+      const tBodyEl = el.querySelector(".kuc-table__table__body");
+      const tableEl = el.querySelector(".kuc-table__table") as HTMLTableElement;
+      const firstRow = tableEl.rows[1] as HTMLTableRowElement;
+      const secondRow = tableEl.rows[2] as HTMLTableRowElement;
+
+      const firstDropdown = firstRow?.cells[0].querySelector(
+        "kuc-dropdown"
+      ) as any;
+      const secondDropdown = secondRow?.cells[0].querySelector(
+        "kuc-dropdown"
+      ) as any;
+      const firstText = firstRow?.cells[1].querySelector("kuc-text") as any;
+      const secondText = secondRow?.cells[1].querySelector("kuc-text") as any;
+
+      expect(tBodyEl?.children.length).to.equal(data.length);
+      expect(firstDropdown.value).to.equal("sample1");
+      expect(firstText.value).to.equal(1);
+      expect(secondDropdown.value).to.equal("sample2");
+      expect(secondText.value).to.equal(2);
+    });
+
+    it("should be updated header when changed columns by setter", async () => {
+      const container = new Table({
+        columns: columns,
+      });
+      container.columns = replacedColumns;
+      const el = await fixture(container);
+      const columnsEl = el.querySelectorAll(".kuc-table__table__header th");
+
+      expect(columnsEl.length).to.equal(2);
+      expect((columnsEl[0] as HTMLTableCellElement).innerText).to.equal("City");
+      expect((columnsEl[1] as HTMLTableCellElement).innerText).to.equal(
+        "Country"
+      );
+    });
+
+    it("should be throw error when assigned null to columns", (done) => {
+      const handleError = (event: any) => {
+        const errorMsg = event.reason.message;
+        expect(errorMsg).to.equal("'columns' property is not array.");
+        window.removeEventListener("unhandledrejection", handleError);
+        done();
+      };
+      window.addEventListener("unhandledrejection", handleError);
+
+      const container = new Table({ columns: null });
+      fixture(container);
+    });
+
+    it("should be throw error when assigned string to columns", (done) => {
+      const handleError = (event: any) => {
+        const errorMsg = event.reason.message;
+        expect(errorMsg).to.equal("'columns' property is not array.");
+        window.removeEventListener("unhandledrejection", handleError);
+        done();
+      };
+      window.addEventListener("unhandledrejection", handleError);
+
+      const container = new Table({ columns: "columns" });
+      fixture(container);
+    });
+
+    it("should be throw error when assigned column without field prop on constructor", (done) => {
+      const handleError = (event: any) => {
+        const errorMsg = event.reason.message;
+        expect(errorMsg).to.equal(
+          "'field' property is not specified in columns."
+        );
+        window.removeEventListener("unhandledrejection", handleError);
+        done();
+      };
+      window.addEventListener("unhandledrejection", handleError);
+
+      const container = new Table({ columns: columnsWithoutField });
+      fixture(container);
+    });
+
+    it("should be throw error when assigned column with duplication field prop on constructor", (done) => {
+      const handleError = (event: any) => {
+        const errorMsg = event.reason.message;
+        expect(errorMsg).to.equal("'field' property is not unique in columns.");
+        window.removeEventListener("unhandledrejection", handleError);
+        done();
+      };
+      window.addEventListener("unhandledrejection", handleError);
+
+      const container = new Table({ columns: columnsDuplicationField });
+      fixture(container);
+    });
+  });
+});
---
+++ src/table/test/data.test.ts
@@ -0,0 +1,106 @@
+import { expect, fixture } from "@open-wc/testing";
+import { Table } from "../index";
+
+const columns = [
+  { field: "name", title: "Name" },
+  { field: "age", title: "Age" },
+];
+const data = [
+  { name: "sample1", age: 1 },
+  { name: "sample2", age: 2 },
+];
+
+const replacedData = [
+  { name: "sample3", age: 3 },
+  { name: "sample4", age: 4 },
+];
+
+describe("Table", () => {
+  describe("data", () => {
+    it("should be empty body when not assigned on constructor", async () => {
+      const container = new Table({ columns: columns });
+      const el = await fixture(container);
+      const tBodyEl = el.querySelector(".kuc-table__table__body");
+      expect(tBodyEl?.children.length).to.equal(0);
+    });
+
+    it("should be add data to table when assigned on constructor", async () => {
+      const container = new Table({ columns: columns, data: data });
+      const el = await fixture(container);
+      const tBodyEl = el.querySelector(".kuc-table__table__body");
+      const tableEl = el.querySelector(".kuc-table__table") as HTMLTableElement;
+      const firstRow = tableEl.rows[1] as HTMLTableRowElement;
+      const secondRow = tableEl.rows[2] as HTMLTableRowElement;
+
+      expect(tBodyEl?.children.length).to.equal(data.length);
+      expect(firstRow?.cells[0].innerText).to.equal(data[0].name);
+      expect(firstRow?.cells[1].innerText).to.equal(data[0].age.toString());
+      expect(secondRow?.cells[0].innerText).to.equal(data[1].name);
+      expect(secondRow?.cells[1].innerText).to.equal(data[1].age.toString());
+    });
+
+    it("should set items when assigned items by setter", async () => {
+      const container = new Table({ columns: columns });
+      container.data = data;
+      const el = await fixture(container);
+      const tBodyEl = el.querySelector(".kuc-table__table__body");
+      const tableEl = el.querySelector(".kuc-table__table") as HTMLTableElement;
+      const firstRow = tableEl.rows[1] as HTMLTableRowElement;
+      const secondRow = tableEl.rows[2] as HTMLTableRowElement;
+
+      expect(tBodyEl?.children.length).to.equal(data.length);
+      expect(firstRow?.cells[0].innerText).to.equal(data[0].name);
+      expect(firstRow?.cells[1].innerText).to.equal(data[0].age.toString());
+      expect(secondRow?.cells[0].innerText).to.equal(data[1].name);
+      expect(secondRow?.cells[1].innerText).to.equal(data[1].age.toString());
+    });
+
+    it("should be changed when updated data by setter", async () => {
+      const container = new Table({ columns: columns, data: data });
+      container.data = replacedData;
+
+      const el = await fixture(container);
+      const tBodyEl = el.querySelector(".kuc-table__table__body");
+      const tableEl = el.querySelector(".kuc-table__table") as HTMLTableElement;
+      const firstRow = tableEl.rows[1] as HTMLTableRowElement;
+      const secondRow = tableEl.rows[2] as HTMLTableRowElement;
+
+      expect(tBodyEl?.children.length).to.equal(replacedData.length);
+      expect(firstRow?.cells[0].innerText).to.equal(replacedData[0].name);
+      expect(firstRow?.cells[1].innerText).to.equal(
+        replacedData[0].age.toString()
+      );
+      expect(secondRow?.cells[0].innerText).to.equal(replacedData[1].name);
+      expect(secondRow?.cells[1].innerText).to.equal(
+        replacedData[1].age.toString()
+      );
+    });
+
+    it("should be throw error when assigned null on constructor", (done) => {
+      const handleError = (event: any) => {
+        const errorMsg = event.reason.message;
+        expect(errorMsg).to.equal("'data' property is not array.");
+        window.removeEventListener("unhandledrejection", handleError);
+        done();
+      };
+      window.addEventListener("unhandledrejection", handleError);
+
+      const container = new Table({ columns: columns, data: null });
+      fixture(container);
+    });
+
+    it("should be throw error when assigned null by setter", (done) => {
+      const handleError = (event: any) => {
+        const errorMsg = event.reason.message;
+        expect(errorMsg).to.equal("'data' property is not array.");
+        window.removeEventListener("unhandledrejection", handleError);
+        done();
+      };
+      window.addEventListener("unhandledrejection", handleError);
+
+      const container = new Table({ columns: columns, data: data });
+      container.data = null;
+      fixture(container);
+    });
+  });
+});
