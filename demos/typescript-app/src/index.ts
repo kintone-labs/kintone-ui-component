442d2d569c3b6cd7760283a605c80beeee90cc96
Nguyen Hoang Anh <nmanhnit@gmail.com>
2022-09-28 09:33:26 +0700

    update example

--- demos/typescript-app/src/index.ts
+++ demos/typescript-app/src/index.ts
@@ -1,21 +1,22 @@
-import { Dropdown, DropdownProps } from "kintone-ui-component";
+import { Dropdown, DropdownProps, DropdownItem } from "kintone-ui-component";
 
 const root = document.getElementById("root") as HTMLElement;
+const items : DropdownItem[] = [
+  {
+    label: "-----",
+    value: "-----",
+  },
+  {
+    label: "Orange",
+    value: "orange",
+  },
+  {
+    label: "Apple",
+    value: "apple",
+  },
+];
 const dropdownProps: DropdownProps = {
-  items: [
-    {
-      label: "-----",
-      value: "-----",
-    },
-    {
-      label: "Orange",
-      value: "orange",
-    },
-    {
-      label: "Apple",
-      value: "apple",
-    },
-  ],
+  items: items,
   value: "-----",
   label: "Fruit",
   error: "Error occurred!",
