850d384824dc59c6d541c82951a5360ad5be1e02
Nguyen Hoang Anh <nmanhnit@gmail.com>
2023-01-31 08:17:34 +0700

    add hidden attribute when there is no error

--- src/time-picker/index.ts
+++ src/time-picker/index.ts
@@ -217,6 +217,7 @@ let exportTimePicker;
           <kuc-base-error
             .text="${this._errorText}"
             .guid="${this._GUID}"
+            ?hidden="${!this.error}"
           ></kuc-base-error>
         </fieldset>
       `;
