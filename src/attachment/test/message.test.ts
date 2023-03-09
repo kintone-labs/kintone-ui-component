4c6996c2eafd2f37fb1f22c09e16f10f84bfd35b
h000780 <hf000780@gmail.com>
2023-03-09 15:24:08 +0800

    SSR-3163: Add a unit-test case for message

--- src/attachment/test/message.test.ts
+++ src/attachment/test/message.test.ts
@@ -4,7 +4,7 @@ import { Attachment } from "../index";
 
 describe("Attachment", () => {
   describe("message", () => {
-    it("should be displayed when message assigned on constructor", async () => {
+    it("should be displayed when assigned on constructor", async () => {
       const container = new Attachment({
         message: "(Max: 1GB)",
         language: "ja",
@@ -15,6 +15,21 @@ describe("Attachment", () => {
       );
       expect((messageEl as HTMLParagraphElement).innerText).to.equal(
         "(Max: 1GB)"
+      );
+    });
+
+    it('should be display "(Max: 2GB)" when changed to "(Max: 2GB)" by setter', async () => {
+      const container = new Attachment({
+        message: "(Max: 1GB)",
+        language: "ja",
+      });
+      container.message = "(Max: 2GB)";
+      const el = await fixture(container);
+      const messageEl = el.querySelector(
+        ".kuc-attachment__group__files__browse-message"
+      );
+      expect((messageEl as HTMLParagraphElement).innerText).to.equal(
+        "(Max: 2GB)"
       );
     });
   });
