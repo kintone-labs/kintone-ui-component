217cab965b769fdc6f9f1bcb55e16496003f85fa
voduchau <h002250@cybozu.onmicrosoft.com>
2023-04-28 16:27:12 +0700

    SSR-2364: Update unit test for FieldGroup's label

--- src/field-group/test/label.test.ts
+++ src/field-group/test/label.test.ts
@@ -9,7 +9,7 @@ describe("FieldGroup", () => {
       const el = await fixture(container);
 
       const labelTextEl = el.querySelector(
-        ".kuc-field-group__group__toggle__label"
+        ".kuc-base-label__text"
       ) as HTMLSpanElement;
       expect(labelTextEl.textContent).to.equal("");
     });
@@ -19,7 +19,7 @@ describe("FieldGroup", () => {
       const el = await fixture(container);
 
       const labelTextEl = el.querySelector(
-        ".kuc-field-group__group__toggle__label"
+        ".kuc-base-label__text"
       ) as HTMLSpanElement;
       expect(labelTextEl.textContent).to.equal("options-label");
     });
@@ -32,7 +32,7 @@ describe("FieldGroup", () => {
       const el = await fixture(container);
 
       const labelTextEl = el.querySelector(
-        ".kuc-field-group__group__toggle__label"
+        ".kuc-base-label__text"
       ) as HTMLSpanElement;
       expect(labelTextEl.textContent).to.equal("replace-label");
     });
