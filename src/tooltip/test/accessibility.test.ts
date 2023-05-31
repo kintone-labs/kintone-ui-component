7acd15f8c8eb135cb418fc30b3b6a455c45f5fc4
voduchau <h002250@cybozu.onmicrosoft.com>
2023-05-31 13:34:38 +0700

    SSR-2966: implement unit test for accessibility and describeChild props

---
+++ src/tooltip/test/accessibility.test.ts
@@ -0,0 +1,104 @@
+import { elementUpdated, expect, fixture } from "@open-wc/testing";
+
+import { Tooltip } from "../index";
+
+const getButtonElement = () => {
+  const buttonEl = document.createElement("button");
+  return buttonEl;
+};
+
+describe("Tooltip", () => {
+  describe("accessibility", () => {
+    it("should open the Tooltip when mouseenter and close the Tooltip when mouse leave", async () => {
+      const container = new Tooltip({
+        describeChild: true,
+        container: getButtonElement(),
+        title: "sample-title",
+      });
+      const el = await fixture(container);
+      const containerEl = el.querySelector(
+        ".kuc-tooltip__group__container"
+      ) as HTMLDivElement;
+
+      const eventMouseEnter = new MouseEvent("mouseenter");
+      containerEl.dispatchEvent(eventMouseEnter);
+      await elementUpdated(el);
+
+      const titleEl = el.querySelector(
+        ".kuc-tooltip__group__title"
+      ) as HTMLDivElement;
+      expect(getComputedStyle(titleEl).getPropertyValue("display")).to.be.equal(
+        "block"
+      );
+
+      const eventMouseLeave = new MouseEvent("mouseleave");
+      containerEl.dispatchEvent(eventMouseLeave);
+      await elementUpdated(el);
+      expect(getComputedStyle(titleEl).getPropertyValue("display")).to.be.equal(
+        "none"
+      );
+    });
+
+    it("should open the Tooltip when focusin container element", async () => {
+      const container = new Tooltip({
+        describeChild: true,
+        container: getButtonElement(),
+        title: "sample-title",
+      });
+      const el = await fixture(container);
+      const containerEl = el.querySelector(
+        ".kuc-tooltip__group__container"
+      ) as HTMLDivElement;
+
+      const eventFocusin = new MouseEvent("focusin");
+      containerEl.dispatchEvent(eventFocusin);
+      await elementUpdated(el);
+
+      const titleEl = el.querySelector(
+        ".kuc-tooltip__group__title"
+      ) as HTMLDivElement;
+      expect(getComputedStyle(titleEl).getPropertyValue("display")).to.be.equal(
+        "block"
+      );
+
+      const eventFocusout = new MouseEvent("focusout");
+      containerEl.dispatchEvent(eventFocusout);
+      await elementUpdated(el);
+      expect(getComputedStyle(titleEl).getPropertyValue("display")).to.be.equal(
+        "none"
+      );
+    });
+
+    it("should close the tooltip when pressing the Escape key while it is open", async () => {
+      const container = new Tooltip({
+        describeChild: true,
+        container: getButtonElement(),
+        title: "sample-title",
+      });
+      const el = await fixture(container);
+      const containerEl = el.querySelector(
+        ".kuc-tooltip__group__container"
+      ) as HTMLDivElement;
+
+      const eventMouseEnter = new MouseEvent("mouseenter");
+      containerEl.dispatchEvent(eventMouseEnter);
+
+      const titleEl = el.querySelector(
+        ".kuc-tooltip__group__title"
+      ) as HTMLDivElement;
+      expect(getComputedStyle(titleEl).getPropertyValue("display")).to.be.equal(
+        "block"
+      );
+
+      const eventEscapeKey = new KeyboardEvent("keydown", {
+        key: "Escape",
+        bubbles: true,
+      });
+      containerEl.dispatchEvent(eventEscapeKey);
+      await elementUpdated(el);
+      expect(getComputedStyle(titleEl).getPropertyValue("display")).to.be.equal(
+        "none"
+      );
+    });
+  });
+});
---
+++ src/tooltip/test/describeChild.test.ts
@@ -0,0 +1,88 @@
+import { expect, fixture } from "@open-wc/testing";
+
+import { Tooltip } from "../index";
+
+const getButtonElement = () => {
+  const buttonEl = document.createElement("button");
+  return buttonEl;
+};
+
+describe("Tooltip", () => {
+  describe("describeChild", () => {
+    it("should set 'aria-label' attribute to first child element when not assigning", async () => {
+      const container = new Tooltip({
+        container: getButtonElement(),
+        title: "sample-title",
+      });
+      const el = await fixture(container);
+      const containerEl = el.querySelector(
+        ".kuc-tooltip__group__container"
+      ) as HTMLDivElement;
+
+      const event = new MouseEvent("mouseenter");
+      containerEl.dispatchEvent(event);
+
+      const firstChildElement = containerEl.firstElementChild;
+      expect(firstChildElement!.getAttribute("aria-label")).to.be.equal(
+        "sample-title"
+      );
+    });
+
+    it("should set 'aria-label' attribute to first child element when assigned to false", async () => {
+      const container = new Tooltip({
+        describeChild: false,
+        container: getButtonElement(),
+        title: "sample-title",
+      });
+      const el = await fixture(container);
+      const containerEl = el.querySelector(
+        ".kuc-tooltip__group__container"
+      ) as HTMLDivElement;
+
+      const event = new MouseEvent("mouseenter");
+      containerEl.dispatchEvent(event);
+
+      const firstChildElement = containerEl.firstElementChild;
+      expect(firstChildElement!.getAttribute("aria-label")).to.be.equal(
+        "sample-title"
+      );
+    });
+
+    it("should assigned the 'title' attribute to first child element when assign to true and the tooltip is closed", async () => {
+      const container = new Tooltip({
+        describeChild: true,
+        container: getButtonElement(),
+        title: "sample-title",
+      });
+      const el = await fixture(container);
+      const containerEl = el.querySelector(
+        ".kuc-tooltip__group__container"
+      ) as HTMLDivElement;
+
+      const firstChildElement = containerEl.firstElementChild;
+      expect(firstChildElement!.getAttribute("title")).to.be.equal(
+        "sample-title"
+      );
+    });
+
+    it("should assigned the 'aria-describedby' attribute to the first child element when the tooltip is opened", async () => {
+      const container = new Tooltip({
+        describeChild: true,
+        container: getButtonElement(),
+        title: "sample-title",
+      });
+      const el = await fixture(container);
+      const containerEl = el.querySelector(
+        ".kuc-tooltip__group__container"
+      ) as HTMLDivElement;
+
+      const event = new MouseEvent("focusin");
+      containerEl.dispatchEvent(event);
+
+      const firstChildElement = containerEl.firstElementChild;
+      expect(firstChildElement!.hasAttribute("aria-describedby")).to.be.equal(
+        true
+      );
+    });
+  });
+});
