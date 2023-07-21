4256f4b1872b90dbdf355ff5a2fa3bf1d65fb35a
h000780 <hf000780@gmail.com>
2023-07-21 10:11:00 +0800

    SSR-3412: Fix-GitHub-Es-lint-Warning

--- src/js/deprecate.ts
+++ src/js/deprecate.ts
@@ -2,6 +2,7 @@ let isShownDeprecationWarning = false;
 if (!isShownDeprecationWarning) {
   isShownDeprecationWarning = true;
   console.warn(
-    `The maintenance of kintone UI Component v0 will end on December 31, 2023, and we will recommend you migrate to v1.\nFor more details, please check here: https://github.com/kintone-labs/kintone-ui-component/discussions/1280`
+    'The maintenance of kintone UI Component v0 will end on December 31, 2023, and we will recommend you migrate to v1.\n' +
+    'For more details, please check here: https://github.com/kintone-labs/kintone-ui-component/discussions/1280'
   );
 }
