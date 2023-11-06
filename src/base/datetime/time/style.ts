addbc119425e2364c0de00381e5b85a55fb61983
h000780 <hf000780@gmail.com>
2023-11-06 14:20:48 +0800

    SSR-3998: Update for review

--- src/base/datetime/time/style.ts
+++ src/base/datetime/time/style.ts
@@ -84,6 +84,8 @@ export const BASE_TIME_CSS = `
 .kuc-base-time__group--disabled {
   background-color: #d4d7d7;
   box-shadow: none;
+  color: #888888;
+  cursor: not-allowed;
 } 
 .kuc-base-time__group--disabled span,
 .kuc-base-time__group__hours:disabled,
