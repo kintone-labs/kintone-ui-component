c1426b41adfa4f674aec6d4cb8ee1f1e4522589b
h000780 <hf000780@gmail.com>
2024-01-04 16:52:34 +0800

    SSR-4090: Modify warn to error

--- src/js/deprecate.ts
+++ src/js/deprecate.ts
@@ -1,7 +1,7 @@
 let isShownDeprecationWarning = false;
 if (!isShownDeprecationWarning) {
   isShownDeprecationWarning = true;
-  console.warn(
+  console.error(
     'The maintenance of kintone UI Component v0 has ended.\n' +
     'Please migrate to kintone UI Component v1: https://github.com/kintone-labs/kintone-ui-component'
   );
