3667a6030dd40637dd4a9a6d0c4e98123b580ab0
h000780 <hf000780@gmail.com>
2024-01-10 17:28:57 +0800

    SSR-4090: Update deprecate content

--- src/js/deprecate.ts
+++ src/js/deprecate.ts
@@ -2,7 +2,7 @@ let isShownDeprecationWarning = false;
 if (!isShownDeprecationWarning) {
   isShownDeprecationWarning = true;
   console.error(
-    'The maintenance of kintone UI Component v0 has ended.\n' +
-    'Please migrate to kintone UI Component v1: https://github.com/kintone-labs/kintone-ui-component'
+    'The maintenance of kintone UI Component v0 ended on 31 December 2023.\n' +
+    'We recommend migrating to kintone UI Component v1: https://github.com/kintone-labs/kintone-ui-component'
   );
 }
