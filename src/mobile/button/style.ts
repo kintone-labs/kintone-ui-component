6ba552126a906ef04d3155214bed37dbf70173a3
h000780 <hf000780@gmail.com>
2024-01-15 17:50:29 +0800

    SSR-4140: Update for review

--- src/mobile/button/style.ts
+++ src/mobile/button/style.ts
@@ -25,8 +25,8 @@ export const MOBILE_BUTTON_CSS = `
     display: none;
   }
   .kuc-mobile-button__button {
-    min-width: var(--kuc-mobile-button-width, "100px");
-    width: var(--kuc-mobile-button-width, "auto");
+    min-width: var(--kuc-mobile-button-width, 100px);
+    width: var(--kuc-mobile-button-width, auto);
     height: var(--kuc-mobile-button-height, 42px);
     padding: 0 12px;
     user-select: none;
@@ -37,31 +37,42 @@ export const MOBILE_BUTTON_CSS = `
     align-items: center;
     align-content: center;
   }
-  .kuc-mobile-button__button:focus {
-    outline: none;
-    border-color: var(--kuc-mobile-button-background-color-focus);
-    background-color: var(--kuc-mobile-button-background-color-focus);
-  }
-  .kuc-mobile-button__button:active {
-    border-color: var(--kuc-mobile-button-background-color-active);
-    background-color: var(--kuc-mobile-button-background-color-active);
-  }
   .kuc-mobile-button__button--submit {
-    border: 2px solid #206694;
-    background-color: #206694;
-    color: #ffffff;
+    border: 2px solid;
+    border-color: var(--kuc-mobile-button-background-color, #206694);
+    background-color: var(--kuc-mobile-button-background-color, #206694);
+    color: var(--kuc-mobile-button-text-color, #ffffff);
     border-radius: 6px;
   }
+  .kuc-mobile-button__button--submit:focus {
+    outline: none;
+    border-color: var(--kuc-mobile-button-background-color-focus, #206694);
+    background-color: var(--kuc-mobile-button-background-color-focus, #206694);
+  }
+  .kuc-mobile-button__button--submit:active {
+    border-color: var(--kuc-mobile-button-background-color-active, #206694);
+    background-color: var(--kuc-mobile-button-background-color-active, #206694);
+  }
   .kuc-mobile-button__button--submit:disabled {
+    color: #ffffff;
     border-color: #a5a5a5;
     background: #a5a5a5;
   }
   .kuc-mobile-button__button--normal {
     border: 2px solid;
-    border-color: var(--kuc-mobile-button-background-color,"#206694");
+    border-color: var(--kuc-mobile-button-background-color, #206694);
     background-color: var(--kuc-mobile-button-background-color, #ffffff);
     color: var(--kuc-mobile-button-text-color, #206694);
     border-radius: 6px;
+  }
+  .kuc-mobile-button__button--normal:focus {
+    outline: none;
+    border-color: var(--kuc-mobile-button-background-color-focus, #206694);
+    background-color: var(--kuc-mobile-button-background-color-focus), #206694;
+  }
+  .kuc-mobile-button__button--normal:active {
+    border-color: var(--kuc-mobile-button-background-color-active, #206694);
+    background-color: var(--kuc-mobile-button-background-color-active, #206694);
   }
   .kuc-mobile-button__button--normal:disabled {
     color: #a5a5a5;
