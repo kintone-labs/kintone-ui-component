886d469a193d5ff49b24526f4aa821e21050afb2
DongLin <ldong@cybozu.cn>
2024-11-07 14:28:51 +0800

    SSR-4321: update the generate license script to correctly get the license of packages with version numbers in allowPackages.

--- .github/actions/generate_licenses/index.js
+++ .github/actions/generate_licenses/index.js
@@ -1,6 +1,7 @@
 const core = require("@actions/core");
 const fs = require("fs");
 const path = require("path");
+const { isMatchPackage } = require("@cybozu/license-manager");
 
 const workingDirectory = path.resolve("./");
 const productLicenseFile = path.resolve("./license-manager/product-license");
@@ -12,72 +13,6 @@ const extractedProdLicenseFilePath = `${workingDirectory}/licenses-prod.json`;
 
 const licenseManagerDevConfigPath = `${workingDirectory}/license-manager/license-manager-dev.config.js`;
 const extractedDevLicenseFilePath = `${workingDirectory}/licenses-dev.json`;
-
-let packageInfo = {};
-try {
-  packageInfo = JSON.parse(fs.readFileSync(packagePath).toString());
-} catch (error) {
-  core.setFailed(error.message);
-}
-
-let dependenciesList;
-try {
-  dependenciesList = Object.keys(packageInfo.dependencies);
-} catch (_) {
-  dependenciesList = [];
-}
-
-let devDependenciesList;
-try {
-  devDependenciesList = Object.keys(packageInfo.devDependencies);
-} catch (_) {
-  devDependenciesList = [];
-}
-
-let prodLicensesInfo = [];
-try {
-  prodLicensesInfo = JSON.parse(
-    fs.readFileSync(extractedProdLicenseFilePath).toString(),
-  );
-} catch (error) {
-  core.setFailed(error.message);
-}
-
-let devNeedExportDependencies = [];
-const devConfig = require(licenseManagerDevConfigPath);
-if (devConfig.analyze && devConfig.analyze.allowPackages) {
-  const devAllowPackages = devConfig.analyze.allowPackages;
-  devNeedExportDependencies = devDependenciesList.filter((dep) => {
-    return devAllowPackages.includes(dep);
-  });
-}
-
-let devLicensesInfo = [];
-try {
-  devLicensesInfo = JSON.parse(
-    fs.readFileSync(extractedDevLicenseFilePath).toString(),
-  );
-} catch (error) {
-  core.setFailed(error.message);
-}
-
-let licenseContent = "";
-
-try {
-  licenseContent = `${fs.readFileSync(productLicenseFile).toString()}
-
-
-`;
-} catch (e) {
-  licenseContent = "";
-}
-
-if (dependenciesList.length > 0 || devNeedExportDependencies.length > 0) {
-  licenseContent += `Licenses for Third-Party Libraries
-
-  The following sections contain licensing information for libraries that
-  we have included with the kuc.min.js.`;
-}
 
 const formatLicenseContent = (licenseInfo) => {
   let repository;
@@ -91,32 +26,98 @@ const formatLicenseContent = (licenseInfo) => {
     }
   }
 
-  return `
-
-
-${licenseInfo.name}${repository ? `\nrepository: ${repository}` : ""}
+  return `\n\n\n${licenseInfo.name}${repository ? `\nrepository: ${repository}` : ""}
 version: ${licenseInfo.version}
 license: ${licenseInfo.license}
 ${licenseInfo.licenseText}`;
 };
 
-dependenciesList.forEach((dependency) => {
-  for (let i = 0; i < prodLicensesInfo.length; i++) {
-    const licenseInfo = prodLicensesInfo[i];
-    if (dependency === licenseInfo.name) {
-      licenseContent += formatLicenseContent(licenseInfo);
-    }
+const generateProdLicenseContent = (packagePath, extractedProdLicenseFilePath) => {
+  let packageInfo = {};
+  try {
+    packageInfo = JSON.parse(fs.readFileSync(packagePath).toString());
+  } catch (error) {
+    core.setFailed(error.message);
   }
-});
 
-devNeedExportDependencies.forEach((dependency) => {
-  for (let i = 0; i < devLicensesInfo.length; i++) {
-    const licenseInfo = devLicensesInfo[i];
-    if (dependency === licenseInfo.name) {
-      licenseContent += formatLicenseContent(licenseInfo);
-    }
+  let dependenciesList;
+  try {
+    dependenciesList = Object.keys(packageInfo.dependencies);
+  } catch (_) {
+    dependenciesList = [];
   }
-});
+
+  let prodLicensesInfo = [];
+  try {
+    prodLicensesInfo = JSON.parse(
+      fs.readFileSync(extractedProdLicenseFilePath).toString(),
+    );
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+
+  let prodLicenseContent = "";
+  dependenciesList.forEach((dependency) => {
+    for (let i = 0; i < prodLicensesInfo.length; i++) {
+      const licenseInfo = prodLicensesInfo[i];
+      if (dependency === licenseInfo.name) {
+        prodLicenseContent += formatLicenseContent(licenseInfo);
+        break;
+      }
+    }
+  });
+
+  return prodLicenseContent;
+};
+
+const generateDevLicenseContent = (licenseManagerDevConfigPath, extractedDevLicenseFilePath) => {
+  let devLicensesInfo = [];
+  try {
+    devLicensesInfo = JSON.parse(
+      fs.readFileSync(extractedDevLicenseFilePath).toString(),
+    );
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+
+  let devLicenseContent = "";
+  const devConfig = require(licenseManagerDevConfigPath);
+  if (devConfig.analyze && devConfig.analyze.allowPackages) {
+    const devAllowPackages = devConfig.analyze.allowPackages;
+    devAllowPackages.forEach((devAllowPackage) => {
+      for (let i = 0; i < devLicensesInfo.length; i++) {
+        const licenseInfo = devLicensesInfo[i];
+        if (isMatchPackage(licenseInfo, devAllowPackage)) {
+          devLicenseContent += formatLicenseContent(licenseInfo);
+        }
+      }
+    });
+  }
+
+  return devLicenseContent;
+};
+
+let licenseContent;
+
+try {
+  licenseContent = `${fs.readFileSync(productLicenseFile).toString()}`;
+} catch (e) {
+  licenseContent = "";
+}
+
+const prodLicenseContent = generateProdLicenseContent(packagePath, extractedProdLicenseFilePath);
+const devLicenseContent = generateDevLicenseContent(licenseManagerDevConfigPath, extractedDevLicenseFilePath);
+
+if (prodLicenseContent || devLicenseContent) {
+  licenseContent += licenseContent ? "\n\n\n" : "";
+  licenseContent += `Licenses for Third-Party Libraries
+
+  The following sections contain licensing information for libraries that
+  we have included with the kuc.min.js.`;
+
+  licenseContent += prodLicenseContent ? prodLicenseContent : "";
+  licenseContent += devLicenseContent ? devLicenseContent : "";
+}
 
 fs.writeFileSync(licenseFile, licenseContent);
 core.setOutput("license_file_path", licenseFile);
