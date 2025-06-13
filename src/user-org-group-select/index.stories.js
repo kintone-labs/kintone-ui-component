ab5d3cebf48e422fcfdbf2df60b75830b58cc778
h000780 <hfzhao@cybozu.cn>
2025-06-13 09:38:14 +0800

    SSR-5439: Create prototype

---
+++ src/user-org-group-select/index.stories.js
@@ -0,0 +1,86 @@
+import { html } from "lit";
+import "./index.ts";
+export default {
+  title: "desktop/user-org-group-select",
+  argTypes: {
+    className: { name: "className" },
+    disabled: { name: "disabled" },
+    error: { name: "error" },
+    icon: { name: "icon" },
+    id: { name: "id" },
+    items: { name: "items" },
+    label: { name: "label" },
+    requiredIcon: { name: "requiredIcon" },
+    selectedIndex: { name: "selectedIndex" },
+    value: { name: "value" },
+    visible: { name: "visible" },
+  },
+  parameters: {
+    actions: {
+      handles: ["change", "click-picker-icon"],
+    },
+  },
+};
+const template = (args) => {
+  const handleMultiChoiceChange = (event) => {
+    console.log(event);
+  };
+  return html`
+    <kuc-user-org-group-select
+      .className="${args.className}"
+      .disabled="${args.disabled}"
+      .error="${args.error}"
+      .icon="${args.icon}"
+      .id="${args.id}"
+      .items="${args.items}"
+      .label="${args.label}"
+      .placeholder="${args.placeholder}"
+      .requiredIcon="${args.requiredIcon}"
+      .value="${args.value}"
+      .visible="${args.visible}"
+      @change="${handleMultiChoiceChange}"
+    ></kuc-user-org-group-select>
+  `;
+};
+export const Base = template.bind({});
+Base.args = {
+  label:
+    "Sample label",
+  requiredIcon: true,
+  items: [
+    {
+      label: "User A",
+      value: "userA",
+      type: "user",
+      disabled: true,
+    },
+    {
+      label: "User B",
+      value: "userB",
+      type: "user",
+    },
+    {
+      label: "Organization AOrganization AOrsAOrganization",
+      value: "orgA",
+      type: "org",
+    },
+    {
+      label: "Group A",
+      value: "groupA",
+      type: "group",
+    },
+    {
+      label: "User C",
+      value: "userC",
+      type: "user",
+    },
+  ],
+  value: ["item-1", "orgA", "groupA"],
+  error: "Error occurred!",
+  className: "sample-class",
+  icon: "user",
+  id: "sample-id",
+  placeholder: "Sample placeholder",
+  visible: true,
+  disabled: false,
+};
---
+++ src/user-org-group-select/type.ts
@@ -0,0 +1,50 @@
+import { PropertyValues } from "lit";
+
+import { KucBase } from "../base/kuc-base";
+
+export declare type UserOrgGroupSelectChangeEventDetail = {
+  oldValue: string[];
+  value: string[];
+};
+export declare type UserOrgGroupSelectItem = {
+  label?: string;
+  value?: string;
+  disabled?: boolean;
+  type?: "user" | "org" | "group";
+};
+
+export declare type UserOrgGroupSelectProps = {
+  className?: string;
+  error?: string;
+  id?: string;
+  label?: string;
+  placeholder?: string;
+  value?: string[];
+  disabled?: boolean;
+  requiredIcon?: boolean;
+  visible?: boolean;
+  items?: UserOrgGroupSelectItem[];
+  icon?: "" | "user" | "org" | "group";
+};
+
+export declare class UserOrgGroupSelect extends KucBase {
+  className: string;
+  error: string;
+  id: string;
+  label: string;
+  placeholder: string;
+  value: string[];
+  disabled: boolean;
+  requiredIcon: boolean;
+  visible: boolean;
+  items: UserOrgGroupSelectItem[];
+  selectedIndex: number[];
+  icon: "" | "user" | "org" | "group";
+  constructor(props?: UserOrgGroupSelectProps);
+  shouldUpdate(changedProperties: PropertyValues): boolean;
+  willUpdate(changedProperties: PropertyValues): void;
+  update(changedProperties: PropertyValues): void;
+  render(): import("lit").TemplateResult<1>;
+  firstUpdated(): void;
+  updated(): Promise<void>;
+}
