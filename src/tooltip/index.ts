ad782cfbea722c18395bd7f9a9cbdf9d5cb31932
Hai Nguyen Van <hai-nguyen@cybozu.vn>
2026-04-03 08:11:45 +0700

    SSR-5932: [kuc v1] Upgrade dependencies and check security alert and EOL checker for v1.25.0

--- src/tooltip/index.ts
+++ src/tooltip/index.ts
@@ -1,7 +1,7 @@
 import { html, PropertyValues } from "lit";
 import { property, query } from "lit/decorators.js";
-import { DirectiveResult } from "lit/directive";
-import { UnsafeHTMLDirective } from "lit/directives/unsafe-html";
+import { DirectiveResult } from "lit/directive.js";
+import { UnsafeHTMLDirective } from "lit/directives/unsafe-html.js";
 
 import { unsafeHTMLConverter } from "../base/converter";
 import { createStyleOnHeader, generateGUID, KucBase } from "../base/kuc-base";
